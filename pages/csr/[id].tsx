3ff5a83793ad510b5686ad80a511f5089cf2693c
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-08 03:29:14 -0700

    REGISTER AND LOGIN

--- pages/csr/[id].tsx
+++ pages/csr/[id].tsx
@@ -1,15 +1,17 @@
 // CSR SINGLE PAGE
-import { NextPage, GetStaticProps } from 'next'
+import { NextPage, GetStaticProps, GetStaticPaths } from 'next'
+import { useRouter } from 'next/router'
 
 interface Props { }
 
 const SingePageCSR: NextPage<Props> = ({ }) => {
 
-
+  const router = useRouter()
 
   return (
     <div>
       <h1 className='bg-zinc-800 text-cyan-500 text-3xl text-center font-bold p-4'>CRS RENDERING - SINGLEPAGE.TSX</h1>
+      <h2 className='flex items-center justify-center mt-10 text-6xl font-bold bg-zinc-800 py-4 px-8 w-fit mx-auto rounded-xl leading-loose'>{router.query.id}</h2>
     </div>
   )
 }
@@ -17,6 +19,13 @@ const SingePageCSR: NextPage<Props> = ({ }) => {
 export const getStaticProps: GetStaticProps = async (ctx) => {
   return {
     props: {},
+  }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: false,
   }
 }
 
