08989c34c35aee6abcd0ee9f45f860c40c436f77
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-06 13:13:15 -0700

    STYLINGS

--- pages/styling/index.tsx
+++ pages/styling/index.tsx
@@ -1,4 +1,13 @@
 //^ STYLINGS PAGE =================================================================================================================================
+//& TUTORIAL
+// STYLING IN NEXT JS :
+// 1 - GLOBAL CSS
+// 2 - CSS MODULES
+// 3 - TAILWIND CSS
+// 4 - SASS
+// 5 - CSS-IN-JS
+
+
 import React from 'react'
 
 const index = () => {
