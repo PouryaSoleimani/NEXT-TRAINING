ef763e3cd7a78e05b1eb2a51e02eb1e0eb68a986
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-06 13:31:58 -0700

    STYLINGS

--- pages/styling/index.tsx
+++ pages/styling/index.tsx
@@ -7,8 +7,9 @@
 // 4 - SASS
 // 5 - CSS-IN-JS
 
-// 1 - GLOBAL CSS FILES 
-
+// 1 - GLOBAL CSS FILES AT _app.tsx
+// 2 - CSS MODULES : 
+import styles from '@/styles/styling.module.css'
 
 import React from 'react'
 
@@ -16,6 +17,7 @@ const Styling = () => {
   return (
     <div>
       <h1 id='stylingTitle'>STYLING PAGE</h1>
+      <h2 className={styles.stylingText}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed nostrum, exercitationem laudantium suscipit animi cum velit voluptates assumenda quibusdam nemo vitae eligendi modi, aliquam reiciendis similique dolorum consequatur nulla! Ab?</h2>
     </div>
   )
 }
