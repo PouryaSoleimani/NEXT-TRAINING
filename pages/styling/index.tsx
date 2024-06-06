9790a325592777226e6acb8bc5f6bcf0b366cd39
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-06 13:48:05 -0700

    STYLINGS

--- pages/styling/index.tsx
+++ pages/styling/index.tsx
@@ -18,6 +18,26 @@ const Styling = () => {
     <div>
       <h1 id='stylingTitle'>STYLING PAGE</h1>
       <h2 className={styles.stylingText}>Lorem ipsum, dolor sit amet  modi, aliquam reiciendis similique dolorum consequatur nulla! Ab?</h2>
+      <button>CLICK ME
+        <style jsx>
+          {`
+          button{
+          background-color : green ;
+          padding : 1rem 2rem;
+          font-size : 30px;
+          font-weight : 800;
+          border : 6px solid white ;
+          border-radius : 30px;
+          margin : 0 auto;
+          width : 100%;
+          cursor : pointer;
+          }
+          button:hover{
+          background-color : #004400
+          }
+          `}
+        </style>
+      </button>
     </div>
   )
 }
