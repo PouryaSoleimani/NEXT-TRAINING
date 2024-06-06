dc077dbe348e8059cf0d8afdcb9d70fe29e3ed33
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-06 04:08:53 -0700

    INIT

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
     <>
       <main className={`flex min-h-screen flex-col items-center justify-center p-24`}>
         <h1 className="py-10 px-10 bg-zinc-300 text-black rounded-lg text-6xl font-sans font-bold shadow-lg shadow-current">HOME PAGE</h1>
-        <h2>{name}-{age}-{job}</h2>
+        <h2 className="text-3xl font-bold text-red-700">{name}-{age}-{job}</h2>
         <ul className="bg-zinc-800 w-screen box-border p-10 mt-10 border-2 border-zinc-600 flex items-center justify-center space-x-10">
           <Link href='/ssg' className="bg-yellow-400 text-3xl p-2 rounded-lg text-black font-bold border-8 border-black">SSG PAGE</Link>
         </ul>
