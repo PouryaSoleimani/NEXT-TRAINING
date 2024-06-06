7e441d0d1dc5fe4670e152fe07824010da8e3b83
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-06 10:39:52 -0700

    PAGES

--- pages/slugs/[...params].tsx
+++ pages/slugs/[...params].tsx
@@ -4,12 +4,12 @@ import React from 'react'
 
 const Params = () => {
     const router = useRouter()
-    const { params = [] } = router.query;
+    const { params } = router.query;
     console.log(params)
 
     return (
         <div className='text-5xl bg-zinc-900 text-yellow-400 flex flex-col items-center justify-around font-bold p-10'>
-            {params.length && (`${params[0].toUpperCase()} -${params[1].toUpperCase()}-${params[2]}-${params[3].toUpperCase()}`)}
+            {params?.length && (`${params[0].toUpperCase()}-${params[1].toUpperCase()}-${params[2]}-${params[3].toUpperCase()}`)}
         </div>
     )
 }
