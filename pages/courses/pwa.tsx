9fc25de88e3359abdc7ba0491caf3a7d41e3ec5e
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-06 07:28:16 -0700

    PAGES

---
+++ pages/courses/pwa.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Pwa = () => {
+  return (
+    <div className='text-6xl font-extrabold text-center mt-10 text-yellow-400'>PWA COURSE PAGE</div>
+  )
+}
+
+export default Pwa
