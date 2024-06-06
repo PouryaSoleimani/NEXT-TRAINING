bc2939890d3bbdeb636888f2e4e031cb1788b449
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-06 07:31:54 -0700

    PAGES

--- pages/courses/pwa.tsx
+++ pages/courses/pwa.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Pwa = () => {
   return (
-    <div className='text-6xl font-extrabold text-center mt-10 text-yellow-400'>PWA COURSE PAGE</div>
+    <div className='text-6xl font-extrabold text-center mt-10 text-yellow-400 underline decoration-slate-600'>PWA COURSE PAGE</div>
   )
 }
 
