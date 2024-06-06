1184daa76b4c189445ee8d9af7302a60d1525d41
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-06 13:11:58 -0700

    STYLING

---
+++ pages/styling/index.tsx
@@ -0,0 +1,10 @@
+//^ STYLINGS PAGE =================================================================================================================================
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default index
