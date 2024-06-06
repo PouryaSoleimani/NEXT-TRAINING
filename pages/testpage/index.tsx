209cab3924203e5ed0782a7604e8a6b5e9ba3870
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-06 07:19:55 -0700

    PAGES

---
+++ pages/testpage/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TestPage = () => {
+  return (
+    <div className='text-5xl font-extrabold text-center mt-10'>THIS IS A TEST PAGE</div>
+  )
+}
+
+export default TestPage
