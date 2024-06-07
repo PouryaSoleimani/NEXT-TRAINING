252f3e035ea56fc42cb57d7cb223eb262ed4e3a6
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-07 06:48:58 -0700

    BOOTSTRAP

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 //^  _APP FILE
 import "@/styles/globals.css";
-import 'bootstrap/dist/css/bootstrap.css'
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
