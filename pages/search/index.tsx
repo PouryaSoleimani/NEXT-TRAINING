16b914e5d4d90da3924e10ca138dd56bc85c0d8c
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-07 09:06:14 -0700

    SEARCH

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -19,31 +19,34 @@ const index: React.FC = () => {
     !searchedProducts.length ? setIsShowError(true) : setIsShowError(false)
   }, [searchInfo])
 
+
+
   const searchHandler = (event: React.ChangeEvent<HTMLInputElement>) => { event.preventDefault(); setSearchInfo(event.target.value) }
 
   function goToSearchInput() { router.push('/search/searchinput') }
 
 
-
-
   // RETURN  
   return (
     <div>
+      {/* SEARCH BOX */}
       <div className='flex items-center justify-evenly flex-col space-y-6 w-[30rem] rounded-b-md h-[14rem] px-6 pt-2 pb-2 bg-zinc-600 mx-auto mt-10'>
         <input className='w-full p-2 font-bold outline-none rounded-md text-black' type="search" id='searchInput' value={searchInfo} onChange={searchHandler} />
         <button className='bg-lime-600 px-2 py-1 rounded-md font-bold text-xl'>SEARCH</button>
       </div>
+      {/* LINK */}
       <button onClick={goToSearchInput} className='bg-blue-700 p-2 text-xl font-bold ml-10 rounded-lg hover:bg-blue-400'>GO TO SEARCH INPUT</button>
-      <div className='w-full my-10 bg-zinc-300 h-fit grid place-items-center p-4 grid-cols-3 gap-y-10'>
-        {isShowError ? (
-          <h1 className='text-4xl bg-red-200 mx-auto text-red-700 font-bold text-center p-4 rounded-md w-full border-2 border-red-800 h-fit'>NO PRODUCTS FOUND</h1>
-        ) : (
-          PRODUCTS.map(product => (
-            <div key={product.id} className='bg-black w-[12rem] py-8 px-4 font-bold text-2xl text-center rounded-md '>
-              <h2>{product.title}</h2>
-            </div>
-          ))
-        )
+
+      {/* RESULTS BOX */}
+      <div className='w-full my-10 bg-zinc-300 h-fit flex  items-center justify-center space-x-4 py-10 px-10 overflow-hidden'>
+        {isShowError ? (<h1 className='text-4xl bg-red-900 text-red-200 font-bold text-center px-4 py-6 rounded-md border-2 border-red-800 h-fit w-screen'>NO PRODUCTS FOUND</h1>)
+          : (
+            PRODUCTS.map(product => (
+              <div key={product.id} className='bg-black w-[12rem] py-8 px-4 font-bold text-2xl text-center rounded-md '>
+                <h2>{product.title}</h2>
+              </div>
+            ))
+          )
         }
       </div>
     </div>
