c3fd70db526b4d1c366a73a73513c9e14df92c1d
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-08 11:37:57 -0700

    RENDERING

--- pages/ssg/ssg.tsx
+++ pages/ssg/ssg.tsx
@@ -1,45 +1,43 @@
+//^ SSG PAGE
 import axios from 'axios'
 import { NextPage, GetStaticProps } from 'next'
+import Link from 'next/link'
 
 interface Props { response: [{ id: number, title: string, price: number }] }
 type SingleItemType = { id: number, title: string, price: number }
 
+//COMPONENT
 const Ssg: NextPage<Props> = ({ response }) => {
     return (
         <>
-            <h1 className='text-4xl font-extrabold text-center py-5 bg-yellow-400'>SSG PAGE</h1>
-
-
-            <div className='text-xl space-y-2 font-semibold'>
-                {response.map((item: SingleItemType) =>
-                    <p key={item.id}>{item.id} - {item.title.slice(0, 12)} : <span className='text-red-500'>${item.price}</span>
-                    </p>)
-                }
+            <div className='text-2xl flex flex-col space-y-2 font-semibold'>
+                {response.map((item: SingleItemType) => (
+                    <>
+                        <Link href={`/ssg/${item.id}`} key={item.id}>
+                            <h2>{item.id} - {item.title} : <span className='text-red-600'>"${item.price}"</span></h2>
+                        </Link>
+                    </>
+                )
+                )}
             </div>
-
-
         </>
-    )
+
+    )   
 }
 
-
-// ^ STATIC PROPS
 export const getStaticProps: GetStaticProps = async (ctx) => {
     const request = axios.get('https://fakestoreapi.com/products')
     const response = (await request).data
+    
 
-
-    return {
-        props: { response },
-    }
+    
+    return { props: { response }, }
 }
 
 export default Ssg
 
 
-
-
-// ? SSG TUTORIAL :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+// ^ SSG TUTORIAL :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 // GET STATIC PROPS :
 // 1 - ONLY RUNS IN SERVER-SIDE
 // 2 - VERY SECURE
