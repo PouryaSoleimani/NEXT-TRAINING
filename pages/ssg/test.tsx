57d7c263f7a88cf7a0efda6b79b0ad77dcff847e
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-09 03:52:14 -0700

    SSG

--- pages/ssg/test.tsx
+++ pages/ssg/test.tsx
@@ -11,7 +11,7 @@ const SsgTest: NextPage<Props> = ({ data }) => {
   return (
     <>
       <h1 className='text-5xl font-extrabold bg-slate-900 text-center py-4'>SSG TEST PAGE</h1>
-      <div className='text-2xl font-semibold space-y-1.5'>
+      <div className='text-2xl font-semibold space-y-4'>
         {data.map((item: SingleItemType) => <h2 key={item.id} className='hover:bg-slate-100 hover:text-black duration-300 cursor-pointer'>{item.id} - {item.title} : <span className='text-emerald-600'>${item.price}</span></h2>)}
       </div>
     </>
