61ea053e2c018543c39472da530c81479b33bf03
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-10 03:18:01 -0700

    SWR

--- pages/ssg/swr.tsx
+++ pages/ssg/swr.tsx
@@ -1,18 +1,16 @@
 import axios from 'axios'
 import React from 'react'
 import useSWR from 'swr'
+import __ from 'lodash'
 
-type SingleUserType = { id: number, username: string, email: string }
+
+type SingleUserType = { id: number, name: { firstname: string, lastname: string }, email: string }
 
 
 const Swr = () => {
 
   const fetcher = () => axios.get('https://fakestoreapi.com/users').then(data => data.data)
   const { data, error, isLoading } = useSWR('https://fakestoreapi.com/users', fetcher)
-  console.log(error)
-
-
-
 
   //^ RETURN
   if (error) return <div className='text-2xl text-red-700'>failed to load</div>
@@ -20,7 +18,7 @@ const Swr = () => {
 
   if (data) return (
     <div className='p-4 text-2xl font-bold flex flex-col space-y-4 bg-zinc-950'>
-      {data?.map((user: SingleUserType) => <p key={user.id} className='hover:bg-zinc-800 p-4 cursor-pointer duration-200 w-fit'>{user.id} - {user.username} : <span className='text-blue-700'>{user.email}</span></p>)}
+      {data?.map((user: SingleUserType) => <p key={user.id} className='hover:bg-zinc-800 p-4 cursor-pointer duration-200 w-fit'>{user.id} - {__.capitalize(user.name.firstname)} {__.capitalize(user.name.lastname)} : <span className='text-blue-700'>{user.email}</span></p>)}
     </div>
   )
 
