ee501c841b5cf4b945574a4eee8dc2f8a1b9db56
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-10 02:52:31 -0700

    SWR

--- pages/ssg/swr.tsx
+++ pages/ssg/swr.tsx
@@ -1,19 +1,26 @@
-import { NextPage, GetStaticProps } from 'next'
+import axios from 'axios'
+import React from 'react'
+import useSWR from 'swr'
 
-interface Props { }
+type SingleUserType = { id: number, username: string, email: string }
 
-const Swr: NextPage<Props> = ({ }) => {
-  return (
-    <div>
 
+const swr = () => {
+
+  const fetcher = () => axios.get('https://fakestoreapi.com/users').then(data => data.data)
+  const { data, error, isLoading } = useSWR('https://fakestoreapi.com/users', fetcher)
+
+
+
+  //^ RETURN
+  if (error) return <div className='text-2xl text-red-700'>failed to load</div>
+  if (isLoading) return <div className='text-6xl text-zinc-500 flex items-center justify-center h-screen'>Loading...</div>
+
+  if (data) return (
+    <div className='p-4 text-2xl font-bold flex flex-col space-y-4 bg-zinc-950'>
+      {data?.map((user: SingleUserType) => <p key={user.id} className='hover:bg-zinc-800 p-4 cursor-pointer duration-200 w-fit'>{user.id} - {user.username} : <span className='text-blue-700'>{user.email}</span></p>)}
     </div>
   )
-}
 
-export const getStaticProps: GetStaticProps = async (ctx) => {
-  return {
-    props: {},
-  }
 }
-
-export default Swr
+export default swr
