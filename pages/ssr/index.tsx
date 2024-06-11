46fbf9093a12142f365028438f629e8c598c0c37
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-11 06:36:56 -0700

    SSR

--- pages/ssr/index.tsx
+++ pages/ssr/index.tsx
@@ -11,7 +11,7 @@ const SSR: NextPage<Props> = ({ Response }) => {
   // RETURN
   return (
     <div className='p-4 flex flex-col items-start justify-center space-y-4 font-extrabold '>
-      <h1 className='p-2 rounded-md text-center bg-pink-400 flex items-center justify-center text-pink-700 font-bold text-3xl'>SSR RENDERING PAGE</h1>
+      <h1 className='p-2 rounded-md text-center bg-zinc-950 flex items-center justify-center text-pink-700 font-bold text-3xl'>SSR RENDERING PAGE</h1>
 
       {Response.map(item => (
         <Link key={item.id} href={`/ssr/${item.id}`}>
@@ -23,7 +23,7 @@ const SSR: NextPage<Props> = ({ Response }) => {
   )
 }
 
-// GET STATIC PROPS =>
+// GET SERVERSIDE PROPS => RUN AT SERVER SIDE ONLY
 export const getServerSideProps: GetServerSideProps = async () => {
 
   const Request = axios.get('https://jsonplaceholder.typicode.com/users')
