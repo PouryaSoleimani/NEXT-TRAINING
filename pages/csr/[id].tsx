16d3a3f05880016fa55ad03c925285af9fb21c26
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-11 14:59:38 -0700

    JSON SERVER

--- pages/csr/[id].tsx
+++ pages/csr/[id].tsx
@@ -11,7 +11,7 @@ type SingleProductType = { id: number, title: string, price: number }
 // COMPONENT
 const SingePageCSR: NextPage<Props> = ({ }) => {
 
-  function getData() { axios.get('https://fakestoreapi.com/products').then(data => setdata(data.data)) }
+  function getData() { axios.get('http://localhost:4000/products').then(data => setdata(data.data)) }
 
   const [data, setdata] = useState([])
 
@@ -28,7 +28,7 @@ const SingePageCSR: NextPage<Props> = ({ }) => {
       </div>
       <div className='flex flex-wrap items-center justify-center mt-6 px-10 gap-4'>
         {data.map((product: SingleProductType) => (
-          <div key={product.id} className='bg-sky-800 flex w-[30rem] my-2 hover:bg-zinc-800 cursor-pointer items-center justify-between pl-2 pr-4 py-4 text-2xl font-semibold rounded-md'>
+          <div key={product.id} className='bg-zinc-800 flex w-[30rem] my-2 hover:bg-zinc-800 cursor-pointer items-center justify-between pl-2 pr-4 py-4 text-2xl font-semibold rounded-md hover:border box-border'>
             <h1 className='w-3/4'>{product.id} - {product.title.slice(0, 20)}</h1>
             <p className='w-fit bg-emerald-400 text-emerald-800 px-1 rounded-md'>$ {product.price}</p>
           </div>
