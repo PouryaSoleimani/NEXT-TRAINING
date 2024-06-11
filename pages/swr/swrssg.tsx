96df7dd165f03201ad2de5db8fb3c8f44e7022cb
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-11 16:25:20 -0700

    SWR

--- pages/swr/swrssg.tsx
+++ pages/swr/swrssg.tsx
@@ -13,9 +13,13 @@ const Swrssg: NextPage<Props> = ({ }) => {
 
   return (
 
-    <section>
+    <section className='flex text-xl font-bold space-x-6 flex-wrap'>
       {error ? (<h1>ERROR</h1>) : isLoading ? (<h1>IS LOADING </h1>) : (
-        data.map((item: SingleItemType) => <h1>{item.title}</h1>)
+        data.map((item: SingleItemType) => (
+          <div className='bg-zinc-300 hover:bg-white text-black w-fit px-2 py-1 my-2 rounded-xl'>
+            <h1>{item.title}</h1>
+          </div>
+        ))
       )}
     </section>
 
