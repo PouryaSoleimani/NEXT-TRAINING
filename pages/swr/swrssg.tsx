c2f8e12a0e4aa691f09696df9e328b67c939f373
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-11 16:04:28 -0700

    SWR

--- pages/swr/swrssg.tsx
+++ pages/swr/swrssg.tsx
@@ -1,16 +1,27 @@
-//^ SWR AND SSG PAGE ------------------------------------------------------------------------------------------------------------------------------ 
-import { NextPage, GetStaticProps } from 'next'
+import { NextPage } from 'next'
+import Link from 'next/link'
+import useSWR from 'swr'
 
 interface Props { }
+type SingleItemType = { id: number, title: string, price: number }
 
 const Swrssg: NextPage<Props> = ({ }) => {
-  return <div></div>
-}
 
-export const getStaticProps: GetStaticProps = async (ctx) => {
-  return {
-    props: {},
-  }
+  const fetcher = () => fetch('http://localhost:4000/products').then(res => res.json())
+
+  const { data, isLoading, error } = useSWR('PRODUCTS', fetcher)
+
+  return (
+
+    <section>
+      {error ? (<h1>ERROR</h1>) : isLoading ? (<h1>IS LOADING </h1>) : (
+        data.map((item: SingleItemType) => <h1>{item.title}</h1>)
+      )}
+    </section>
+
+  )
+
+
 }
 
 export default Swrssg
