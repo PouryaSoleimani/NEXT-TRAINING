94a18477154bef7a31f3d5afff0df1c5c0c3c36a
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-11 06:29:56 -0700

    SSR

--- pages/ssr/index.tsx
+++ pages/ssr/index.tsx
@@ -2,6 +2,7 @@
 import axios from 'axios'
 import { NextPage, GetServerSideProps } from 'next'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 interface Props { Response: [{ id: number, name: string, username: string, email: string, address: object, phone: string, website: string, company: object }] }
 
@@ -12,11 +13,13 @@ const SSR: NextPage<Props> = ({ Response }) => {
   return (
     <div className='p-4 flex flex-col items-start justify-center space-y-4 font-extrabold '>
       <h1 className='p-2 rounded-md text-center bg-pink-400 flex items-center justify-center text-pink-700 font-bold text-3xl'>SSR RENDERING PAGE</h1>
+
       {Response.map(item => (
         <Link key={item.id} href={`/ssr/${item.id}`}>
           <h2 className='text-xl hover:bg-zinc-800'>{item.id}--{item.name}</h2>
         </Link>
       ))}
+
     </div>
   )
 }
