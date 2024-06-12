c6df3006da5a3341b0833d2aaad280b1ab319e77
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-11 19:56:29 -0700

    SWR

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -11,10 +11,10 @@ const Index: NextPage<Props> = ({ data }) => {
   return (
     <section className='flex flex-col items-start justify-start'>
       <div className='w-full'><h1 className='bg-zinc-800 p-4 text-4xl font-bold text-center text-red-600'>TEST PAGE</h1></div>
-      <div className='bg-zinc-800 mt-4 w-fit rounded-xl mx-auto px-5 py-2 flex flex-wrap items-center justify-center  gap-10 space-y-3'>
+      <div className='bg-zinc-800 mt-4 w-fit rounded-xl mx-auto px-5 py-4 flex flex-wrap items-center justify-center gap-10'>
         {data.map((item: SingleItemType) => (
           <Link href={`/test/${item.id}`} key={item.id}>
-            <h1 className='text-xl w-[20rem] bg-slate-900 cursor-pointer font-semibold hover:bg-black p-2'>{item.id} - {item.title} : <span className='bg-green-600 text-green-950 font-extrabold text-xl px-3'>${item.price}</span></h1>
+            <h1 className='text-xl w-[20rem] bg-zinc-900 cursor-pointer font-semibold hover:bg-black shadow-sm hover:shadow shadow-black p-2 flex justify-between'>{item.id} - {item.title}  <span className='bg-green-600 text-green-950 font-extrabold text-xl px-3'>${item.price}</span></h1>
           </Link>
         ))}
       </div>
