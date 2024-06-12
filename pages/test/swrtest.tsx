7c5fa50831b8c7dec9a89306b21af3524ab51d20
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-11 20:19:27 -0700

    TEST

--- pages/test/swrtest.tsx
+++ pages/test/swrtest.tsx
@@ -10,9 +10,9 @@ const Swrtest: NextPage<Props> = ({ }) => {
   const { data, isLoading, error } = useSWR("products", fetcher)
 
   return (
-    <div>
+    <div className='grid grid-cols-4 gap-2 place-items-center w-screen h-screen'>
       {error ? (<h1>ERROR</h1>) : isLoading ? (<h1>LOADING</h1>) : (
-        data.map((item: SingleItemType) => <h1 key={item.id}>{item.title}</h1>)
+        data.map((item: SingleItemType) => <h1 className='px-2 py-1 text-2xl flex items-center hover:border-8 border-green-600 box-border  font-semibold w-[15rem] h-[4.5rem] bg-white text-black' key={item.id}>{item.title}</h1>)
       )}
 
     </div>
