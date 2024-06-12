8d5685a66c5d611a17fd3cff303708a771e777a9
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-11 19:34:37 -0700

    TEST'

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,4 +1,4 @@
-//? TEST PAGE ---------------------------------------------------------------------------------------------------------------------------------------------------------
+//* TEST PAGE ---------------------------------------------------------------------------------------------------------------------------------------------------------
 import axios from 'axios'
 import { NextPage, GetStaticProps } from 'next'
 import Link from 'next/link'
@@ -6,23 +6,22 @@ import Link from 'next/link'
 interface Props { data: [{ id: number, title: string, price: number }] }
 type SingleItemType = { id: number, title: string, price: number }
 
-//^COMPONENT
+//^ COMPONENT ---------------------------------------------------------------------------------------------------------------------------------------------------------
 const Index: NextPage<Props> = ({ data }) => {
   return (
     <section className='flex flex-col items-start justify-start'>
-      <div className='w-full'>
-        <h1 className='bg-zinc-800 p-4 text-4xl font-bold text-center text-red-600'>TEST PAGE</h1>
-      </div>
-      <div className='bg-zinc-800 mt-10 w-fit rounded-xl mx-auto px-5 py-2 grid grid-cols-3 place-items-center gap-10 space-y-3'>
+      <div className='w-full'><h1 className='bg-zinc-800 p-4 text-4xl font-bold text-center text-red-600'>TEST PAGE</h1></div>
+      <div className='bg-zinc-800 mt-4 w-fit rounded-xl mx-auto px-5 py-2 flex flex-wrap items-center justify-center  gap-10 space-y-3'>
         {data.map((item: SingleItemType) => (
           <Link href={`/test/${item.id}`} key={item.id}>
-            <h1 className='text-xl w-[20rem] cursor-pointer font-semibold hover:bg-black p-2'>{item.id} - {item.title} : <span>{item.price}</span></h1>
+            <h1 className='text-xl w-[20rem] bg-slate-900 cursor-pointer font-semibold hover:bg-black p-2'>{item.id} - {item.title} : <span className='bg-green-600 text-green-950 font-extrabold text-xl px-3'>${item.price}</span></h1>
           </Link>
         ))}
       </div>
     </section>
   )
 }
+
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const req = axios.get('http://localhost:4000/products')
