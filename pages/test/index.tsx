3a79d0b5e40d21c5ced3f54ec327bc26552776a1
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-11 19:24:25 -0700

    TEST

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -10,11 +10,9 @@ type SingleItemType = { id: number, title: string, price: number }
 const Index: NextPage<Props> = ({ data }) => {
   return (
     <section className='flex flex-col items-start justify-start'>
-
       <div className='w-full'>
         <h1 className='bg-zinc-800 p-4 text-4xl font-bold text-center text-red-600'>TEST PAGE</h1>
       </div>
-
       <div className='bg-zinc-800 mt-10 w-fit rounded-xl mx-auto px-5 py-2 grid grid-cols-3 place-items-center gap-10 space-y-3'>
         {data.map((item: SingleItemType) => (
           <Link href={`/test/${item.id}`} key={item.id}>
