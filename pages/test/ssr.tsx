17f5b1569a95f260e12b3c2ecf08f8780a606d5a
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-11 18:59:56 -0700

    SSR

---
+++ pages/test/ssr.tsx
@@ -0,0 +1,37 @@
+import axios from 'axios'
+import { NextPage, GetServerSideProps } from 'next'
+import Link from 'next/link'
+
+interface Props { data: [{ id: number, title: string, price: number }] }
+type SingleItemType = { id: number, title: string, price: number }
+
+
+//^ COMPONENT
+const Ssr: NextPage<Props> = ({ data }) => {
+  return (
+    <div className='p-10 font-bold text-4xl'>
+      {data.map((item: SingleItemType) => (
+        <Link href={`/test/${item.id}`} key={item.id} className='hover:bg-zinc-700'>
+          <h1>{item.id} - {item.title} : {item.price}</h1>
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+
+
+
+
+
+//* getServerSideProps
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const req = axios.get('http://localhost:4000/products')
+  const data = (await req).data
+
+  return {
+    props: { data }
+  }
+}
+
+export default Ssr
