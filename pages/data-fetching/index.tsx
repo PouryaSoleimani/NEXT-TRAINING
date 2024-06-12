2cd82b31abda7347dc870e6b3f2cac18bed81462
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-11 20:29:43 -0700

    TEST

--- pages/data-fetching/index.tsx
+++ pages/data-fetching/index.tsx
@@ -1,29 +1,20 @@
 //^ DATA-FETCHING PAGE =========================================================================================================================================================
-import React from 'react'
+import React, { useState } from 'react'
 import { products } from '@/data/db.json'
-
-type DataItemType = { id: string, title: string, price: number }
-type DatasType = DataItemType[]
-
 
 // COMPONENT
 const DataFetching = () => {
-
-  const DATAS: DatasType = products
-
-
-
+  const [data, setdata] = useState(products)
 
   // RETURN
   return (
     <div>
       <h1 className='text-yellow-400 text-6xl font-bold text-center py-4'>DATA FETCHING PAGE</h1>
       <div className='bg-zinc-700 flex flex-col px-10 py-5 space-y-5 text-2xl font-semibold'>
-        {DATAS.map((item: DataItemType) => <p key={item.id}>{item.title} - ${item.price}</p>)}
+        {data.map((item) => <p key={item.id}>{item.title} - ${item.price}</p>)}
       </div>
     </div>
   )
 }
-
 export default DataFetching
 
