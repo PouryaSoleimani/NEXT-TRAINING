b9f79de817c9c898cb9b80f42bd0fa7fc8fa5f7f
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-12 09:22:27 -0700

    SEARCH

--- pages/users/userinfo.tsx
+++ pages/users/userinfo.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const userinfo: React.FC = () => {
 
- const clickHandler = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => { console.log(`CLICK CLICK`) }
+  const clickHandler = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => { console.log(`CLICK CLICK`) }
 
- return (
-  <>
-   <h1>userinfo</h1>
-   <button onClick={clickHandler} className='p-3 bg-sky-700 rounded-xl'>CLICK ME</button>
-  </>
- )
+  return (
+    <>
+      <h1>userinfo</h1>
+      <button onClick={clickHandler} className='p-3 bg-sky-700 rounded-xl'>CLICK ME</button>
+    </>
+  )
 }
 
 export default userinfo
