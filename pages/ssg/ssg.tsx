9b1f4c859880102c605d89aa2c40fbe40ca4254b
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-19 13:35:00 -0700

    DATABASE

--- pages/ssg/ssg.tsx
+++ pages/ssg/ssg.tsx
@@ -8,23 +8,23 @@ type SingleItemType = { id: number, title: string, price: number }
 
 //COMPONENT
 const Ssg: NextPage<Props> = ({ response }) => {
-    return (
-        <div className='text-2xl flex flex-col space-y-2 font-semibold'>
-            {response.map((item: SingleItemType) => (
-                <Link href={`/ssg/${item.id}`} key={item.id}>
-                    <h2>{item.id} - {item.title} : <span className='text-red-600'>"${item.price}"</span></h2>
-                </Link>
-            ))}
-        </div>
-    )
+  return (
+    <div className='text-2xl flex flex-col space-y-2 font-semibold'>
+      {response.map((item: SingleItemType) => (
+        <Link href={`/ssg/${item.id}`} key={item.id}>
+          <h2>{item.id} - {item.title} : <span className='text-red-600'>"${item.price}"</span></h2>
+        </Link>
+      ))}
+    </div>
+  )
 }
 
 //^ GET STATIC PROPS 
 export const getStaticProps: GetStaticProps = async (ctx) => {
-    const request = axios.get('https://fakestoreapi.com/products')
-    const response = (await request).data
+  const request = axios.get('https://fakestoreapi.com/products')
+  const response = (await request).data
 
-    return { props: { response }, }
+  return { props: { response }, revalidate: 43200 }
 }
 
 export default Ssg
