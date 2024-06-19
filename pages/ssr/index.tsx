bc4f0b21f0b3dabddc979f61ba729267b623e0b7
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-19 16:56:47 -0700

    SSR

--- pages/ssr/index.tsx
+++ pages/ssr/index.tsx
@@ -5,20 +5,16 @@ import Link from 'next/link'
 
 interface Props { Response: [{ id: number, name: string, username: string, email: string, address: object, phone: string, website: string, company: object }] }
 
-// COMPONENT
 const SSR: NextPage<Props> = ({ Response }) => {
 
-  // RETURN
   return (
     <div className='p-4 flex flex-col items-start justify-center space-y-4 font-extrabold '>
       <h1 className='p-2 rounded-md text-center bg-zinc-800 flex items-center justify-center text-pink-700 font-bold text-3xl'>SSR RENDERING PAGE</h1>
-
       {Response.map(item => (
         <Link key={item.id} href={`/ssr/${item.id}`}>
           <h2 className='text-xl hover:bg-zinc-800'>{item.id}--{item.name}</h2>
         </Link>
       ))}
-
     </div>
   )
 }
@@ -26,12 +22,12 @@ const SSR: NextPage<Props> = ({ Response }) => {
 //^ GET SERVERSIDE PROPS => RUNS AT SERVER SIDE ONLY
 export const getServerSideProps: GetServerSideProps = async (context) => {
 
-  console.log(context.query)
-
   const Request = axios.get('https://jsonplaceholder.typicode.com/users')
   const Response = (await Request).data
 
-  return { props: { Response } }
+  return {
+    props: { Response: Response }
+  }
 
 }
 
