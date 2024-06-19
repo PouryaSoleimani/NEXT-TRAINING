b5f888119631548d8838d34912c0117687115df6
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-19 08:42:49 -0700

    SWR

--- pages/swr/[id].tsx
+++ pages/swr/[id].tsx
@@ -3,16 +3,13 @@ import { useRouter } from 'next/router'
 import useSWR from 'swr'
 
 type Props = {}
-
 const SwrSinglePage: NextPage<Props> = ({ }) => {
 
   const router = useRouter()
   const productID = router.query.id
 
-
   const fetcher = async () => await fetch(`http://localhost:4000/products/${productID}`).then(result => result.json())
   const { data, error, isLoading } = useSWR(`http://localhost:4000/products/${productID}`, fetcher)
-
 
   return (
     <div className='bg-stone-700 text-3xl font-bold p-8'>
