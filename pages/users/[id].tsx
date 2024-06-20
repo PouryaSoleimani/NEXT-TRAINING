c350a1ed890e9551812fe7f088ccebc635736af2
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-20 07:45:19 -0700

    USERS

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,8 +1,6 @@
 //^ USERS DYNAMIC PAGE
 import axios from 'axios'
 import { NextPage, GetStaticPaths, GetStaticProps } from 'next'
-import { Suspense } from 'react'
-
 interface Props { user: { id: number, name: string, email: string, city: string, phone: string } }
 type SingleUserType = { id: number, name: string, email: string, city: string, phone: string }
 
@@ -10,14 +8,10 @@ type SingleUserType = { id: number, name: string, email: string, city: string, p
 const UserInfoPage: NextPage<Props> = ({ user }) => {
   return (
     <div className='flex items-center justify-center w-full overflow-hidden h-screen text-3xl font-bold underline decoration-emerald-600 decoration-8 scale-150 '>
-      <h1 className='bg-zinc-900 p-8 rounded-xl text-center overflow-hidden border-8 border-green-950'>{user.id}</h1>
-      <Suspense fallback={<h3>LOADING ...</h3>}>
-        <h1 className='bg-zinc-900 p-6 rounded-xl text-center py-8 border-8 border-green-950'>{user.name}</h1>
-      </Suspense>
+      <h1 className='bg-zinc-900 p-8 rounded-xl text-center overflow-hidden border-8 border-green-950'>{user.id} - {user.name}</h1>
     </div>
   )
 }
-
 
 //* STATIC PATHS 
 export const getStaticPaths: GetStaticPaths = async () => {
