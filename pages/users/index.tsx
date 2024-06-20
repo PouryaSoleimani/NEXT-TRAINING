d4f6033c92179b789aab5476879d3b4b4af14365
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-20 07:00:17 -0700

    USERS

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -5,13 +5,14 @@ import Link from 'next/link'
 interface Props { users: [{ id: number, name: string, email: string, city: string, phone: string }] }
 type SingleUserType = { id: number, name: string, email: string, city: string, phone: string }
 
+//^ COMPONENT 
 const UsersPage: NextPage<Props> = ({ users }) => {
   return (
     <>
       <h1 className='text-4xl font-bold text-center py-4 bg-blue-900'>USERS PAGE</h1>
       <div className='flex flex-wrap items-center justify-center p-10 gap-6'>
 
-        {users.map((user: SingleUserType) => (
+        {users.slice(0, 9).map((user: SingleUserType) => (
           <div key={user.id} className='text-xl font-sans border-4 border-blue-600 bg-zinc-200 text-zinc-800 w-fit h-fit p-4 rounded-xl font-bold hover:scale-90 cursor-pointer duration-300'>
             <h1> {user.id} . {user.name}</h1>
             <h2>FROM :{user.city}</h2>
@@ -25,6 +26,7 @@ const UsersPage: NextPage<Props> = ({ users }) => {
   )
 }
 
+//^ GET STATIC PROPS
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const request = axios.get('https://jsonplaceholder.typicode.com/users')
   const users = (await request).data
