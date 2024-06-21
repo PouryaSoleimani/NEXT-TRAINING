1779e112bdd3ed444621e7e9f55b65a370252354
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-21 08:15:15 -0700

    SEARCH

--- pages/search/search-ssg.tsx
+++ pages/search/search-ssg.tsx
@@ -1,14 +1,31 @@
+import axios from 'axios'
 import { NextPage, GetStaticProps } from 'next'
 
-interface Props { }
+interface Props { products: [{ id: number, title: string }] }
 
-const SearchSSGpage: NextPage<Props> = ({ }) => {
-  return <div></div>
+const SearchSSGpage: NextPage<Props> = ({ products }) => {
+  return (
+    <>
+      <div>
+        <input type="search" />
+      </div>
+      <div>
+        {products.map(item => (
+          <div key={item.id}>
+            <h1>{item.title}</h1>
+          </div>
+        ))}
+      </div>
+    </>
+  )
 }
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
+  const req = axios.get('https://fakestoreapi.com/products')
+  const products = (await req).data
+
   return {
-    props: {},
+    props: { products },
   }
 }
 
