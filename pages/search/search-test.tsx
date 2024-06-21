98936ce4f66612c3b700271af590d7c306307339
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-21 07:22:21 -0700

    SEARCH

--- pages/search/search-test.tsx
+++ pages/search/search-test.tsx
@@ -27,7 +27,7 @@ const SearchTestPage: React.FC = () => {
         <input type="search" placeholder='search' value={search} onChange={event => setSearch(event.target.value)} className='p-1 font-bold text-black w-[20rem] outline-none' />
       </div>
       <div className='flex flex-wrap items-center justify-center p-10 gap-2'>
-        {isShowError ? (<h1 className='text-red-700 text-4xl font-extrabold'>NO PRODUCT FOUND</h1>) : (
+        {isShowError ? (<h1 className='text-red-700 text-4xl font-extrabold'>NO PRODUCT</h1>) : (
           PRODUCTS.map((item: SingleItemType) => (
             <div key={item.id} className='font-bold text-xl bg-zinc-700 p-2 rounded-md'>
               <h1>{item.title.slice(0, 10)}</h1>
