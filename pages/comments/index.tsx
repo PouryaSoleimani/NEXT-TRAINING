8dd4742aac68d6489711170a1ea175a8691cd154
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-21 10:19:55 -0700

    COMMENTS

--- pages/comments/index.tsx
+++ pages/comments/index.tsx
@@ -1,22 +1,42 @@
+//^ COMMENTS PAGE ==================================================================================================================================================
 import axios from 'axios'
 import { NextPage, GetStaticProps } from 'next'
+import { useState } from 'react'
 
 interface Props { comments: [{ postId: number, id: number, name: string, email: string, body: string }] }
 
 const Index: NextPage<Props> = ({ comments }) => {
+
+  const [COMMENTS, setCOMMENTS] = useState(comments)
+
+  function filterCommentHandler(ID: number) {
+    const copy = [...comments]
+    const filteredComments: any = copy.filter(item => { return item.postId === ID })
+    setCOMMENTS(filteredComments)
+  }
   return (
-    <div className='flex flex-wrap items-center justify-center p-4 gap-2'>
-      {comments.map(item => (
-        <div key={item.id} className='flex flex-col space-y-2 bg-zinc-800 w-fit text-zinc-200 items-start p-2 rounded-md justify-center'>
-          <h1 className='text-xl font-bold'>{item.name.slice(0, 20)} : </h1>
-          <h4 className='text-sm text-zinc-500 font-thin'>{item.email}</h4>
-          <h4>For post # {item.postId}</h4>
-          <span className='bg-white text-black w-[14rem] h-[14rem] overflow-auto'>
-            <p className='whitespace-pre-wrap px-2 py-1'>{item.body}...</p>
-          </span>
-        </div>
-      ))}
-    </div>
+    <>
+      <div className='flex items-center justify-around px-2 py-3'>
+        <button onClick={event => filterCommentHandler(1)} className='border text-black font-bold text-xl border-black px-2 w-[10rem] bg-red-500'>1</button>
+        <button onClick={event => filterCommentHandler(2)} className='border text-black font-bold text-xl border-black px-2 w-[10rem] bg-blue-500'>2</button>
+        <button onClick={event => filterCommentHandler(3)} className='border text-black font-bold text-xl border-black px-2 w-[10rem] bg-green-500'>3</button>
+        <button onClick={event => filterCommentHandler(4)} className='border text-black font-bold text-xl border-black px-2 w-[10rem] bg-pink-500'>4</button>
+        <button onClick={event => filterCommentHandler(5)} className='border text-black font-bold text-xl border-black px-2 w-[10rem] bg-yellow-400'>5</button>
+      </div>
+
+      <div className='flex flex-wrap items-center justify-center p-4 gap-2'>
+        {COMMENTS.map(item => (
+          <div key={item.id} className='flex flex-col space-y-2 bg-zinc-800 w-fit text-zinc-200 items-start p-2 rounded-md justify-center'>
+            <h1 className='text-xl font-bold'>{item.name.slice(0, 20)} : </h1>
+            <h4 className='text-sm text-zinc-500 font-thin'>{item.email}</h4>
+            <h4 className='flex items-center justify-start space-x-6'>For post : <span className='text-xl font-extrabold bg-blue-900 px-2 text-white rounded-md ml-2'>{item.postId}</span> </h4>
+            <span className='bg-white text-black w-[14rem] h-[14rem] overflow-auto'>
+              <p className='whitespace-pre-wrap px-2 py-1'>{item.body}...</p>
+            </span>
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
 
