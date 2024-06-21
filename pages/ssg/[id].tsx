27f4886ede413fb4c3925b923e558d0d12fd116a
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-21 04:50:09 -0700

    SSG

--- pages/ssg/[id].tsx
+++ pages/ssg/[id].tsx
@@ -14,7 +14,7 @@ const StaticPage: NextPage<Props> = ({ product }) => {
   if (router.isFallback) { return (<h1>PLEASE WAIT</h1>) }
   function backHandler() { router.back() }
 
-  // RETURN  
+  //! RETURN  
   return (
     <div className='flex items-center justify-center h-screen flex-col space-y-6'>
       <h1 className='bg-zinc-700 text-4xl font-bold p-8 text-zinc-200'>{product.id} - {product.title.slice(0, 30)}  || {product.price}$</h1>
@@ -23,7 +23,7 @@ const StaticPage: NextPage<Props> = ({ product }) => {
   )
 }
 
-//^GET STATIC PATHS : (SSG-DYNAMIC) ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+//^GET STATIC PATHS : (SSG-DYNAMIC) 
 //THIS FUNCTIONS IS JUST LIKE {GET STATIC PROPS} -- BUT IT GET USED IN DYNAMIC ROUTES ==> [id].tsx 
 export const getStaticPaths: GetStaticPaths = async () => {
   //-> 2 - DEFINING THE ACCEPTABLE PATHS FOR DYNAMIC PAGES ...
@@ -37,7 +37,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
   }
 }
 
-//^GET STATIC PROPS (SSG) ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//^GET STATIC PROPS (SSG) 
 export const getStaticProps = async (context: GetStaticPropsContext<ParsedUrlQuery, PreviewData>) => {
   //-> 1 - FETCHING DATA BY CONTEXT PARAMS ID AND SENDING IT TO THE COMPONENT ...
   const productID = context.params?.id
