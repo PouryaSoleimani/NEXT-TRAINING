8d4734fda3b2a4514de8271cb5d4a8f4dc8bb19f
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-21 06:00:00 -0700

    SEARCH

--- pages/search/search-test.tsx
+++ pages/search/search-test.tsx
@@ -6,14 +6,14 @@ import { ChangeEvent, useEffect, useState } from 'react'
 interface Props { products: [{ id: number, title: string }] }
 
 const SearchTestPage: NextPage<Props> = ({ products }) => {
-  
+
   const [PRODUCTS, setPRODUCTS] = useState(products)
   const [search, setSearch] = useState('')
   const [isShowError, setIsShowError] = useState(false)
 
   useEffect(() => {
     const resultArray: any = PRODUCTS.filter((item) => item.title.toLowerCase().includes(search.toLowerCase()))
-    if (!resultArray.length) { setIsShowError(true) } else { setIsShowError(false) }
+    if (resultArray.length) { setIsShowError(false) } else { setIsShowError(true) }
     if (!search.length) { setPRODUCTS(products) } else { setPRODUCTS(resultArray) }
   }, [search])
 
@@ -22,7 +22,7 @@ const SearchTestPage: NextPage<Props> = ({ products }) => {
   return (
     <>
       <div className='bg-zinc-700 px-3 py-10 flex items-center justify-center'>
-        <input type="text" placeholder='search' className='p-2 font-semibold w-72 text-black outline-none' value={search} onChange={searchHandler} />
+        <input type="search" placeholder='search' className='p-2 font-semibold w-72 text-black outline-none' value={search} onChange={searchHandler} />
       </div>
       <div className='flex flex-wrap gap-4 p-10 items-center justify-center'>
         {isShowError ? (<h1 className='bg-red-200 text-red-900 font-bold text-2xl text-center rounded-md border-4 p-2 border-red-700'>NO PRODUCTS FOUND !</h1>) : (
