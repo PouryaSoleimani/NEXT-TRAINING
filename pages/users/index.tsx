18f4c271019106050c8717d4bfa0ce0c790354fb
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-21 11:27:57 -0700

    SEARCH

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -3,8 +3,8 @@ import axios from 'axios'
 import { NextPage, GetStaticProps } from 'next'
 import Link from 'next/link'
 
-interface Props { users: [{ id: number, name: string, email: string, city: string, phone: string }] }
-type SingleUserType = { id: number, name: string, email: string, city: string, phone: string }
+interface Props { users: [{ id: number, name: string, email: string, address: { city: string }, phone: string }] }
+type SingleUserType = { id: number, name: string, email: string, address: { city: string }, phone: string }
 
 //^ COMPONENT 
 const UsersPage: NextPage<Props> = ({ users }) => {
@@ -16,7 +16,7 @@ const UsersPage: NextPage<Props> = ({ users }) => {
           <Link href={`/users/${user.id}`} key={user.id}>
             <div className='text-xl font-sans border-8 border-emerald-600 bg-zinc-200 text-zinc-800 w-fit h-fit p-4 rounded-xl font-bold hover:scale-90 cursor-pointer duration-300'>
               <h1> {user.id} . {user.name}</h1>
-              <h2>FROM :{user.city}</h2>
+              <h2>FROM :{user.address.city}</h2>
               <p>EMAIL : {user.email.slice(10)}...</p>
               <p>PHONE NUMBER :{user.phone.slice(0, 10)}...</p>
             </div>
