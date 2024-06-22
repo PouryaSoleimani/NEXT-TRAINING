5c2637d56f9705ce3fc8bf952f4e13fc358ac58a
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-22 07:23:16 -0700

    API

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,6 +1,11 @@
 //! USERS ROUTE
 function usersHandler(Request, Response) {
-  return Response.json({ message: "WELCOME TO USERS ROUTE" }, { id: 1, firstName: "Pourya", lastName: "Soleimani" })
+  return Response.json({
+    message: "WELCOME TO USERS ROUTE",
+    user: { id: 1, firstName: "Pourya", lastName: "Soleimani" },
+    user2: { id: 2, firstName: "Ali", lastName: "Mahdavi" },
+    user3: { id: 3, firstName: "Mohammad", lastName: "Karimi" },
+  })
 }
 
 export default usersHandler
