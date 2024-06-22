d174aeb4d09b5dbe30ed19bf879675ffa7a14f23
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-22 11:25:05 -0700

    API

---
+++ pages/api/products/index.js
@@ -0,0 +1,25 @@
+//^ PRODUCTS ROUTE ======================================================================================================================================
+
+//!FAKE DATABASE
+const allProducts = [
+  { id: 1, title: "Watch", price: 220 },
+  { id: 2, title: "Laptop", price: 980 },
+  { id: 3, title: "Keyboard", price: 80 },
+]
+
+
+function productHandler(Req, Res) {
+  switch (Req.method) {
+    case "GET": return Res.json({ message: 'PRODUCTS ROUTE', data: allProducts })
+    case "POST": {
+      const { id, title, price } = Req.body
+      allProducts.push({ id, title, price })
+      return Res.json({ message: "PRODUCT ADDED SUCCESSFULLY !", data: allProducts })
+    }
+
+    default: return Res.json({message : "DEFAULT METHOD :)"})
+  }
+
+}
+
+export default productHandler
---
+++ pages/products/index.tsx
@@ -0,0 +1,22 @@
+//^ PRODUCTS PAGE ======================================================================================================================================
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+type SingleItemType = { id: number, title: string, price: number }
+
+//COMPONENT
+const ProductsPage = () => {
+  const [products, setProducts] = useState([])
+  useEffect(() => { axios.get("http://localhost:3000/api/products").then(response => setProducts(response.data.data)) }, [])
+
+  return (
+    <div className='flex items-center justify-center h-screen font-bold text-3xl flex-col space-y-4'>
+      {products?.map((item: SingleItemType) => (
+        <div key={item.id} className='flex flex-col items-center justify-center space-y-2 w-[20rem] bg-zinc-900 px-2 py-2 rounded-md'>
+          <h1>{item.id} - {item.title} - {item.price}</h1>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default ProductsPage
