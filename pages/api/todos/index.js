11501cba6c2a6bbae16b8e229ddefc05b548311d
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-24 12:20:40 -0700

    DATABASE

--- pages/api/todos/index.js
+++ pages/api/todos/index.js
@@ -5,11 +5,9 @@ import fs from 'fs'
 
 // FUNCTION ============================================================================================================================================ 
 function todosHandler(Req, Res) {
-
   switch (Req.method) {
-
     case "GET": {
-      const root = process.cwd() // ROOT 
+      const root = process.cwd() // ACCESSING PROJECTS ROOT FOLDER 
       const dataBasePath = path.join(root, "data", "db.json") // ACCESSING DATABASE FILE
       const bufferTypeDataBase = fs.readFileSync(dataBasePath) // READING THE FILE {db.json}
       const parsedDataBase = JSON.parse(bufferTypeDataBase) // PARSING THE FILE FROM BUFFER TO JSON
@@ -17,12 +15,10 @@ function todosHandler(Req, Res) {
 
       return Res.json({ message: "TODOS ROUTE  |  GET METHOD", todos }) // RETURNING THE {todos} IN RESPONSE
     }
-
     case "POST": { return Res.json({ message: "TODOS ROUTE  |  POST METHOD" }) }
     case "PUT": { return Res.json({ message: "TODOS ROUTE  |  PUT METHOD" }) }
     case "DELETE": { return Res.json({ message: "TODOS ROUTE  |  DELETE METHOD" }) }
     default: { return Res.json({ message: "TODOS ROUTE  |  DEFAULT METHOD" }) }
-
   }
 }
-export default todosHandler
+export default todosHandler 
