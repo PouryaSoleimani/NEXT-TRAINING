674538f0795dea61913b3eb5413c81eb4987c9b1
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-24 09:23:43 -0700

    DATABASE

--- pages/todos/index.tsx
+++ pages/todos/index.tsx
@@ -5,16 +5,17 @@ type SingleTodoType = { id: number, title: string, isCompleted: boolean }
 
 const TodosPage = () => {
   const [todos, setTodos] = useState([])
-
+  const [isCompleted, setIsCompleted] = useState(false)
   useEffect(() => {
     axios.get('/api/todos').then(response => setTodos(response.data.todos))
   }, [])
 
+  function toggleCompleted() { setIsCompleted(prev => !prev) }
 
   return (
     <div className='flex items-center justify-center flex-col h-screen text-4xl font-bold text-center space-y-6'>
       {todos.map((todo: SingleTodoType) => (
-        <h1 key={todo.id} className='bg-zinc-700 px-2 w-[17rem] py-2 rounded-lg'>{todo.title}</h1>
+        <h1 onClick={toggleCompleted} key={todo.id} className={`bg-zinc-900 px-2 w-[20rem] py-3  rounded-lg border-8 ${isCompleted ? "border-emerald-700" : "border-red-700"}`}>{todo.title}</h1>
       ))}
     </div>
   )
