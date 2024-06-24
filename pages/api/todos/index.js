c08b8362c5edda8b367621e9ad2430fed58fb08c
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-24 09:12:45 -0700

    DATABASe

--- pages/api/todos/index.js
+++ pages/api/todos/index.js
@@ -8,11 +8,16 @@ function todosHandler(Req, Res) {
   switch (Req.method) {
 
     case "GET": { return Res.json({ message: "TODOS ROUTE  |  GET METHOD" }) }
+
+
+
+
+
     case "POST": { return Res.json({ message: "TODOS ROUTE  |  POST METHOD" }) }
     case "PUT": { return Res.json({ message: "TODOS ROUTE  |  PUT METHOD" }) }
     case "DELETE": { return Res.json({ message: "TODOS ROUTE  |  DELETE METHOD" }) }
     default: { return Res.json({ message: "TODOS ROUTE  |  DEFAULT METHOD" }) }
-    
+
   }
 }
 export default todosHandler
