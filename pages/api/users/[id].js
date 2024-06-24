4a1e4a5768d3b1c7a2f50e59c804a428cfd149c9
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-24 00:42:11 -0700

    API

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -1,28 +1,23 @@
-//^ USERS DYNAMIC ROUTE ===========================================================================================================================
-//IMPORTING DATABASE
-import { users } from './index.js'
+//^ USERS DYNAMIC ROUTE ================================================================================================================================================================
+// IMPORTING DATABASE 
+import { users } from './index'
 
+function userDynamicHandler(Request, Response) {
+  const ID = Request.query.id
+  const mainUser = users.find(user => user.id == ID)
 
-// FUNCTION
-function userDynamicHandler(req, res) {
-
-  const mainUser = users.filter(user => { return user.id.toString() === req.query.id })
-
-  switch (req.method) {
+  switch (Request.method) {
 
     case "GET": {
-      if (mainUser.length) {
-        return res.json({ message: `RESPONSE => ${req.query.id}`, user: mainUser })
+      if (mainUser) {
+        return Response.json({ message: "USERS DYNAMIC ROUTE", mainUser })
       } else {
-        return res.json({ message: "404 | USER NOT FOUND" })
+        return Response.json({ message: "404 | USER NOT FOUND" })
       }
     }
 
-    default: {
-      return res.json({ message: "USERS DYNAMIC ROUTE" })
-    }
+    default: { return { message: "USERS DYNAMIC ROUTE" } }
 
   }
 }
-
 export default userDynamicHandler
