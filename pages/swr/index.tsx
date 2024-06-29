ff5b8e9c330b642935cb87eddeccc546a048270c
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-29 12:19:36 -0700

    review

--- pages/swr/index.tsx
+++ pages/swr/index.tsx
@@ -17,16 +17,12 @@ const SWR: NextPage<Props> = ({ }) => {
     <div>
       <h1 className='text-amber-700 p-2 font-bold text-4xl text-center border-b-8'>SWR PAGE</h1>
       <div className='bg-zinc-900 p-8 font-bold flex flex-col items-start justify-center space-y-42 text-2xl'>
-
         {isLoading ? <h1 className='text-sky-900'>IS LOADING</h1> : error ? (<h1 className='text-red-700 font-extrabold bg-red-400 p-6'>ERROR</h1>) : (
-
           data.map((product: { id: number, title: string, price: number }) => (
             <Link key={product.id} href={`/swr/${product.id}`} className='hover:bg-black p-2' >
               <h2> {product.id} - {product.title} || {product.price} $</h2>
             </Link>
-
           )))}
-
       </div>
     </div >
   )
