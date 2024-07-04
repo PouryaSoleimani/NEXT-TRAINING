3b009f2f5f399d70373250d2a760417defff0a6c
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-04 16:22:04 -0700

    ANT DESIGN

--- pages/ant-design/index.tsx
+++ pages/ant-design/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import { DownloadOutlined } from '@ant-design/icons';
-import { Button, Divider, Flex, Radio } from 'antd';
+import { Radio } from 'antd';
 import type { ConfigProviderProps } from 'antd';
 
 type SizeType = ConfigProviderProps[ 'componentSize' ];
@@ -14,34 +13,6 @@ const AntDesign: React.FC = () => {
         <Radio.Button value="default">Default</Radio.Button>
         <Radio.Button value="small">Small</Radio.Button>
       </Radio.Group>
-      <Divider orientation="left" plain>
-        Preview
-      </Divider>
-      <Flex gap="small" align="flex-start" vertical>
-        <Flex gap="small" wrap>
-          <Button type="primary" size={size}>
-            Primary
-          </Button>
-          <Button size={size}>Default</Button>
-          <Button type="dashed" size={size}>
-            Dashed
-          </Button>
-        </Flex>
-        <Button type="link" size={size}>
-          Link
-        </Button>
-        <Flex gap="small" wrap>
-          <Button type="primary" icon={<DownloadOutlined />} size={size} />
-          <Button type="primary" shape="circle" icon={<DownloadOutlined />} size={size} />
-          <Button type="primary" shape="round" icon={<DownloadOutlined />} size={size} />
-          <Button type="primary" shape="round" icon={<DownloadOutlined />} size={size}>
-            Download
-          </Button>
-          <Button type="primary" icon={<DownloadOutlined />} size={size}>
-            Download
-          </Button>
-        </Flex>
-      </Flex>
     </>
   );
 };
