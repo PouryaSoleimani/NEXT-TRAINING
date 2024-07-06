45e138c3740d8d651d28f353e4b3a56983eae2cd
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-06 12:04:25 -0700

    COMMENTS

--- pages/fakestore-users/[id].tsx
+++ pages/fakestore-users/[id].tsx
@@ -1,16 +1,26 @@
+import axios from 'axios'
 import { NextPage, GetServerSideProps } from 'next'
+import { useRouter } from 'next/router'
 
-interface Props { }
-
-const FakeStoreDynamicPage: NextPage<Props> = ({ }) => {
-  return <div></div>
+interface Props { user: { id: number, email: string, username: string, name: { firstname: string, lastname: string } } }
+type SingleUserType = { id: number, email: string, username: string, name: { firstname: string, lastname: string } }
+const FakeStoreDynamicPage: NextPage<Props> = ({ user }) => {
+  const router = useRouter()
+  function backHandler() { router.back() }
+  return (
+    <div className='flex items-center justify-center w-screen h-screen flex-col space-y-4'>
+      <h1 className='text-3xl font-extrabold bg-zinc-800 px-5 py-3 rounded-xl border-4 border-orange-500'>{user.id}  - {user.name.firstname.toUpperCase()} {user.name.lastname.toUpperCase()}</h1>
+      <button className='bg-green-600 px-3 text-3xl font-extrabold rounded-md py-2' onClick={backHandler}>BACK</button>
+    </div>
+  )
 }
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const id = ctx.query.id
-  
+  const req = axios.get(`https://fakestoreapi.com/users/${id}`)
+  const user = (await req).data
   return {
-    props: {}
+    props: { user }
   }
 }
 
