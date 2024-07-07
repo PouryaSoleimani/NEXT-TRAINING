a1e696931b40e2c4bf2f514a032aae1cab60a220
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-07 09:41:57 -0700

    SETTINGS

--- pages/courses/nodejs.tsx
+++ pages/courses/nodejs.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const nodejs: React.FC = () => {
- return (
-  <div className='font-bold h-screen bg-lime-600 flex items-center justify-center text-9xl'>NODE-JS COURSE PAGE</div>
- )
+    return (
+        <div className='font-bold h-screen bg-lime-600 flex items-center justify-center text-9xl'>NODE-JS COURSE PAGE</div>
+    )
 }
 
 export default nodejs
