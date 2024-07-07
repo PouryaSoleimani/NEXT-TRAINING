600460fb1ff0a6bee6edc5849f638f4bcb293507
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-07 12:28:17 -0700

    SETTINGS

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,8 @@ export default function Home() {
 
   return (
     <>
-      <main className={`flex min-h-screen flex-col items-center justify-center p-24`}>
+      <main className={`flex min-h-screen flex-col items-center justify-start space-y-10 p-24`}>
+        <object data="/favicon.ico" width={50} height={50} className="border rounded-xl hover:scale-75 duration-300 "></object>
         <h1 className="py-10 px-10 bg-zinc-900 text-zinc-300 rounded-lg text-4xl font-sans font-bold hover:scale-90 duration-300 cursor-pointer">NEXT TRAINIG HOME PAGE</h1>
         <ul className="bg-zinc-700 w-screen box-border p-10 mt-10 border-2 border-zinc-600 flex flex-wrap items-center justify-center space-x-2">
           <Link href='/1/products' className="bg-lime-500 text-2xl px-2 py-1 rounded-xl text-black font-bold border-[10px] border-zinc-950" >DYNAMIC</Link>
@@ -24,7 +25,6 @@ export default function Home() {
           <Link href='/comments' className="bg-orange-400 text-2xl px-2 py-1 rounded-xl text-black font-bold border-[10px] border-zinc-950">COMMENTS</Link>
           <Link href='/form' className="bg-cyan-600 text-2xl px-2 py-1 rounded-xl text-black font-bold border-[10px] border-zinc-950">FORM</Link>
         </ul>
-        <object data="/favicon.ico" width={50} height={50} className="border rounded-xl hover:scale-75 duration-300 "></object>
       </main>
     </>
   );
