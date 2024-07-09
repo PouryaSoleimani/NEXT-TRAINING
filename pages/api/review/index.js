7e1185cdbed0a8e5f80a49003cd7b78773361cac
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-09 09:48:25 -0700

    CONFIG

--- pages/api/review/index.js
+++ pages/api/review/index.js
@@ -1,11 +1,13 @@
 //^ REVIEW API ROUTE ============================================================================================================
 function reviewHandler(req, res) {
     switch (req.method) {
+
         case "GET": { return res.json({ message: "REVIEW GET MODE" }) }
         case "POST": { return res.json({ message: "REVIEW POST MODE" }) }
         case "PUT": { return res.json({ message: "REVIEW PUT MODE" }) }
         case "DELETE": { return res.json({ message: "REVIEW DELETE MODE" }) }
         default: { return res.json({ message: "REVIEW DEFAULT MODE" }) }
+        
     }
 }
 export default reviewHandler
