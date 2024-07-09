75853a0dc3f5ae19830be2a2b99dc36ddc4a99c1
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-09 20:37:02 +0330

    CONFIG

--- pages/api/review/requestBody.js
+++ pages/api/review/requestBody.js
@@ -4,12 +4,15 @@ const usersList = [
 ]
 function postHandler(request, response) {
     switch (request.method) {
+
         case "GET": { return response.json({ message: "REVIEW ROUTE || GET METHOD", usersList }) }
+
         case "POST": {
             const user = request.body
             usersList.push(user)
             return response.json({ message: "REVIEW ROUTE || POST METHOD", user: user, usersList: usersList })
         }
+
         case "PUT": { return response.json({ message: "REVIEW ROUTE || PUT METHOD" }) }
         case "DELETE": { return response.json({ message: "REVIEW ROUTE || DELETE METHOD" }) }
         default: { return response.json({ message: "REVIEW ROUTE || DEFAULT METHOD" }) }
