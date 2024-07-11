ad176c1717f31a2aaf412bfa4719701507e4b5e7
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-11 18:53:24 +0330

    API

--- pages/api/review/requestBody.js
+++ pages/api/review/requestBody.js
@@ -13,23 +13,16 @@ function postHandler(request, response) {
 
     switch (request.method) {
 
-        case "GET":
-            { return response.json({ message: "REVIEW ROUTE || GET METHOD", usersList }) }
+        case "GET": { return response.json({ message: "REVIEW ROUTE || GET METHOD", usersList }) }
 
-        case "POST":
-            {
-                const user = request.body;
-                usersList.push(user);
-                return response.json({ message: "REVIEW ROUTE || POST METHOD", user: user, usersList: usersList })
-            };
-
-        case "PUT":
-            { return response.json({ message: "REVIEW ROUTE || PUT METHOD" }) }
-        case "DELETE":
-            { return response.json({ message: "REVIEW ROUTE || DELETE METHOD" }) }
-        default:
-            { return response.json({ message: "REVIEW ROUTE || DEFAULT METHOD" }) }
-
+        case "POST": {
+            const user = request.body;
+            usersList.push(user);
+            return response.json({ message: "REVIEW ROUTE || POST METHOD", user: user, usersList: usersList })
+        }
+        case "PUT": { return response.json({ message: "REVIEW ROUTE || PUT METHOD" }) }
+        case "DELETE": { return response.json({ message: "REVIEW ROUTE || DELETE METHOD" }) }
+        default: { return response.json({ message: "REVIEW ROUTE || DEFAULT METHOD" }) }
     }
 }
 
