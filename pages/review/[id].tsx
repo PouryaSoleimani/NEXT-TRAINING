15b4e4ef94c5ab89df75206cb11a19ce77cbe2b4
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-11 21:07:11 +0330

    API

--- pages/review/[id].tsx
+++ pages/review/[id].tsx
@@ -1,16 +1,23 @@
 //^ DYNAMIC PAGE SINGLE
+import axios from 'axios'
 import { NextPage, GetStaticPaths, GetStaticProps } from 'next'
 
-interface Props { }
+interface Props { Mainuser: { id: number, username: string } }
 
 // COMPONENT
-const DynamicApiSingle: NextPage<Props> = ({ }) => {
-    return <div></div>
+const DynamicApiSingle: NextPage<Props> = ({ Mainuser }) => {
+    return (
+        <section className='flex items-center justify-center h-screen w-screen'>
+            <div className='flex items-center justify-center'>
+                <h1>{Mainuser.username}</h1>
+            </div>
+        </section>
+    )
 }
 
 
 
-//STATIC PATHS
+//^ GET STATIC PATHS
 export const getStaticPaths: GetStaticPaths = async () => {
     return {
         paths: [],
@@ -18,10 +25,14 @@ export const getStaticPaths: GetStaticPaths = async () => {
     }
 }
 
-//STATIC PROPS
+//^ GET STATIC PROPS
 export const getStaticProps: GetStaticProps = async (ctx) => {
+    const ID: number = Number(ctx.params?.id)
+    const request = axios.get(`http://localhost:api/review/${ID}`)
+    const Mainuser = (await request).data.mainUser
+
     return {
-        props: {},
+        props: { Mainuser },
     }
 }
 export default DynamicApiSingle
