46ce9dc10b1b4372c3000c3ce1f10ca2c0701c63
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-11 21:23:24 +0330

    API

--- pages/review/[id].tsx
+++ pages/review/[id].tsx
@@ -1,17 +1,29 @@
 //^ DYNAMIC PAGE SINGLE
 import axios from 'axios'
 import { NextPage, GetStaticPaths, GetStaticProps } from 'next'
+import { useRouter } from 'next/router'
 interface Props { Mainuser: { id: number, username: string } }
 
 // COMPONENT
 const DynamicApiSingle: NextPage<Props> = ({ Mainuser }) => {
+    const router = useRouter()
+    function backHandler() { router.back() }
 
     return (
-        <section className='flex items-center justify-center h-screen w-screen'>
-            <div className='flex items-center justify-center'>
-                <h1 className='text-9xl uppercase font-extrabold'>{Mainuser.id} - {Mainuser.username}</h1>
-            </div>
-        </section>
+        <section className='flex items-center justify-center h-screen w-screen' >
+            {Mainuser ? (
+
+                <div className='flex items-center justify-center'>
+                    <h1 className='text-9xl uppercase font-extrabold'>{Mainuser.id} - {Mainuser.username}</h1>
+                </div>
+
+            ) : (
+                <div>
+                    <h1 className='text-9xl uppercase font-extrabold text-orange-500'>User Not Found</h1>
+                    <button onClick={backHandler} className='px-6 py-2 bg-sky-600 rounded-xl'>← BACK </button>
+                </div>
+            )}
+        </section >
     )
 }
 
@@ -34,9 +46,6 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
     const ID = ctx.params?.id
     const request = axios.get(`http://localhost:3000/api/review/${ID}`)
     const Mainuser = (await request).data.mainUser
-
-    return {
-        props: { Mainuser },
-    }
+    return { props: { Mainuser }, }
 }
 export default DynamicApiSingle
