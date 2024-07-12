623ba3d6117282df52e86173a9db689f3b5fe4b5
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-12 14:26:57 +0330

    API

--- pages/api/review/database.js
+++ pages/api/review/database.js
@@ -4,7 +4,15 @@ import path from 'path'
 
 function databaseHandler(request, response) {
     const root = process.cwd()
-    
+    const database = path.join(root, "db", "db.json")
+    const DB = fs.readFileSync(database)
+    console.log(DB)
+
+    switch (request.method) {
+        case "GET": { return response.json({ message: "FS AND PATH" }) }
+        default: { return response.json({ message: "FS AND PATH" }) }
+
+    }
 }
 
 export default databaseHandler
