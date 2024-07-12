3f95ba004452e69a28b3fe62cecadb626404fd7a
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-12 14:18:28 +0330

    API

--- pages/api/review/database.js
+++ pages/api/review/database.js
@@ -1,3 +1,7 @@
+//^ FS AND PATH ROUTE ===========================================================================================================
+import path from 'path'
+import fs from 'fs'
+
 function databaseHandler(request, response) {
     console.log(request, response)
 }
