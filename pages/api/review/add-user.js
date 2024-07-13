5b5abaa27bcc1caef13ba8720ecc6e09d32323eb
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-13 20:24:13 +0330

    ADD USER API

--- pages/api/review/add-user.js
+++ pages/api/review/add-user.js
@@ -18,9 +18,8 @@ const addUserHandler = (req, res) => {
         case "PUT": {
             const { email, password } = req.body
             const newUser = { id: crypto.randomUUID(), email, password }
-            USERS.push(newUser)
             const error = fs.writeFileSync(dbPath, JSON.stringify(newUser))
-            if (error) { return res.status(500).json({ message: "INVALID PARAMETERS" }) }
+            if (!error) { return res.status(500).json({ message: "INVALID PARAMETERS" }) }
             else { return res.status(201).json({ message: "USER CREATED SUCCESSFULLY", USERS }) }
         }
         default: { return res.json({ message: "DEFAULT METHOD" }) }
@@ -29,10 +28,3 @@ const addUserHandler = (req, res) => {
 }
 export default addUserHandler
 
-// crypto.randomUUID()
-
-// fs.writeFileSync( dbPath , newUser)
-
-// const err = fs.writeFileSync( dbPath , JSON.stringify(newUser) )
-
-// if(err) = {console.log(err)}
