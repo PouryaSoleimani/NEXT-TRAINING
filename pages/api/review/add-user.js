343036eb78e2f56a6a0e75a38a37013933f47720
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-13 20:11:38 +0330

    ADD USER API

--- pages/api/review/add-user.js
+++ pages/api/review/add-user.js
@@ -6,15 +6,16 @@ import fs from 'fs'
 
 //===============================================================================================================================================
 const addUserHandler = (req, res) => {
+    //ACCESSING THE DATABASE
     const root = process.cwd()
     const dbPath = path.join(root, "data", "db.json")
     const dbBuffer = fs.readFileSync(dbPath)
     const dbParsed = JSON.parse(dbBuffer)
     const USERS = dbParsed.users
-
+    // SWITCH CASE
     switch (req.method) {
         case "GET": { return res.json({ message: "ADD USER GET METHOD", users: USERS }) }
-        case "PUT" : {}
+        case "PUT": { }
         default: { return res.json({ message: "DEFAULT METHOD" }) }
     }
 
