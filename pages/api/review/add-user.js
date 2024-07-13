5b84fa445cbecf748131ea30a7b21bb173691ca1
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-13 20:06:38 +0330

    FS AND PATH

--- pages/api/review/add-user.js
+++ pages/api/review/add-user.js
@@ -1,1 +1,15 @@
 //^ ADD USER PAGE ================================================================================================================================
+import path from "path"
+import fs from 'fs'
+
+
+
+//
+const addUserHandler = (req, res) => {
+    const root = process.cwd()
+    const dbPath = path.join(root, "data", "db.json")
+    const dbBuffer = fs.readFileSync(dbPath)
+    const dbParsed = JSON.parse(dbBuffer)
+    
+
+}
