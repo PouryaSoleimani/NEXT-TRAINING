20af0b46262b11f91d51a55432bbd86ed7d14c31
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-15 15:17:17 +0330

    API

--- pages/api/review/deleteuser/[deleteID].js
+++ pages/api/review/deleteuser/[deleteID].js
@@ -5,13 +5,13 @@ import path from 'path'
 function deleteUserHandler(request, response) {
     switch (request.method) {
         case "DELETE": {
-            //*ACCESSING DATABASE
+            //ACCESSING DATABASE
             const root = process.cwd()
             const dbPath = path.join(root, "data", "db.json")
             const dbBuffered = fs.readFileSync(dbPath)
             const dbParsed = JSON.parse(dbBuffered.toString())
             const USERSLIST = dbParsed.users
-            //* DELETE USER
+            //DELETE USER
             const { deleteID } = request.query
             const MAINUSER = USERSLIST.find(user => user.id == String(deleteID))
             if (MAINUSER) {
