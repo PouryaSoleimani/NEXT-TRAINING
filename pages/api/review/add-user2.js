73fc209640163ca788eb02f62164f9aa7d61c13f
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-15 14:37:41 +0330

    API

--- pages/api/review/add-user2.js
+++ pages/api/review/add-user2.js
@@ -7,12 +7,24 @@ function addUserHandler2(request, response) {
     const dbPath = path.join(root, "data", "db.json")
     const dbBuffered = fs.readFileSync(dbPath)
     const dbParsed = JSON.parse(dbBuffered.toString())
-    const usersList = dbParsed.users
+    const USERSLIST = dbParsed.users
 
     switch (request.method) {
-        case "GET": { return response.status(200).json({ message: "ADD USER 2 || GET METHOD", data: usersList }) }
-        case "POST" : {
-            
+        case "GET": { return response.status(200).json({ message: "ADD USER 2 || GET METHOD", data: USERSLIST }) }
+        case "POST": {
+            const { username, password } = request.body
+
+            const newUser = { id: crypto.randomUUID(), username, password }
+
+            USERSLIST.push(newUser)
+
+            const ERROR = fs.writeFileSync(dbPath, JSON.stringify(dbParsed, null, 2))
+
+            if (ERROR) {
+                response.status(500).json({ message: "INVALID PARAMETERS ..." })
+            } else {
+                response.status(201).json({ message: "USER CREATED", data: USERSLIST })
+            }
         }
         default: { return response.status(200).json({ message: "ADD USER 2 || DEFAULT METHOD" }) }
     }
