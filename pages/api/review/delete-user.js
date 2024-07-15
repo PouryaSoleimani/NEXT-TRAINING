c818362104ee38e3bd201a8c6e55f0b4da8d7cbe
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-15 14:56:33 +0330

    API

--- pages/api/review/delete-user.js
+++ pages/api/review/delete-user.js
@@ -1,1 +1,8 @@
 //! DELETE USER API ROUTE =========================================================================================================
+import fs from 'fs'
+import path from 'path'
+
+function deleteUserHandler(request, response) {
+    
+
+}
