16c4a9df0ab1c8243d0e215674448ae340aadfef
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-15 15:16:36 +0330

    API

--- pages/api/review/deleteuser/[deleteID].js
+++ pages/api/review/deleteuser/[deleteID].js
@@ -5,21 +5,21 @@ import path from 'path'
 function deleteUserHandler(request, response) {
     switch (request.method) {
         case "DELETE": {
-            //ACCESSING DATABASE
+            //*ACCESSING DATABASE
             const root = process.cwd()
             const dbPath = path.join(root, "data", "db.json")
             const dbBuffered = fs.readFileSync(dbPath)
             const dbParsed = JSON.parse(dbBuffered.toString())
             const USERSLIST = dbParsed.users
-            // DELETE USER
-            const { id } = request.query
-            return response.status(200).json({ id : request.query })
-            //     const MAINUSER = USERSLIST.find(user => String(user.id) !== String(id))
-            //     if (MAINUSER) {
-            //         return response.status(200).json({ message: "DELETE USER", data: MAINUSER })
-            //     } else {
-            //         return response.status(404).json({ message: "USER NOT FOUND" })
-            //     }
+            //* DELETE USER
+            const { deleteID } = request.query
+            const MAINUSER = USERSLIST.find(user => user.id == String(deleteID))
+            if (MAINUSER) {
+                return response.status(200).json({ MAINUSER })
+            } else {
+                return response.status(404).json({ message: "USER NOT FOUND" })
+            }
+
         }
         default: { return response.json({ message: "DELETE USERS DYNAMIC ROUTE" }) }
     }
