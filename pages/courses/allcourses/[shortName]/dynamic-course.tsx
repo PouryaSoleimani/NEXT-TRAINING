8b242eac15daa9899fb14fd0a6d9e217028a1699
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-10-11 16:02:07 +0330

    REVIEW

--- pages/courses/allcourses/[shortName]/dynamic-course.tsx
+++ pages/courses/allcourses/[shortName]/dynamic-course.tsx
@@ -1,9 +1,16 @@
+import { useRouter } from 'next/router'
 import React from 'react'
 
+
+
+
+//^ COMPONENT
 const DynamicCourseRoute = () => {
+    const router = useRouter()
     return (
-        <div>
+        <div className='flex items-center justify-center w-screen h-screen flex-col space-y-10'>
             <h1 className='text-6xl font-extrabold'>DynamicCourseRoute</h1>
+            <p className='text-6xl font-extrabold bg-white text-black px-8 py-4 rounded-2xl'>COURSE ID :{router.query.shortName?.toString().toLocaleUpperCase()}</p>
         </div>
     )
 }
