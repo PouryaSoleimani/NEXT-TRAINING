d885b22f353e638d2f6bae36bbd85a9cb700b265
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-10-11 20:27:25 +0330

    REVIEW

--- pages/404.tsx
+++ pages/404.tsx
@@ -10,7 +10,7 @@ const NotFound = () => {
 
   return (
     <div className='flex flex-col space-y-2 items-center justify-center h-screen font-extrabold tracking-tighter text-3xl'>
-      <h1 className='bg-zinc-900/50 backdrop-blur-3xl p-4 rounded-md border-2 border-white tracking-widest'>404 ERROR | Not Found</h1>
+      <h1 className='bg-zinc-900/50 border-4 border-red-900 backdrop-blur-3xl p-4 rounded-md tracking-widest'>404 ERROR | Not Found</h1>
       <button onClick={backHandler} className='bg-zinc-900/80 text-2xl p-2 hover:bg-zinc-900 hover:border font-bold rounded-md'>⬅ BACK</button>
     </div>
   )
