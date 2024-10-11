86de235ffbadedb4deaecf7a9c9341b5c6dca0a5
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-10-11 14:30:34 +0330

    REVIEW

---
+++ pages/users/courses.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserCourses = () => {
+    return (
+        <h1 className='text-6xl font-extrabold'>USER REGISTERED COURSES</h1>
+    )
+}
+
+export default UserCourses
