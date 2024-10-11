6959b35868152f71d420bc86a0b0c4ccf726f0c6
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-10-11 20:58:00 +0330

    REVIEW

--- pages/slugs/index.tsx
+++ pages/slugs/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Slug = () => {
     return (
         <div className='w-screen h-screen flex items-center justify-center'>
-            <h1 className='bg-slate-900 text-slate-200 p-7 text-6xl font-bold'>SLUGS PAGE</h1>
+            <h1 className='bg-slate-900 text-slate-200 p-7 text-6xl font-bold'>SLUGS HOME PAGE</h1>
         </div>
     )
 }
