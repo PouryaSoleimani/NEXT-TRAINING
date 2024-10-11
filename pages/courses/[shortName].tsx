70b5c7d6fc4140c85266aa404fa1108c6e8645f0
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-10-11 15:00:06 +0330

    REVIEW

--- pages/courses/[shortName].tsx
+++ pages/courses/[shortName].tsx
@@ -6,9 +6,9 @@ import React from 'react'
 const SingleProductPage = () => {
     const router = useRouter()
     return (
-        <div>
-            <h1>SingleProductPage</h1>
-            <h2>Course Name : {router.query.shortName?.toString().toUpperCase()} </h2>
+        <div className='w-screen h-screen flex items-center justify-center flex-col space-y-10'>
+            <h1 className='text-5xl font-extrabold bg-blue-800 px-10 py-5 rounded-xl'>SingleProductPage</h1>
+            <h2 className='text-4xl font-extrabold bg-red-900 px-10 py-5 rounded-xl'>Course Name : {router.query.shortName?.toString().toUpperCase()} </h2>
         </div>
     )
 }
