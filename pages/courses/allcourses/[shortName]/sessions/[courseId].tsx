8fc1e1204a49904f46296dcf949a7239a514333f
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-10-11 18:48:52 +0330

    REVIEW

--- pages/courses/allcourses/[shortName]/sessions/[courseId].tsx
+++ pages/courses/allcourses/[shortName]/sessions/[courseId].tsx
@@ -12,8 +12,9 @@ const SingleCoursePage = () => {
     return (
         <div className='w-screen h-screen flex items-center justify-center flex-col'>
             <h1 className='text-6xl font-extrabold'>SingleCoursePage</h1>
-            <p className='text-3xl font-extrabold my-10 bg-zinc-900 px-10 py-5 rounded-xl'>SESSION NUMBER : {router.query.courseId?.toString().toUpperCase()}</p>
-            <Link href="/" className='bg-white text-black p-1 rounded-md hover:bg-emerald-900 hover:text-white duration-300'><Home className='w-10 h-10' /></Link>
+            <p className='text-3xl font-extrabold mt-10 bg-zinc-900 px-10 py-5 rounded-xl'>COURSE NAME : {router.query.shortName?.toString().toUpperCase()}</p>
+            <p className='text-3xl font-extrabold my-4 bg-zinc-900 px-10 py-5 rounded-xl'>SESSION NUMBER : {router.query.courseId?.toString().toUpperCase()}</p>
+            <Link href="/" className='bg-white text-black px-10 py-2 rounded-md hover:bg-emerald-900 hover:text-white duration-300'><Home className='w-10 h-10' /></Link>
         </div>
     )
 }
