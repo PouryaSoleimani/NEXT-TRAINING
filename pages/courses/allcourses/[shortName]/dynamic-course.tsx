594da6c28b863382bfa4ae58ddb5c51c86d81b06
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-10-11 16:45:35 +0330

    REVIEW

--- pages/courses/allcourses/[shortName]/dynamic-course.tsx
+++ pages/courses/allcourses/[shortName]/dynamic-course.tsx
@@ -7,6 +7,7 @@ import React from 'react'
 //^ COMPONENT
 const DynamicCourseRoute = () => {
     const router = useRouter()
+    console.log(router.query)
     return (
         <div className='flex items-center justify-center w-screen h-screen flex-col space-y-10'>
             <h1 className='text-6xl font-extrabold'>DynamicCourseRoute</h1>
