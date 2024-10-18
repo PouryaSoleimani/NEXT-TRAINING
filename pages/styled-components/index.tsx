5c188829064251809d92587b37f683bf76d379cb
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-10-18 15:27:50 +0330

    REVIEW ____ SLUG

--- pages/styled-components/index.tsx
+++ pages/styled-components/index.tsx
@@ -6,13 +6,15 @@ import styled from 'styled-components'
 const StyleComponentsPage = () => {
 
     const ButtonFilled = styled.button`
-    background-color : darkgreen ;
-    border : 5px solid darkgreen ;
+    background-color : darkred ;
+    border : 5px solid darkred ;
     border-radius : 15px;
     font-size : 25px;
     padding : 1rem 3rem;
     color : white;
     font-weight : 900;
+    width : 150px;
+    transition : all 300ms linear ;
     `
     const ButtonOutline = styled.button`
     background-color : transparent ;
@@ -22,6 +24,8 @@ const StyleComponentsPage = () => {
     padding : 1rem 3rem;
     color : white;
     font-weight : 900;
+    width : 150px;
+    transition : all 300ms linear ;
     `
     return (
         <>
@@ -29,8 +33,8 @@ const StyleComponentsPage = () => {
                 <h1 className='bg-zinc-800 text-pink-500 font-extrabold text-6xl text-center py-5'>STYLED COMPONENTS PAGE</h1>
             </div>
             <div className='flex p-10 items-center justify-center space-x-10'>
-                <ButtonFilled className='hover:bg-emerald-600 hover:border-emerald-600'>ACCEPT</ButtonFilled>
-                <ButtonOutline className='hover:bg-red-800 hover:border-red-800'>REJECT</ButtonOutline>
+                <ButtonFilled className='hover:bg-red-700 hover:border-red-700'>Yes</ButtonFilled>
+                <ButtonOutline className='hover:border-red-700'>No</ButtonOutline>
             </div>
         </>
     )
