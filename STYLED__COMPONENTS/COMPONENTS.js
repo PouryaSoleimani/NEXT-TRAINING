bdf8685a4762571bf3672686f7dd686be296e0da
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-10-18 16:13:55 +0330

    STYLED COMPONENTS

--- STYLED__COMPONENTS/COMPONENTS.js
+++ STYLED__COMPONENTS/COMPONENTS.js
@@ -7,12 +7,13 @@ width : 150px;
 padding : 1rem;
 border-radius : 20px ;
 color : black ;
-border: 5px solid black;
+border: 5px solid #FABB14;
 font-size : 26px;
 font-weight : 900;
 font-family : LALEZAR;
 box-sizing : border-box;
 box-shadow : 1px 1px 1px #2c2c2c;
+transition : all 300ms linear
 `
 
 export const StyledOutLineButton = styled.button`
@@ -27,4 +28,5 @@ font-weight : 900;
 font-family : LALEZAR;
 box-sizing : border-box;
 box-shadow : 1px 1px 1px #2c2c2c;
+transition : all 300ms linear
 `
