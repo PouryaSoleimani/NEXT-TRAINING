221a6f17a3e319b0e1b519fee4be5cef7b4f6942
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-10-18 16:22:45 +0330

    STYLED COMPONENTS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 // ^ APP.JS FILE =====================================================================================================================================
 import "@/styles/globals.css";
+// import 'bootstrap/dist/css/bootstrap.css'
 import type { AppProps } from "next/app";
 
 
 export default function App({ Component, pageProps }: AppProps) {
   return (<Component {...pageProps} />);
 }
+ 
---
+++ pages/bootstrap/index.tsx
@@ -0,0 +1,17 @@
+//~ BOOTSTRAP TRAINING PAGE =====================================================================================================================
+import React from 'react'
+
+const BootstrapTrainingPage = () => {
+    return (
+        <section className='bg-black'>
+            <div>
+                <h1 className='bg-violet-800 text-white p-10 text-6xl font-extrabold text-center'>BootstrapTrainingPage</h1>
+            </div>
+            <div>
+
+            </div>
+        </section>
+    )
+}
+
+export default BootstrapTrainingPage
