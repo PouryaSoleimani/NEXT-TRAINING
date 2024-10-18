435b4d59f33b0074301768e3be90ca7d446fc9b0
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-10-18 17:34:39 +0330

    STYLED COMPONENTS

--- pages/bootstrap/index.tsx
+++ pages/bootstrap/index.tsx
@@ -3,12 +3,18 @@ import React from 'react'
 
 const BootstrapTrainingPage = () => {
     return (
-        <section className='bg-black'>
+        <section>
             <div>
                 <h1 className='bg-violet-800 text-white p-10 text-6xl font-extrabold text-center capitalize'>BOOTSTRAP TRAINING PAGE</h1>
             </div>
-            <div>
-
+            <div className='p-10'>
+                <ul>
+                    <li className='alert alert-primary'>FIRST</li>
+                    <li className='alert alert-secondary'>SECOND</li>
+                    <li className='alert alert-warning'>THIRD</li>
+                    <li className='alert alert-danger'>FOURTH</li>
+                    <li className='alert alert-success'>FIFTH</li>
+                </ul>
             </div>
         </section>
     )
