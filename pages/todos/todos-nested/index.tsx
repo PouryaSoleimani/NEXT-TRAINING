500504276b1827b839295694fbcd6a7d6280d3e6
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-01-31 12:57:53 +0330

    NEXT TRAINING

---
+++ pages/todos/todos-nested/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TodosNestedPage = () => {
+  return (
+    <div className='w-screen h-screen flex items-center justify-center'>
+      <h1 className='font-black text-7xl text-white bg-zinc-800 p-3 rounded-xl'>  Todos Nested Page </h1>
+    </div>
+  )
+}
+
+export default TodosNestedPage
