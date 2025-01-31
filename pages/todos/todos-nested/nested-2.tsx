3ef701c93eb90d641d8614140991c20baa9a75bd
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-01-31 13:04:41 +0330

    NEXT TRAINING

--- pages/todos/todos-nested/nested-2.tsx
+++ pages/todos/todos-nested/nested-2.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Nested2Page = () => {
     return (
         <div className='w-screen h-screen flex items-center justify-center'>
-            <h1 className='text-7xl text-red-900 font-black bg-zinc-500 p-4 rounded-xl'>NESTED 2 TODO PAGE</h1>
+            <h1 className='text-7xl text-red-800 font-black bg-zinc-200 p-4 rounded-xl'>NESTED 2 TODO PAGE</h1>
         </div>
     )
 }
