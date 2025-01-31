48fff45309870d872c836a32bd73bad4d6112f61
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-01-31 13:17:53 +0330

    NEXT TRAINING

--- pages/todos/[todoID]/index.tsx
+++ pages/todos/[todoID]/index.tsx
@@ -7,7 +7,7 @@ const TodoDynamicPage = () => {
     return (
         <div className='w-sceen h-screen flex flex-col gap-y-10 items-center justify-center'>
             <h1 className='text-6xl font-bold text-white'>Todos Dynamic Page</h1>
-            <h2 className='text-6xl text-red-700 font-black bg-zinc-700 py-5 px-32 rounded-xl'>{todoID}</h2>
+            <h2 className='text-6xl text-red-700 font-black bg-zinc-700 py-5 px-32 rounded-xl tracking-widest'>{todoID}</h2>
         </div>
     )
 }
