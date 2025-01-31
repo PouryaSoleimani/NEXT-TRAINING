9b014061d59d481f30d2f55001c5c7d27ff82ba6
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-01-31 13:14:18 +0330

    NEXT TRAINING

--- pages/todos/[todoID]/index.tsx
+++ pages/todos/[todoID]/index.tsx
@@ -1,11 +1,13 @@
+import { useRouter } from 'next/router'
 import React from 'react'
 
 const TodoDynamicPage = () => {
-
+    const router = useRouter()
+    const { todoID } = router.query
     return (
-        <div className='w-sceen h-screen flex items-center justify-center'>
+        <div className='w-sceen h-screen flex flex-col gap-y-10 items-center justify-center'>
             <h1 className='text-6xl font-bold text-white'>Todos Dynamic Page</h1>
-            <h2></h2>
+            <h2 className='text-6xl text-red-700 font-black bg-zinc-700 py-5 px-32 rounded-xl'>{todoID}</h2>
         </div>
     )
 }
