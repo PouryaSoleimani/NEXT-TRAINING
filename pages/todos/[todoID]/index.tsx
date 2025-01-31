a4922a41dde237bb9a469088fb10e0ab5154d9d2
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-01-31 13:08:41 +0330

    NEXT TRAINING

---
+++ pages/todos/[todoID]/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TodoDynamicPage = () => {
+
+    return (
+        <div className='w-sceen h-screen flex items-center justify-center'>
+            <h1 className='text-6xl font-bold text-white'>Todos Dynamic Page</h1>
+            <h2></h2>
+        </div>
+    )
+}
+
+export default TodoDynamicPage
