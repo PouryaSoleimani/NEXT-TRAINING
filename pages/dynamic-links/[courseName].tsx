f0c7dba406d5b37ffe382e713f74cd22a9464440
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-02-07 18:48:58 +0330

    ⬛⬛⬛ NEXT TRAINING ⬛⬛⬛

--- pages/dynamic-links/[courseName].tsx
+++ pages/dynamic-links/[courseName].tsx
@@ -7,7 +7,7 @@ const SingleCoursePage = () => {
     const { courseName } = router.query
     return (
         <div className='w-screen h-screen flex flex-col gap-y-10 mt-10 items-center juftify-center bg-black text-white'>
-            <h1 className='mt-16'> WELCOME TO <span className='bg-green-900 text-white font-black text-5xl px-10 text-center rounded-3xl border-8 border-white box-border'>  {courseName?.toLocaleString().toUpperCase().trim()}  √</span> COURSE</h1>
+            <h1 className='mt-16'> WELCOME TO <span className='bg-yellow-400 text-black font-black text-5xl px-10 text-center rounded-3xl border-8 border-zinc-700 box-border'>  {courseName?.toLocaleString().toUpperCase().trim()}  √</span> COURSE</h1>
         </div>
     )
 }
--- pages/dynamic-links/index.tsx
+++ pages/dynamic-links/index.tsx
@@ -10,12 +10,13 @@ const DynamicLinksPage = () => {
         { id: 1, title: 'MONGODB', link: '/dynamic-links/mongodb' },
         { id: 1, title: 'EXPRESS', link: '/dynamic-links/express' },
     ]
+
     return (
         <div className='w-screen h-screen flex flex-col gap-y-10 mt-10 items-center juftify-center bg-black text-white px-10'>
             <h1 className='text-5xl font-black'>Dynamic Links Page</h1>
             <ul className='w-[80%] flex flex-wrap gap-x-10 gap-y-10 justify-center items-center bg-zinc-900 p-10 border-2 border-zinc-600 rounded-2xl'>
                 {courses.map(course => (
-                    <li key={course.id} className='bg-zinc-700 no-underline text-white p-4 text-3xl font-bold rounded-xl hover:scale-105 duration-300'>
+                    <li key={course.id} className='bg-zinc-700 no-underline text-white p-4 text-3xl font-bold rounded-xl hover:scale-105 hover:bg-yellow-500 duration-300'>
                         <Link href={course.link} className='no-underline text-white font-bold'>{course.title}</Link>
                     </li>
                 ))}
