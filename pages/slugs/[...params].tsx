3c61e4083a97ad945c9d220ad725a02d469ddc21
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-02-07 23:19:54 +0330

    ⬛⬛⬛ NEXT TRAINING ⬛⬛⬛

--- pages/slugs/[...params].tsx
+++ pages/slugs/[...params].tsx
@@ -1,4 +1,4 @@
-//^SLUG PAGE
+//^SLUG PAGE =======================================================================================================================================================
 import { useRouter } from 'next/router'
 import React from 'react'
 import styled from 'styled-components';
