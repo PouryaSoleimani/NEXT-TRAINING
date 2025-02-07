d8b1bb7fc561745654e5542155941e9860e4c414
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-02-07 19:13:59 +0330

    ⬛⬛⬛ NEXT TRAINING ⬛⬛⬛

--- pages/dynamic-links/[courseName].tsx
+++ pages/dynamic-links/[courseName].tsx
@@ -5,9 +5,13 @@ const SingleCoursePage = () => {
 
     const router = useRouter()
     const { courseName } = router.query
+    function backHandler(): void { router.back() }
+
+
     return (
-        <div className='w-screen h-screen flex flex-col gap-y-10 mt-10 items-center juftify-center bg-black text-white'>
+        <div className='w-screen h-screen flex flex-col gap-y-10 mt-10 items-center juftify-start bg-black text-white'>
             <h1 className='mt-16'> WELCOME TO <span className='bg-yellow-400 text-black font-black text-5xl px-10 text-center rounded-3xl border-8 border-zinc-700 box-border'>  {courseName?.toLocaleString().toUpperCase().trim()}  √</span> COURSE</h1>
+            <button onClick={backHandler} className='bg-yellow-400 text-black font-black text-4xl px-4 py-2 rounded-xl hover:text-white duration-300'>BACK</button>
         </div>
     )
 }
