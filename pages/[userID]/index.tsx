b7d3614a657d8aada25ccba1daad66ffb7889338
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-02-07 17:24:02 +0330

    ⬛⬛⬛ NEXT TRAINING ⬛⬛⬛

---
+++ pages/[userID]/index.tsx
@@ -0,0 +1,15 @@
+import { useRouter } from 'next/router'
+import React from 'react'
+
+const NestedDynamicRoute = () => {
+    const router = useRouter()
+    const { userID } = router.query
+    return (
+        <div className='w-screen h-screen flex flex-col gap-y-10 mt-10 items-center juftify-center bg-black text-white'>
+            <h1 className='font-black text-center w-screen text-5xl bg-zinc-800 py-6'>Dynamic Nested Routes</h1>
+            <h1 className='font-black text-center w-screen text-5xl bg-zinc-800 py-10'>DYNAMIC PAGE NAME IS  : <span className='text-red-900 bg-zinc-400 px-4 py-2 rounded-xl box-border border-black border-8'>  {userID} </span>  </h1>
+        </div>
+    )
+}
+
+export default NestedDynamicRoute
