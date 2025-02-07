caa0a010cda1b3dd7ab1df7d23086b78781326a7
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-02-07 20:51:14 +0330

    ⬛⬛⬛ NEXT TRAINING ⬛⬛⬛

--- pages/redirecting/index.tsx
+++ pages/redirecting/index.tsx
@@ -3,10 +3,11 @@ import React, { useEffect } from 'react'
 import toast, { Toaster } from 'react-hot-toast';
 
 const RedirectingPage = () => {
+    
     const router = useRouter()
     function redirectHandler(): void { router.push('/dynamic-links') }
     const notify = () => toast.loading('Redirecting to Dynamic Links Page')
-    
+
     useEffect(() => {
         notify()
         setTimeout(() => { redirectHandler() }, 3000)
@@ -14,10 +15,7 @@ const RedirectingPage = () => {
 
     return (
         <div className='w-screen h-screen flex items-center justify-center bg-black text-white'>
-            <Toaster
-                position="top-center"
-                reverseOrder={false}
-            />
+            <Toaster position="top-center" reverseOrder={false} />
             <h1 className='text-6xl font-bold text-white'>Redirecting Page</h1>
         </div>
     )
