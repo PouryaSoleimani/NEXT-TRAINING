1730a198a4b1a8c88e75289a7fef32aba7f33c47
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-23 13:13:29 +0330

    ⬛ LEARNING AND PRACTICING ⬛

--- pages/articles.tsx
+++ pages/articles.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Articles() {
     return (
         <div className='flex items-center justify-center w-screen h-screen flex-col space-y-5'>
-            <h1 className='bg-white/10 backdrop-blur-sm font-extrabold text-6xl p-12 rounded-3xl'>ARTICLES PAGE</h1>
+            <h1 className='bg-white/10 backdrop-blur-sm font-extrabold text-6xl p-12 rounded-3x text-white'>ARTICLES PAGE</h1>
             <p className='text-3xl bg-white/10 p-10 rounded-3xl text-zinc-300 border-8 border-zinc-950 w-1/2 font-bold leading-9 tracking-tighter'>Lorem, ipsum dolor  accusamus voluptatem animi enim deserunt officiis et culpa voluptatibus iusto.</p>
         </div>
     )
