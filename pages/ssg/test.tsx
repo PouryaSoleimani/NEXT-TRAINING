722b0736f90f93e41f35f265dff7baa9c3050efe
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-23 23:40:39 +0330

    ⬛ LEARNING  ⬛

--- pages/ssg/test.tsx
+++ pages/ssg/test.tsx
@@ -7,10 +7,10 @@ type SingleItemType = { id: number, title: string, price: number }
 
 const Test: NextPage<Props> = ({ data }) => {
   return (
-    <div className="p-6 font-sans bg-zinc-900 min-h-screen">
+    <div className="p-6 font-sans bg-zinc-950 min-h-screen  flex items-center justify-center gap-4 flex-wrap">
       {data.map((item) => (
-        <div key={item.id} className="border border-zinc-300 rounded-lg p-4 mb-4 shadow-lg bg-black hover:shadow-xl transition-shadow text-white w-96 text-center text-4xl">
-          <h3 className="text-5xl font-semibold text-zinc-100 mb-8">
+        <div key={item.id} className="border-4 border-zinc-300 rounded-lg p-4 mb-4 shadow-lg bg-black hover:shadow-xl transition-shadow text-white w-96 text-center text-xl">
+          <h3 className="text-3xl font-semibold text-zinc-100 mb-8">
             {item.title}
           </h3>
           <p className="text-blue-400">
