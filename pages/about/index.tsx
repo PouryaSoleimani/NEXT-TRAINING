2bfc465f0c4d25627a40cde3605ec623ff9f380e
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-23 23:22:04 +0330

    ⬛ LEARNING  ⬛

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -6,12 +6,15 @@ type SingleItemType = { id: number, title: string, price: number }
 const Index: NextPage<Props> = ({ data }) => {
    return (
       <div>
-         {data.map(item => (
-            <div key={item.id}>
-               <h2>{item.title}</h2>
-               <p>Price: ${item.price}</p>
-            </div>
-         ))}
+         <h1 className='text-5xl font-extrabold bg-slate-900 text-center py-4 text-white'>SSG TEST PAGE</h1>
+         <div className='flex items-center justify-around flex-wrap'>
+            {data.map(item => (
+               <div key={item.id} className='bg-zinc-700 border-4 border-red-800 w-96 p-4 rounded-xl m-4 text-white font-bold flex flex-col items-center text-2xl'>
+                  <h2 className='text-4xl'>{item.title}</h2>
+                  <p>Price: ${item.price}</p>
+               </div>
+            ))}
+         </div>
       </div>
    )
 }
