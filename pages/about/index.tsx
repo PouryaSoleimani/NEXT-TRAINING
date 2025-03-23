d36db9fcf29720f214a9d3ae591f51deb5cece3f
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-23 23:18:19 +0330

    ⬛ LEARNING  ⬛

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import { NextPage, GetStaticProps } from 'next'
 
-const About = () => {
-   return (<div>About Us</div>)
+interface Props { data: [{ id: number, title: string, price: number }] }
+type SingleItemType = { id: number, title: string, price: number }
+
+const Index: NextPage<Props> = ({ data }) => {
+   return (
+      <div>
+         {data.map(item => (
+            <div key={item.id}>
+               <h2>{item.title}</h2>
+               <p>Price: ${item.price}</p>
+            </div>
+         ))}
+      </div>
+   )
 }
 
-export default About
+export const getStaticProps: GetStaticProps = async (ctx) => {
+   const data = [
+      { id: 1, title: 'First Item', price: 100 },
+      { id: 2, title: 'Second Item', price: 200 },
+      { id: 3, title: 'Third Item', price: 300 },
+   ]
+   return {
+      props: { data: data },
+   }
+}
+
+export default Index
