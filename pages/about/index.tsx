e40e7bc45946de284315c89720852946ee04e439
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-23 23:48:27 +0330

    ⬛ LEARNING  ⬛

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -6,12 +6,14 @@ type SingleItemType = { id: number, title: string, price: number }
 const Index: NextPage<Props> = ({ data }) => {
    return (
       <div>
-         <h1 className='text-5xl font-extrabold bg-slate-900 text-center py-4 text-white'>SSG TEST PAGE</h1>
-         <div className='flex items-center justify-around flex-wrap'>
-            {data.map(item => (
-               <div key={item.id} className='bg-zinc-700 border-4 border-red-800 w-96 p-4 rounded-xl m-4 text-white font-bold flex flex-col items-center text-2xl'>
-                  <h2 className='text-4xl'>{item.title}</h2>
-                  <p>Price: ${item.price}</p>
+         <h1 className="text-3xl font-semibold text-zinc-100 mb-4 text-center bg-red-900 py-3 border-y-8 border-zinc-800">Item List</h1>
+
+         <div className="grid grid-cols-3 justify-items-center gap-10 px-10 py-1">
+            {data.map((item) => (
+               <div key={item.id} className="border-4 border-emerald-900 bg-zinc-900 rounded-lg p-4 mb-4 shadow-lg hover:shadow-xl transition-shadow text-white w-96 text-center text-3xl">
+                  <h1 className="text-4xl font-semibold text-zinc-100 mb-4">{item.title}</h1>
+                  <p className="text-blue-400">${item.price.toFixed(2)}</p>
+                  <button className="mt-4 bg-black tracking-tight text-lg font-black text-white py-2 px-4 rounded">View Details</button>
                </div>
             ))}
          </div>
@@ -25,6 +27,7 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
       { id: 2, title: 'Second Item', price: 200 },
       { id: 3, title: 'Third Item', price: 300 },
       { id: 4, title: 'Fourth Item', price: 400 },
+      { id: 5, title: 'Fifth Item', price: 500 },
    ]
    return {
       props: { data: data },
