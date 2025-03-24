e6b7b4317e074c125aa9cf08cb9e06a07eee0475
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-24 21:17:52 +0330

    🟥SSG ---> DATA FETCHING

--- pages/products2/[id].tsx
+++ pages/products2/[id].tsx
@@ -55,16 +55,15 @@ export const getStaticPaths: GetStaticPaths = async () => {
       { params: { id: "5" } },
    ]
    // DYNAMIC WAY
-   const request = await fetch(`https://fakestoreapi.com/products`)
+   const request = await fetch("https://fakestoreapi.com/products?limit=10")
    const products = await request.json()
    const pathsDynamic = products.map((product: SingleProductType) => {
       return {
-         paths: { id: product.id }
+         params: { id: product.id.toString() }
       }
    })
-
    return {
-      paths: pathsDynamic,
+      paths: [],
       fallback: false,
    }
 }
