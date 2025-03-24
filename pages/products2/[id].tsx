5c40ac897e985af63658fa227577937df63b79e0
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-24 13:38:02 +0330

    SSG ---> DATA FETCHING

--- pages/products2/[id].tsx
+++ pages/products2/[id].tsx
@@ -1,21 +1,32 @@
 import { NextPage, GetStaticProps, GetStaticPaths } from 'next'
 
-interface Props { ID: number }
+interface Props { ID: number, product: { id: number, title: string, price: string } }
 
-const SingleProductPage: NextPage<Props> = ({ ID }) => {
+const SingleProductPage: NextPage<Props> = ({ ID, product }) => {
    return (
-      <div className='w-screen h-screen flex items-center justify-center text-zinc-200 font-bold text-5xl'>
-         <h2 className='font-bold bg-zinc-900 px-5 py-2 rounded-md'> SINGLE PRODUCT # {ID}</h2>
-      </div>
+      <section className='w-screen h-screen flex items-center justify-center flex-col gap-y-5'>
+         <div className='flex items-center justify-center text-zinc-200 font-bold text-5xl'>
+            <h2 className='font-bold bg-zinc-900 px-5 py-4 rounded-md'> SINGLE PRODUCT # {ID}</h2>
+         </div>
+         <div className='flex items-center justify-center text-zinc-200 font-bold text-5xl'>
+            <h2 className='font-bold bg-zinc-900 px-5 py-10 rounded-md'> {product?.title.slice(0, 10)} : ${product?.price}</h2>
+         </div>
+      </section>
    )
 }
 
 
 export const getStaticProps: GetStaticProps = async (context) => {
+   //  GETTING THE DYNAMIC ROUTE PARAMS FROM CONTEXT
    const ID = context.params?.id
    console.log(ID)
+   const request = await fetch(`https://fakestoreapi.com/products/${ID}`)
+   const product = await request.json()
    return {
-      props: { ID },
+      props: {
+         ID: ID,
+         product: product
+      },
    }
 }
 
