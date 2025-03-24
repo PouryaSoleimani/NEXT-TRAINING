d67a4ba81319e9a65035888d81441cf450a9fa8e
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-24 23:19:09 +0330

    🟥SSG ---> DATA FETCHING

--- COMPONENTS/FOOTER/GlassMorphismCard.tsx
+++ COMPONENTS/FOOTER/GlassMorphismCard.tsx
@@ -7,7 +7,7 @@ type SingleProductType = { id: number; title: string; price: string; image: stri
 
 const GlassMorphismCard: React.FC<GlassmorphismCardProps> = ({ product }) => {
    return (
-      <div className="relative bg-white bg-opacity-10 backdrop-blur-xl border border-gray-200 rounded-xl shadow-lg p-6 transition-transform transform hover:scale-105 hover:shadow-2xl flex flex-col justify-center">
+      <div className="relative bg-white bg-opacity-10 backdrop-blur-xl border border-gray-200 rounded-xl shadow-lg p-3 transition-transform transform hover:scale-105 hover:shadow-2xl flex flex-col justify-center">
          {/* Product Image */}
          <div className="relative overflow-hidden rounded-lg flex justify-center">
             <Image src={product.image} alt={product.title} className="rounded-lg" width={200} height={150} />
@@ -17,13 +17,11 @@ const GlassMorphismCard: React.FC<GlassmorphismCardProps> = ({ product }) => {
          <h5 className="mt-4 text-lg font-semibold text-white truncate">{product.title.slice(0, 25)}</h5>
 
          {/* Price and Button */}
-         <div className="flex items-center justify-between mt-4">
+         <div className="flex items-center justify-between">
             <span className="text-3xl font-bold text-blue-400">${product.price}</span>
-            <Tooltip title="Add to Cart">
-               <Button type="primary" icon={<ShoppingBasket className="w-5 h-5" />} className="bg-blue-500 hover:bg-blue-600 border-none text-white rounded-lg px-4 py-2"   >
-                  Buy Now
-               </Button>
-            </Tooltip>
+            <Tooltip title="Add to Cart" >
+               <Button type="primary" icon={<ShoppingBasket className="w-5 h-5" />} className="bg-blue-500 hover:bg-blue-600 border-none text-white rounded-lg px-3 py-2"   >Buy Now</Button>
+            </Tooltip>s
          </div>
 
          {/* Decorative Glow */}
