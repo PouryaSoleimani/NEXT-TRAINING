c328aecd3932f18485db3176074aba9dc79e4d35
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-24 11:54:41 +0330

    ⬛SSG⬛

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { NextPage, GetStaticProps } from 'next'
+import Image from 'next/image'
 
-const index = () => {
+interface Props { products: [{ id: number, title: string, price: number, category: string, image: string }] }
+type SingleProductType = { id: number, title: string, price: number, category: string, image: string }
+
+//  COMPONENT
+const Index: NextPage<Props> = ({ products }) => {
    return (
-      <div>index</div>
+      <>
+         <h1>PRODUCTS</h1>
+         {products.map((product: SingleProductType) => (
+            <div key={product.id}>
+               <Image src={product.image} width={100} height={100} alt='Image' />
+               <h2>{product.title}</h2>
+               <p>{product.price}</p>
+            </div>
+         ))}
+      </>
    )
 }
 
-export default index
+export const getStaticProps: GetStaticProps = async (ctx) => {
+   const request = await fetch("https://fakestoreapi.com/products?limit=10")
+   const data = await request.json()
+
+   return {
+      props: { products: data },
+   }
+}
+
+export default Index
