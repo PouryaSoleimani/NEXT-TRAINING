1bcdeab4730d19b349c5baa1c71331a3bca9e994
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-24 10:04:47 +0330

    ⬛ LEARNING  ⬛

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,37 +1,29 @@
 import { NextPage, GetStaticProps } from 'next'
 
-interface Props { data: [{ id: number, title: string, price: number }] }
-type SingleItemType = { id: number, title: string, price: number }
 
-const Index: NextPage<Props> = ({ data }) => {
+//^ COMPONENT  
+interface Props { posts: SinglePostType[]; }
+type SinglePostType = { id: number, title: string, body: string }
+const SSG: NextPage<Props> = ({ posts }) => {
    return (
-      <div>
-         <h1 className="text-3xl font-semibold text-zinc-100 mb-4 text-center bg-red-900 py-3 border-y-8 border-zinc-800">Item List</h1>
-
-         <div className="grid grid-cols-3 justify-items-center gap-10 px-10 py-1">
-            {data.map((item) => (
-               <div key={item.id} className="border-4 border-emerald-900 bg-zinc-900 rounded-lg p-4 mb-4 shadow-lg hover:shadow-xl transition-shadow text-white w-96 text-center text-3xl">
-                  <h1 className="text-4xl font-semibold text-zinc-100 mb-4">{item.title}</h1>
-                  <p className="text-blue-400">${item.price.toFixed(2)}</p>
-                  <button className="mt-4 bg-black tracking-tight text-lg font-black text-white py-2 px-4 rounded">View Details</button>
-               </div>
-            ))}
-         </div>
+      <div className='p-6 font-sans bg-zinc-950 min-h-screen  flex items-center justify-center gap-4 flex-wrap'>
+         {posts.map((post: SinglePostType) => (
+            <div key={post.id} className='border-2 p-4 rounded-lg text-white font-black'>{post.title}</div>
+         ))}
       </div>
    )
 }
 
+
+//^ GET STATIC PROPS
 export const getStaticProps: GetStaticProps = async (ctx) => {
-   const data = [
-      { id: 1, title: 'First Item', price: 100 },
-      { id: 2, title: 'Second Item', price: 200 },
-      { id: 3, title: 'Third Item', price: 300 },
-      { id: 4, title: 'Fourth Item', price: 400 },
-      { id: 5, title: 'Fifth Item', price: 500 },
-   ]
+   const request = await fetch('https://jsonplaceholder.typicode.com/posts')
+   const data = await request.json()
    return {
-      props: { data: data },
+      props: { posts: data },
    }
 }
 
-export default Index
+
+
+export default SSG
