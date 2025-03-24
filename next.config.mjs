e8e0ad31aaa766ef31e955a8f14579aefd4a8461
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-24 11:58:29 +0330

    ⬛SSG⬛

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'fakeStoreApi',
-        pathname: 'fakestoreapi.com',
-      },
-    ],
-  },
 };
 
 export default nextConfig;
