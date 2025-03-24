20cb8e8294bf10538b325ad7aef4af24bc5db01f
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-24 12:08:51 +0330

    SSG ---> DATA FETCHING

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -8,14 +8,16 @@ type SingleProductType = { id: number, title: string, price: number, category: s
 const Index: NextPage<Props> = ({ products }) => {
    return (
       <>
-         <h1>PRODUCTS</h1>
-         {products.map((product: SingleProductType) => (
-            <div key={product.id}>
-               <Image src={product.image} width={100} height={100} alt='Image' />
-               <h2>{product.title}</h2>
-               <p>{product.price}</p>
-            </div>
-         ))}
+         <h1 className='bg-zinc-800 text-center py-3 text-black font-bold border-b-4 border-white'>PRODUCTS</h1>
+         <div className='flex justify-around items-center flex-wrap gap-5 my-10'>
+            {products.map((product: SingleProductType) => (
+               <div key={product.id} className='border-4 rounded-xl w-64 h-80 flex flex-col items-center justify-around p-5 text-lg gap-2'>
+                  <Image src={product.image} width={90} height={90} alt='Image' className='w-32 h-32 rounded-md' />
+                  <h5 className='whitespace-nowrap text-white font-bold mt-3'>{product.title.slice(0, 20)}</h5>
+                  <p className='bg-blue-300 text-blue-900 px-5 py-2 font-bold text-xl rounded-md'>${product.price}</p>
+               </div>
+            ))}
+         </div>
       </>
    )
 }
