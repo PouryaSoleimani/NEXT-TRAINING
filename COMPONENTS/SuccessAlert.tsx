d85a3820b597d5ad211b9845fb63888759a9a953
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-25 00:42:24 +0330

    🟥SSG ---> DATA FETCHING

--- COMPONENTS/SuccessAlert.tsx
+++ COMPONENTS/SuccessAlert.tsx
@@ -3,6 +3,6 @@ import { Alert } from 'antd';
 import { Check } from 'lucide-react';
 
 
-const SuccessAlert: React.FC = () => <Alert message="Success Text" type="success" showIcon className=' border-none py-2 font-bold w-96' />;
+const SuccessAlert: React.FC = () => <Alert message="Added to Your Cart" type="success" showIcon className=' border-none py-2 font-bold w-96' />;
 
 export default SuccessAlert
