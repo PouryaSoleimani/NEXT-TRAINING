e017acca5a63bf07487c0a3e4df1aecac0bdc91e
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-24 11:15:35 +0330

    ⬛SSG⬛

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,14 +1,13 @@
 import { NextPage, GetStaticProps } from 'next'
 
-interface Props {
-   posts: any[];
-}
+interface Props { posts: [{ id: number, title: string, body: string }] }
+type SinglePostType = { id: number, title: string, body: string }
 
 // COMPONENT
 const Index: NextPage<Props> = ({ posts }) => {
    return (
       <div className='flex flex-wrap gap-4 mx-auto items-center justify-around p-10'>
-         {posts.map(post => (
+         {posts.map((post: SinglePostType) => (
             <div key={post.id} className='border-2 p-4 my-4 w-64 h-32 flex items-center justify-center flex-col rounded-xl bg-zinc-900'>
                <h2 className='text-white'>{post.title.slice(0, 10)}</h2>
                <p className='text-zinc-500'>{post.body.slice(0, 40)}</p>
@@ -20,10 +19,10 @@ const Index: NextPage<Props> = ({ posts }) => {
 
 // GET STATIC PROPS ==> ONLY RUNS AT SERVER SIDE
 export const getStaticProps: GetStaticProps = async (ctx) => {
+
    const request = await fetch('https://jsonplaceholder.typicode.com/posts')
    const data = await request.json()
 
-   console.log("GET STATIC PROPS RUNNING")
    return {
       props: { posts: data },
    }
