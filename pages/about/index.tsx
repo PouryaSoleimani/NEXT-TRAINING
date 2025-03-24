d0b9f7127801a25fc32e914edc6f65d1f89eed88
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-24 10:27:00 +0330

    ⬛SSG⬛

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -8,7 +8,9 @@ const SSG: NextPage<Props> = ({ posts }) => {
    return (
       <div className='font-sans bg-zinc-950 min-h-screen  flex items-center justify-around w-[90%] mx-auto gap-4 flex-wrap p-10'>
          {posts.map((post: SinglePostType) => (
-            <div key={post.id} className='border-8 border-orange-400  p-4 rounded-lg text-white font-black w-64 h-32 flex items-center justify-center duration-500 hover:scale-110'>{post.title.slice(0, 20)}</div>
+            <div key={post.id} className='border-8 border-orange-400  p-4 rounded-lg text-white font-black w-64 h-32 flex items-center justify-center duration-500 hover:scale-110'>
+               {post.title.slice(0, 40)}
+            </div>
          ))}
       </div>
    )
