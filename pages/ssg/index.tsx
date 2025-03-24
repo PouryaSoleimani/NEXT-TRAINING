1efff246f6a88b3ab0ebe4ad6c44e0185bdab479
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-24 13:08:39 +0330

    SSG ---> DATA FETCHING

--- pages/ssg/index.tsx
+++ pages/ssg/index.tsx
@@ -4,8 +4,10 @@ import axios from 'axios'
 import Link from 'next/link'
 import { Home } from 'lucide-react';
 import { ShieldX } from 'lucide-react';
+
 //PROPS TYPE
 interface PropsType { products: [{ id: number, title: string, price: number, description: string, category: string, image: string, rating: { rate: number, count: number } }], }
+type SingleProductType = { id: number, title: string, price: number, description: string, category: string, image: string, rating: { rate: number, count: number } }
 
 //^ COMPONENT
 const Index: NextPage<PropsType> = ({ products }) => {
@@ -14,22 +16,22 @@ const Index: NextPage<PropsType> = ({ products }) => {
   return (
     <>
       <div className='flex flex-col border-2 border-white bg-zinc-900 items-start space-y-2 text-xl p-10 text-slate-200 justify-center font-bold h-full my-10'>
-        {products.map(item =>
+        {products.map((item: SingleProductType) =>
           <div key={item.id}>
-            <h1 className='py-2' >
+            <h3 className='py-2' >
               <Link href={`/ssg/${item.id}`} className='hover:bg-black hover:text-black p-3 rounded-xl decoration-none no-underline text-white tracking-tighter font-semibold'>
                 {item.id} - {item.title.slice(0, 20)} - {item.price}$-
                 {Array(Math.ceil(item.rating.rate)).fill(0).map(item => ('🤍'))}
                 {Array((5 - Math.ceil(item.rating.rate))).fill(0).map(item => ('🖤'))}
               </Link>
-            </h1>
+            </h3>
           </div>
         )}
       </div>
 
-      <div className='flex items-center justify-center'>
-        <Link href='/' className='bg-green-400 flex items-center gap-2 rounded-xl p-4 border-4 border-zinc-600 text-black text-2xl font-bold mx-auto no-underline'><Home size={32} strokeWidth={2.5} />HOME PAGE</Link>
-        <Link href='/ssg/product-notfound' className='bg-red-400 flex items-center gap-2 rounded-xl p-4 border-4 border-zinc-600 text-black text-2xl font-bold mx-auto no-underline'><ShieldX size={32} strokeWidth={2.5} />NOT FOUND PAGE</Link>
+      <div className='flex items-center justify-center w-fit mx-auto gap-3'>
+        <Link href='/' className='bg-green-800 hover:bg-green-600 flex items-center gap-2 rounded-xl px-3 py-3 text-black text-lg font-bold mx-auto no-underline'><Home size={20} strokeWidth={2.5} />HOME PAGE</Link>
+        <Link href='/ssg/product-notfound' className='bg-red-900 hover:bg-red-600 flex items-center gap-2 rounded-xl px-3 py-3 text-black text-lg font-bold mx-auto no-underline'><ShieldX size={20} strokeWidth={2.5} />NOT FOUND PAGE</Link>
       </div>
     </>
   )
@@ -38,6 +40,7 @@ const Index: NextPage<PropsType> = ({ products }) => {
 
 //^ STATIC SITE GENERATION ( SSG ) (ONLY RUNS ON SERVER SIDE) ====================================================================================================================
 export const getStaticProps: GetStaticProps = async (ctx) => {
+
   //-> 1 - FETCHING DATA AND SENDING IT TO THE COMPONENT ABOVE ...
   const request = axios.get('https://fakestoreapi.com/products?limit=7');
   const response = (await request).data
