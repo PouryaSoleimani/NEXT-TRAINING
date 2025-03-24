3ec7a9214ea41ca6acfb0ec6834afcfdce0c572f
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-24 18:56:00 +0330

    🟥SSG ---> DATA FETCHING

--- pages/products2/index.tsx
+++ pages/products2/index.tsx
@@ -19,7 +19,7 @@ const Products2Page: NextPage<Props> = ({ products }) => {
 
          <div className='flex justify-around items-center flex-wrap gap-3 my-10 px-10'>
             {products.map((product: SingleProductType) => (
-               <Link href={`/products2/${product.id}`} className='hover:scale-105 duration-500 cursor-pointer no-underline'>
+               <Link href={`/products2/${product.id}`} className='hover:scale-105 duration-500 cursor-pointer no-underline' >
                   <Card key={product.id} hoverable style={{ width: 200, height: 230, backgroundColor: "#101010", padding: 0, margin: 0, transition: "all 500ms ease-out" }} className='hover:scale-[102%]' cover={<Image src={product.image} width={90} height={110} alt='Image' className='w-32 h-52 rounded-md' />}    >
                      <h5 className='whitespace-nowrap text-white font-bold'>{product.title.slice(0, 20)}</h5>
                      <Tooltip title="Add to Cart" >
