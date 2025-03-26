546ff3624d4656356fdbdb1371975b80c486c0be
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-26 17:19:10 +0330

    🟥SSR🟥

--- pages/ssr/ssr2.tsx
+++ pages/ssr/ssr2.tsx
@@ -9,6 +9,7 @@ type SingleUserType = { id: number; name: string; username: string; email: strin
 // COMPONENT  ==================================================================================================================================
 const SSR2: NextPage<Props> = ({ data }) => {
     return (
+
         <div className="flex w-screen min-h-screen p-12 gap-8 flex-wrap items-center justify-center bg-gradient-to-br from-gray-900 via-black to-gray-800">
             {data.map((user: SingleUserType) => (
                 <div key={user.id} className="w-72 h-48 flex flex-col items-center justify-center bg-gray-900 rounded-xl shadow-lg hover:shadow-2xl transition-shadow border border-gray-700 hover:bg-gray-800 transform hover:scale-105 duration-500 hover:text-white">
@@ -18,6 +19,7 @@ const SSR2: NextPage<Props> = ({ data }) => {
                 </div>
             ))}
         </div>
+
     );
 };
 
