721eafb90fc06834e0c5d06732715e2526aa780c
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-26 23:00:23 +0330

    SSR

--- pages/ssr/[id].tsx
+++ pages/ssr/[id].tsx
@@ -1,4 +1,5 @@
 //^ DYNAMIC SSR PAGE _________________________________________________________________________________________________________________________________________
+import { url } from 'inspector'
 import { NextPage, GetServerSideProps } from 'next'
 import { useRouter } from 'next/router'
 
@@ -11,15 +12,17 @@ const SingleUser: NextPage<Props> = ({ data }) => {
   function backHandler() { router.back() }
 
   return (
-    <div className='flex flex-col items-center bg-zinc-950 py-8 px-6 mt-32 w-fit rounded-xl mx-auto border-2 border-white shadow-[8px_8px_15px_rgba(0,0,0,0.5),-8px_-8px_15px_rgba(255,255,255,0.1)] hover:scale-105 transition-transform duration-500 cursor-pointer'>
-      <p className='text-3xl font-bold text-white mb-4'>{data.id}</p>
-      <p className='text-2xl font-semibold text-white mb-2'>{data.name}</p>
-      <p className='text-lg text-blue-100 mb-2 bg-blue-950 p-2 rounded-md w-52'>{data.email}</p>
-      <p className='text-lg text-white mb-4 bg-red-950 p-2 rounded-md'>{data.phone}</p>
-      <button className='bg-green-700 hover:bg-green-600 text-white py-2 px-20 text-lg font-medium rounded-lg shadow-[4px_4px_10px_rgba(0,0,0,0.5),-4px_-4px_10px_rgba(255,255,255,0.1)] transition-colors duration-300' onClick={backHandler} >
-        BACK
-      </button>
-    </div>
+    <section className='w-screen h-screen flex items-center justify-center bg-opacity-30 ' style={{ backgroundImage: `url("/NEW___PICS/CARTOON____BG.jpg")` }}>
+      <div className='flex flex-col items-center bg-white/10 backdrop-blur-sm backdrop-brightness-50 z-50  py-8 px-6 w-fit rounded-xl mx-auto border-2 border-white shadow-[8px_8px_15px_rgba(0,0,0,0.5),-8px_-8px_15px_rgba(255,255,255,0.1)] hover:scale-105 transition-transform duration-500 cursor-pointer'>
+        <p className='text-3xl font-bold text-white mb-4'>{data.id}</p>
+        <p className='text-2xl font-semibold text-white mb-2'>{data.name}</p>
+        <p className='text-lg text-blue-100 mb-2 bg-blue-950 p-2 rounded-md w-52'>{data.email}</p>
+        <p className='text-lg text-white mb-4 bg-red-950 p-2 rounded-md'>{data.phone}</p>
+        <button className='bg-green-700 hover:bg-green-600 text-white py-2 px-20 text-lg font-medium rounded-lg shadow-[4px_4px_10px_rgba(0,0,0,0.5),-4px_-4px_10px_rgba(255,255,255,0.1)] transition-colors duration-300' onClick={backHandler} >
+          BACK
+        </button>
+      </div>
+    </section>
   )
 }
 
