12836e773d01feef77c329687eaf670c3b04dc93
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-26 17:17:35 +0330

    🟥SSR🟥

--- pages/ssr/ssr2.tsx
+++ pages/ssr/ssr2.tsx
@@ -11,11 +11,11 @@ const SSR2: NextPage<Props> = ({ data }) => {
     return (
         <div className="flex w-screen min-h-screen p-12 gap-8 flex-wrap items-center justify-center bg-gradient-to-br from-gray-900 via-black to-gray-800">
             {data.map((user: SingleUserType) => (
-                <Card key={user.id} className="w-72 h-48 flex flex-col items-center justify-center bg-gray-900 rounded-xl shadow-lg hover:shadow-2xl transition-shadow border border-gray-700 hover:bg-gray-800 transform hover:scale-105 duration-500" bordered={true}>
+                <div key={user.id} className="w-72 h-48 flex flex-col items-center justify-center bg-gray-900 rounded-xl shadow-lg hover:shadow-2xl transition-shadow border border-gray-700 hover:bg-gray-800 transform hover:scale-105 duration-500 hover:text-white">
                     <h2 className="text-xl font-semibold text-black group-hover:text-white">{user.name}</h2>
                     <p className="text-gray-600">{user.username}</p>
                     <h4 className="text-sm text-gray-500">{user.email.slice(0, 15)}</h4>
-                </Card>
+                </div>
             ))}
         </div>
     );
