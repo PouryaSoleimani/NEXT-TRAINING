ed99f7b7fc4ba2ed5a6474e78984d88eb76e016a
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-26 16:06:04 +0330

    ⬛SSR⬛

--- pages/ssr/ssr2.tsx
+++ pages/ssr/ssr2.tsx
@@ -8,9 +8,9 @@ type SingleUserType = { id: number, name: string, username: string, email: strin
 
 const SSR2: NextPage<Props> = ({ data }) => {
     return (
-        <div className='flex w-screen h-screen p-10 gap-10 flex-wrap items-center justify-center'>
+        <div className='flex w-screen h-1/2 mt-auto p-10 gap-5 flex-wrap items-center justify-center'>
             {data.map((user: SingleUserType) => (
-                <Card type='inner' key={user.id} className='w-72 h-48 flex flex-col items-center justify-center font-extralight'>
+                <Card type='inner' key={user.id} className='w-72 h-48 flex flex-col items-center justify-center font-extralight' bordered>
                     <h2>{user.name}</h2>
                     <p>{user.username}</p>
                     <h4>{user.email.slice(0, 15)}</h4>
