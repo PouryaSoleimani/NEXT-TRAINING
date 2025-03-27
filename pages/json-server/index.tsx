6f2c8accacbf95b606e01338f3cc8e0a813106e5
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-27 10:58:23 +0330

    JSON SERVER

--- pages/json-server/index.tsx
+++ pages/json-server/index.tsx
@@ -20,13 +20,13 @@ const JsonServerPage = () => {
         <div>
             <div>
                 <h1 className='bg-zinc-800 text-cyan-500 text-3xl text-center font-bold p-4 w-full'>JSON SERVER</h1>
-                <div className='flex flex-wrap items-center justify-center gap-4 p-5'>
+                <div className='flex flex-wrap items-center justify-center gap-2 p-5'>
                     {users.length ?
                         users.map((user) => (
-                            <div className='card w-[20rem] h-[20rem]  mx-auto p-4 bg-zinc-800 text-cyan-500 rounded-md' key={user.id}>
+                            <div className=' w-[20rem] h-[12rem]  mx-auto p-4 bg-zinc-900 border text-cyan-500 rounded-md' key={user.id}>
                                 <h1 className='w-full text-center bg-black text-white font-bold'>{user.id}</h1>
-                                <h2 className='tracking-tighter '>{user.name}</h2>
-                                <p className='text-center w-full mt-2'>{user.age}</p>
+                                <h2 className='tracking-tighter text-center font-black mt-2 '>{user.name}</h2>
+                                <p className='text-center w-full mt-2 text-3xl font-black'>{user.age}</p>
                             </div>
                         ))
                         :
