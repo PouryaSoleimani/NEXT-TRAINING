1987e7cc97f4901005afcc095edcffba68edf0ce
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-27 11:09:39 +0330

    JSON SERVER

--- pages/json-server/[userID].tsx
+++ pages/json-server/[userID].tsx
@@ -7,21 +7,17 @@ const UserID = () => {
     const [SingleUser, setSingleUser] = useState({ id: 0, name: '', age: 0 })
 
     useEffect(() => {
-        fetch(`http://localhost:4000/users/${userID}`)
+        fetch(`http://localhost:4000/users?id=${userID}`)
             .then(response => response.json())
-            .then(json => { setSingleUser(json) })
+            .then(json => setSingleUser(json))
     }, [])
 
     return (
         <div className='w-screen h-screen flex items-center justify-center p-10'>
-            <div className="card">
-                {SingleUser ?
-                    <>
-                        <h1 className="text-5xl font-bold">{SingleUser.id}</h1>
-                        <h2 className="text-3xl font-semibold">{SingleUser.name}</h2>
-                        <h3 className="text-2xl font-medium">{SingleUser.age}</h3>
-                    </>
-                    : 'Loading...'}
+            <div  className='card'>
+                    <h1 className="text-5xl font-bold">{SingleUser.id}</h1>
+                    <h2 className="text-3xl font-semibold">{SingleUser.name}</h2>
+                    <h3 className="text-2xl font-medium">{SingleUser.age}</h3>
             </div>
         </div>
     )
