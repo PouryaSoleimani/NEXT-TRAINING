11bef917155cf056dfca7cfe91bc298343128488
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-27 19:50:32 +0330

    CSR

--- pages/json-server/SingleTodoComponent.tsx
+++ pages/json-server/SingleTodoComponent.tsx
@@ -25,16 +25,16 @@ const SingleTodoComponent: React.FC<SingleTodo> = ({ id, name, age, isToggle }) 
         <>
             <div key={id} className='relative flex flex-col bg-gradient-to-br from-zinc-900 to-zinc-800 p-6 m-4 rounded-2xl backdrop-blur-sm border border-white/10 shadow-[4px_4px_10px_rgba(0,0,0,0.4),-4px_-4px_10px_rgba(255,255,255,0.1)]           hover:shadow-[8px_8px_20px_rgba(0,0,0,0.4),-8px_-8px_20px_rgba(255,255,255,0.1)] space-y-4 hover:scale-105 transition-all duration-300 group w-[18rem]'   >
                 <div className='absolute -top-3 left-1/2 -translate-x-1/2 px-4 py-1 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-lg'>
-                    <h1 className='text-white font-bold text-2xl rounded-md'>{`ID: ${id}`}</h1>
+                    <h1 className='text-white font-bold text-3xl rounded-md'>{`ID: ${id}`}</h1>
                 </div>
 
-                <h2 className='text-2xl font-black text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-blue-400 text-center mt-4 mb-2'>
+                <h2 className='text-5xl font-black text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-blue-400 text-center mt-4 mb-2'>
                     {name}
                 </h2>
 
                 <div className='bg-black/30 rounded-xl p-3 mb-3'>
-                    <p className='text-center text-3xl font-black text-white/90'>
-                        {age} <span className='text-sm font-normal text-white/60'>years</span>
+                    <p className='text-center text-5xl font-black text-white/90'>
+                        {age} <span className='text-xl font-normal text-white/60'>years</span>
                     </p>
                 </div>
 
