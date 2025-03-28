5a377fecd7501857093bb8fdb34b83d0ca93f13b
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-28 12:51:56 +0330

    CSR

--- pages/swr/index.tsx
+++ pages/swr/index.tsx
@@ -22,11 +22,11 @@ const SWR: NextPage<Props> = ({ }) => {
   //RETURN ===============================================================================================================================================
   return (
     <div>
-      <h1 className='text-amber-700 p-2 font-bold text-4xl text-center border-b-8'>SWR PAGE</h1>
+      <h1 className='text-amber-500 p-2 font-bold text-5xl text-center border-b-8 py-10'>SWR PAGE</h1>
       <div className='bg-zinc-900 p-8 font-bold grid grid-cols-4 gap-5 text-2xl'>
         {data ?
           data.map((user: SingleUserType) => (
-            <Link key={user.id} href={`/swr/${user.id}`} className='text-center border-2 border-zinc-800 rounded-md hover:-translate-y-2 duration-300'>
+            <Link key={user.id} href={`/swr/${user.id}`} className='text-center border-2 border-black rounded-md hover:-translate-y-2 duration-300'>
               <p className='bg-zinc-800 p-4 rounded-md text-white'>{user.name}</p>
               <p className='bg-zinc-900 p-4 rounded-md text-white'>{user.age}</p>
             </Link>
