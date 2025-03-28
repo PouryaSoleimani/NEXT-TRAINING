6df17647b8fe64b3e7d7fb33a9a93a2f1261a634
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-28 15:50:07 +0330

    REVEIW

--- pages/data-fetching/index.tsx
+++ pages/data-fetching/index.tsx
@@ -1,6 +1,7 @@
 //^ DATA-FETCHING PAGE =========================================================================================================================================================
 import React, { useState } from 'react'
 import { users } from '@/data/db.json' //Importing DataBase From an External File
+import Link from 'next/link'
 type SingleUserType = { id: string, name: string, age: number, isToggle?: boolean }
 
 // COMPONENT
@@ -12,7 +13,11 @@ const DataFetching = () => {
     <div>
       <h1 className='text-yellow-400 text-6xl font-bold text-center py-4'>DATA FETCHING PAGE</h1>
       <div className='bg-zinc-950 text-white grid grid-cols-4 gap-6 p-5'>
-        {data.map((item: SingleUserType) => <p key={item.id} className='py-1 bg-black p-3 rounded-md border text-xl font-bold'>{item.name} - ${item.age}</p>)}
+        {data.map((item: SingleUserType) =>
+          <Link href={`/json-server/${item.id}`} key={item.id}>
+            <p className='py-1 bg-black p-3 rounded-md border text-xl font-bold'>{item.name} - ${item.age}</p>
+          </Link>
+        )}
       </div>
     </div>
   )
