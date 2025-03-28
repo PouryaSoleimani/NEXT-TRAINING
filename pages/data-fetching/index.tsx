264a464f096ebb49cb1dc2f73bc57cd673d56db0
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-28 15:44:42 +0330

    ⬛CSR⬛

--- pages/data-fetching/index.tsx
+++ pages/data-fetching/index.tsx
@@ -1,17 +1,18 @@
 //^ DATA-FETCHING PAGE =========================================================================================================================================================
 import React, { useState } from 'react'
-import { products } from '@/data/db.json' //Importing DataBase From an External File
+import { users } from '@/data/db.json' //Importing DataBase From an External File
+type SingleUserType = { id: string, name: string, age: number, isToggle?: boolean }
 
 // COMPONENT
 const DataFetching = () => {
-  const [data, setdata] = useState(products)
+  const [data, setdata] = useState(users)
 
   // RETURN
   return (
     <div>
       <h1 className='text-yellow-400 text-6xl font-bold text-center py-4'>DATA FETCHING PAGE</h1>
       <div className='bg-zinc-700 flex flex-col px-10 py-5 space-y-5 text-2xl font-semibold'>
-        {data.map((item) => <p key={item.id}>{item.title} - ${item.price}</p>)}
+        {data.map((item: SingleUserType) => <p key={item.id}>{item.name} - ${item.age}</p>)}
       </div>
     </div>
   )
