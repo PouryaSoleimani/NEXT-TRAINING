6e3cd1010d86f6203b83b1a8e070c3d2d814b48b
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-29 01:38:18 +0330

    REVIEW

--- pages/ssr/ssr4.tsx
+++ pages/ssr/ssr4.tsx
@@ -9,9 +9,9 @@ const Ssr4: NextPage<Props> = ({ data }) => {
         <section className='w-screen h-screen flex items-center justify-center flex-col gap-y-10 bg-gradient-to-br from-zinc-900 via-black to-zinc-800'>
             <h1 className='w-full p-2 rounded-md text-center bg-zinc-800 flex items-center justify-center text-pink-700 font-bold text-4xl'>SSR RENDERING PAGE</h1>
 
-            <div className='flex flex-wrap gap-4 items-center justify-center'>
+            <div className='grid grid-cols-4 gap-6 p-5'>
                 {data.map((user: SingleUserType) => (
-                    <div key={user.id} className="p-4 bg-gray-800 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
+                    <div key={user.id} className="p-4 bg-gray-800 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 border border-white/10">
                         <h2 className="text-xl font-semibold text-white">{user.name}</h2>
                         <p className="text-gray-400">{user.username}</p>
                         <p className="text-gray-500">{user.email}</p>
