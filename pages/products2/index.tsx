6f7c91080ab9c5d24f5010f61ef5a98cdcef1a0e
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-28 17:12:18 +0330

    REVEIW

--- pages/products2/index.tsx
+++ pages/products2/index.tsx
@@ -15,7 +15,7 @@ const Products2Page: NextPage<Props> = ({ products }) => {
 
    return (
       <>
-         <h1 className='bg-zinc-800 text-center py-3 text-black font-bold border-b-4 border-white'>PRODUCTS</h1>
+         <h1 className='bg-zinc-800 text-center py-3 text-white font-bold border-b-4 border-white'>PRODUCTS</h1>
 
          <div className='flex justify-center  items-center flex-wrap gap-3 my-10 p-16'>
             {products.map((product: SingleProductType) => (
@@ -23,7 +23,7 @@ const Products2Page: NextPage<Props> = ({ products }) => {
                   <Card key={product.id} hoverable style={{ width: 200, height: 230, backgroundColor: "#101010", padding: 0, margin: 0, transition: "all 500ms ease-out", border: "none" }} className='hover:scale-[101%]' cover={<Image src={product.image} width={90} height={110} alt='Image' className='w-32 h-52 rounded-md' />}    >
                      <h5 className='whitespace-nowrap text-white font-bold'>{product.title.slice(0, 20)}</h5>
                      <Tooltip title="Add to Cart" >
-                        <Button type="primary" icon={<ShoppingBasket className='w-5 h-5' />} className='tracking-tighter'>${product.price}</Button>
+                        <Button type="primary" icon={<ShoppingBasket className='w-8 h-8' />} className='tracking-tighter'>${product.price}</Button>
                      </Tooltip>
                   </Card>
                </Link>
