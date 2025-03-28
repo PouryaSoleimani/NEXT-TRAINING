9ee85cafc9cd9aa03f1b90e1751ff0039d5626d9
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-28 18:56:42 +0330

    REVIEW

--- pages/sort/sort-test.tsx
+++ pages/sort/sort-test.tsx
@@ -26,10 +26,11 @@ const SortTest: NextPage<props> = ({ products }) => {
   return (
     <div >
       <h1 className='text-3xl text-center py-2 bg-lime-500 font-extrabold'>SORT TEST</h1>
-      <div id="SortingButtons" className='bg-lime-800 px-10 py-3 flex items-center justify-center space-x-5'>
+      <div id="SortingButtons" className='bg-zinc-800 px-10 py-3 flex items-center justify-center space-x-5'>
         <button onClick={sortAscending} className='bg-black font-bold p-2 rounded-md hover:bg-zinc-800 duration-300 text-white'>Cheapest</button>
         <button onClick={sortDescending} className='bg-white text-black font-bold p-2 rounded-md hover:bg-zinc-300 duration-300'>Most Expensive</button>
       </div>
+
       <div className='flex flex-wrap items-center justify-center gap-2 py-3 px-1'>
         {PRODUCTS.slice(0, 18).map((item: SingleItemType) => (
           <div key={item.id} className='p-3 bg-zinc-950 cursor-pointer flex flex-col items-center justify-center space-y-2 hover:border-zinc-300 duration-700 w-[15rem] h-fit border-2 border-zinc-700 rounded-xl'>
