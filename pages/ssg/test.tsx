e102b98bdf06d4aff0beeabad0aeb3c30903ba8d
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-29 00:44:57 +0330

    REVIEW

--- pages/ssg/test.tsx
+++ pages/ssg/test.tsx
@@ -11,9 +11,7 @@ const Test: NextPage<Props> = ({ data }) => {
     <div className="p-6 font-sans bg-zinc-950 min-h-screen  flex items-center justify-center gap-4 flex-wrap">
       {data.map((item) => (
         <div key={item.id} className="border-4 border-zinc-300 rounded-lg p-4 mb-4 shadow-lg bg-black hover:shadow-xl transition-shadow text-white w-96 text-center text-xl">
-          <h3 className="text-3xl font-semibold text-zinc-100 mb-8">
-            {item.title}
-          </h3>
+          <h3 className="text-3xl font-semibold text-zinc-100 mb-8">  {item.title} </h3>
           <p className="text-blue-400">  Price: <strong className="text-zinc-100">${item.price.toFixed(2)}</strong> </p>
         </div>
       ))}
