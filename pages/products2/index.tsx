5016f8c5f38206c4b5f1e47d61365d1e5772c2d7
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-28 17:13:24 +0330

    REVEIW

--- pages/products2/index.tsx
+++ pages/products2/index.tsx
@@ -19,6 +19,7 @@ const Products2Page: NextPage<Props> = ({ products }) => {
 
          <div className='flex justify-center  items-center flex-wrap gap-3 my-10 p-16'>
             {products.map((product: SingleProductType) => (
+
                <Link href={`/products2/${product.id}`} className='hover:scale-105 duration-500 cursor-pointer no-underline border-none' >
                   <Card key={product.id} hoverable style={{ width: 200, height: 230, backgroundColor: "#101010", padding: 0, margin: 0, transition: "all 500ms ease-out", border: "none" }} className='hover:scale-[101%]' cover={<Image src={product.image} width={90} height={110} alt='Image' className='w-32 h-52 rounded-md' />}    >
                      <h5 className='whitespace-nowrap text-white font-bold'>{product.title.slice(0, 20)}</h5>
@@ -27,6 +28,7 @@ const Products2Page: NextPage<Props> = ({ products }) => {
                      </Tooltip>
                   </Card>
                </Link>
+
             ))}
          </div>
       </>
