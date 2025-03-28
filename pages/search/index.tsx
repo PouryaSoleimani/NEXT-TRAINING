b01169153de5816307be3d78892055c25d1d89aa
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-28 17:56:44 +0330

    REVEIW

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -24,23 +24,23 @@ const index: React.FC = () => {
 
   // RETURN  ======================================================================================================================================
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center gap-y-10'>
 
-      <div className='flex items-center justify-evenly flex-col space-y-6 w-[30rem] rounded-b-md h-[14rem] px-6 pt-2 pb-2 bg-zinc-600 mx-auto mt-10'>
+      <div className='flex items-center justify-center flex-col space-y-6 w-[30rem] rounded-b-md h-[14rem] px-6 pt-2 pb-2 bg-zinc-600 mx-auto mt-10'>
         <input className='w-full p-2 font-bold outline-none rounded-md text-black' type="search" id='searchInput' value={searchInfo} onChange={searchHandler} />
         <button className='bg-lime-600 px-2 py-1 rounded-md font-bold text-xl'>SEARCH</button>
       </div>
 
-      <button onClick={goToSearchInput} className='bg-blue-700 p-2 text-xl font-bold ml-10 rounded-lg hover:bg-blue-400'>GO TO SEARCH INPUT</button>
+      <button onClick={goToSearchInput} className='bg-blue-700 p-2 text-3xl font-bold ml-10 rounded-lg hover:bg-blue-400 mx-auto'>GO TO SEARCH INPUT</button>
 
 
-      <div className='w-full my-10 bg-zinc-300 h-fit flex flex-wrap items-center justify-center gap-2 py-10 px-10 overflow-hidden'>
+      <div className='my-10 bg-zinc-800 h-fit grid grid-cols-4 items-center gap-2 py-10 px-10 overflow-hidden'>
         {isShowError ? (
           <h1 className='text-4xl bg-red-900 text-red-200 font-bold text-center px-4 py-6 rounded-md border-2 border-red-800 h-fit w-screen'>NO PRODUCTS FOUND</h1>)
           : (
             Users.map(user => (
-              <div key={user.id} className='bg-black w-[12rem] py-8 px-4 font-bold text-2xl text-center rounded-md '>
-                <h2>{user.name}</h2>
+              <div key={user.id} className='bg-black w-[24rem] py-8 px-4 font-bold text-2xl text-center rounded-md hover:border box-border '>
+                <h2 className='whitespace-nowrap'>{user.name}</h2>
               </div>
             ))
           )
