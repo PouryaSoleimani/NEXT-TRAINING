de3b541204fba3671a3d1696926b1656b433c30f
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-29 03:05:52 +0330

    REVIEW

--- pages/swr/swr2.tsx
+++ pages/swr/swr2.tsx
@@ -18,7 +18,7 @@ const swr2 = () => {
     return (
         <div className='grid grid-cols-3 gap-4 p-10 swr2'>
             {data.map((item: any) => (
-                <div key={item.id} className='bg-white opacity-30 hover:opacity-100 text-end text-black px-4 py-5 my-2 rounded-md w-[30rem] cursor-pointer shadow-lg hover:shadow-2xl transition-all duration-300 ease-in-out'>
+                <div key={item.id} className='bg-white opacity-30 hover:opacity-100 hover:-translate-y-2 text-end text-black px-4 py-5 my-2 rounded-md w-[30rem] cursor-pointer shadow-lg hover:shadow-2xl transition-all duration-300 ease-in-out'>
                     <h5 className='text-black text-5xl'>{item.name.toUpperCase()}</h5>
                     <h5 className='text-black text-2xl'>{item.age}</h5>
                 </div>
