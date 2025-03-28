8d62e716c3fb732ac6013c1e912901651a2399e8
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-28 17:07:58 +0330

    REVEIW

--- pages/menu/index.tsx
+++ pages/menu/index.tsx
@@ -9,15 +9,15 @@ interface Props { }
 
 const Index: NextPage<Props> = ({ }) => {
 
-  const [PRODUCTS, setPRODUCTS] = useState([...db.products])
+  const [Users, setUsers] = useState([...db.users])
 
   return (
     <div>
       <h1 className='bg-amber-700 p-4 font-bold text-3xl text-center'>MENU PAGE</h1>
       <div className='flex flex-col px-4 space-y-4 text-2xl py-4 font-bold tracking-tighter '>
-        {PRODUCTS.map(item => (
-          <Link href={`/menu/${item.id}`} key={item.id} >
-            <p> - {item.title}</p>
+        {Users.map(item => (
+          <Link href={`/menu/${item.id}`} key={item.id} className='text-white'>
+            <p>{item.id} - {item.name}</p>
           </Link>
         ))}
       </div>
