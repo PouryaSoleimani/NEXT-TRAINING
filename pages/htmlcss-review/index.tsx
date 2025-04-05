4d0c85c0e89ed8ae586c0df3f39f51718138f70a
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-05 21:58:59 +0330

    REVIEW

--- pages/htmlcss-review/index.tsx
+++ pages/htmlcss-review/index.tsx
@@ -19,6 +19,7 @@ const HTMLCSSReviewComponent = () => {
                 <li>Responsive Design</li>
             </ul>
 
+            {/* TYPOGRAPHY */}
             <p className='text-center w-full bg-white text-black font-black text-3xl py-3'>Make sure to practice these concepts!</p>
             <h2>Resources</h2>
             <p><del className='text-red-900'>300/000</del> 200/000 </p>
@@ -29,26 +30,33 @@ const HTMLCSSReviewComponent = () => {
             <p>H<small>2</small>O</p>
             <p>H<big>2</big>O</p>
 
+            {/*  DOWNLOAD */}
             <Link href="/NEW___PICS/CARTOON____BG.jpg" download target='_blank' rel="noopener noreferrer" title='DOWNLOAD THE IMAGE'>
                 <button className="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded">
                     Download PDF
                 </button>
             </Link>
-
+            {/* PHONE NUMBER  */}
             <Link href="tel:+989375008682" title='CALL ME'>
                 <button className='bg-blue-500 hover:bg-blue-700 px-4 py-2 rounded mx-1'>
                     +989375008682
                 </button>
             </Link>
-
+            {/* PICTURE */}
             <picture>
                 <source srcSet="/NEW___PICS/CARTOON____BG.jpg" type="image/webp" media="(min-width: 600px)" />
                 <source srcSet="/NEW___PICS/404_page_cover.jpg" type="image/webp" media="(min-width: 400px)" />
                 <img src="/NEW___PICS/not-found.png" alt="Responsive image" />
             </picture>
+
             {/* TABLE  */}
-            <table>
-                
+            <hr />
+            <table className='table'>
+                <tr>
+                    <th>#</th>
+                    <th>NAME</th>
+                    <th>JOB</th>
+                </tr>
             </table>
 
 
