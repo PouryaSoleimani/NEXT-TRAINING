31a0f01819e119a982e8c6f707841de348c2db82
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-05 18:49:56 +0330

    REVIEW

--- pages/htmlcss-review/index.tsx
+++ pages/htmlcss-review/index.tsx
@@ -29,7 +29,7 @@ const HTMLCSSReviewComponent = () => {
             <p>H<big>2</big>O</p>
 
             <Link href="/NEW___PICS/CARTOON____BG.jpg" download target='_blank' rel="noopener noreferrer" title='DOWNLOAD THE IMAGE'>
-                <button className="bg-blue-500 text-white px-4 py-2 rounded">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded">
                     Download PDF
                 </button>
             </Link>
