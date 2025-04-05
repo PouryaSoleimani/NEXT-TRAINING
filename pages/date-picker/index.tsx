e10f5664b0a5afe356ed810f136d950494f7adfa
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-05 14:19:51 +0330

    DATE--PICKER

--- pages/date-picker/index.tsx
+++ pages/date-picker/index.tsx
@@ -6,7 +6,7 @@ import persian from "react-date-object/calendars/persian"
 import persian_fa from "react-date-object/locales/persian_fa"
 import "react-multi-date-picker/styles/backgrounds/bg-dark.css"
 
-
+import toast from 'react-hot-toast';
 
 
 // COMPONENT=========================================================================================================================================================================
@@ -14,12 +14,21 @@ const DatePickerComponent = () => {
 
     const [value, setValue] = useState<Value>(new Date());
 
+
     function dateChangeHandler(event: Value) {
         setValue(event)
-        console.info(value)
+        console.info(value?.toString())
+        const notify = new Notification("تاریخ انتخاب شده", {
+            body: `تاریخ انتخاب شده: ${event?.toString()}`,
+            icon: "/images/icon.png",
+        })
+        const notifyDate = () => toast(`تاریخ ثبت شده ${value?.toString()}`, { style: { background: "#000", color: "#fff", border: "2px solid #fff", fontSize: "1.5rem", fontFamily: "yekan" } });
+        notifyDate()
     }
 
 
+    
+    // RETURN=========================================================================================================================================================================
     return (
         <div className="flex flex-col space-y-2 items-center justify-center h-screen font-extrabold tracking-tighter text-3xl">
             <h1 className='bg-zinc-900/50 border-4 border-red-900 backdrop-blur-3xl p-4 text-white font-semibold tracking-tighter rounded-xl'>Date Picker</h1>
