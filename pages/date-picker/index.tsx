17b0f3fb2e411d1b219f0a7fd491ac324a5c76ab
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-05 13:37:15 +0330

    DATE--PICKER

--- pages/date-picker/index.tsx
+++ pages/date-picker/index.tsx
@@ -4,7 +4,7 @@ import DatePicker, { Calendar, DateObject } from "react-multi-date-picker"
 import type { Value } from "react-multi-date-picker"
 import persian from "react-date-object/calendars/persian"
 import persian_fa from "react-date-object/locales/persian_fa"
-
+import "react-multi-date-picker/styles/backgrounds/bg-dark.css"
 
 const DatePickerComponent = () => {
 
@@ -15,7 +15,9 @@ const DatePickerComponent = () => {
     return (
         <div className="flex flex-col space-y-2 items-center justify-center h-screen font-extrabold tracking-tighter text-3xl">
             <h1 className='bg-zinc-900/50 border-4 border-red-900 backdrop-blur-3xl p-4 text-white font-semibold tracking-tighter rounded-xl'>Date Picker</h1>
-            <DatePicker value={value} onChange={setValue} calendar={persian} locale={persian_fa} />
+            <div dir="rtl">
+                <DatePicker value={value} onChange={setValue} calendar={persian} locale={persian_fa} className="bg-dark"/>
+            </div>
         </div>
     )
 }
