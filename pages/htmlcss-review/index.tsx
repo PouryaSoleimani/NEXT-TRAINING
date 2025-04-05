adf5bc248cf88c1581df6c042f12af32fe873ddd
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-05 16:28:07 +0330

    REVIEW

--- pages/htmlcss-review/index.tsx
+++ pages/htmlcss-review/index.tsx
@@ -3,8 +3,10 @@ import React from 'react'
 const HTMLCSSReviewComponent = () => {
     return (
         <div className='p-4 text-white'>
+
             <h1 className='text-center mt-4 font-black'>HTML & CSS Review</h1>
             <p className='text-orange-500 font-black text-2xl'>This is a review of HTML and CSS concepts.</p>
+
             <ul className='text-zinc-200'>
                 <li>HTML Elements</li>
                 <li>CSS Selectors</li>
@@ -12,8 +14,10 @@ const HTMLCSSReviewComponent = () => {
                 <li>Grid Layout</li>
                 <li>Responsive Design</li>
             </ul>
+
             <p className='text-center w-full bg-white text-black font-black text-3xl py-3'>Make sure to practice these concepts!</p>
             <h2>Resources</h2>
+
         </div>
     )
 }
