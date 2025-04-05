18ee80547b83b3d5114d16d352a0ee27d64e537b
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-05 14:13:28 +0330

    DATE--PICKER

--- pages/date-picker/index.tsx
+++ pages/date-picker/index.tsx
@@ -1,18 +1,18 @@
 // DATE PICKER=========================================================================================================================================================================
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import DatePicker from "react-multi-date-picker"
 import type { Value } from "react-multi-date-picker"
 import persian from "react-date-object/calendars/persian"
 import persian_fa from "react-date-object/locales/persian_fa"
 import "react-multi-date-picker/styles/backgrounds/bg-dark.css"
-import { transition } from "react-element-popper/animations/transition"
+
 
 
 
 // COMPONENT=========================================================================================================================================================================
 const DatePickerComponent = () => {
 
-    const [value, setValue] = useState < Value > (new Date());
+    const [value, setValue] = useState<Value>(new Date());
 
     function dateChangeHandler(event: Value) {
         setValue(event)
@@ -24,7 +24,7 @@ const DatePickerComponent = () => {
         <div className="flex flex-col space-y-2 items-center justify-center h-screen font-extrabold tracking-tighter text-3xl">
             <h1 className='bg-zinc-900/50 border-4 border-red-900 backdrop-blur-3xl p-4 text-white font-semibold tracking-tighter rounded-xl'>Date Picker</h1>
             <div dir="rtl">
-                <DatePicker value={value} onChange={event => dateChangeHandler(event)} calendar={persian} locale={persian_fa} className="bg-dark" animations={[transition({ duration: 800, from: 35 })]} />
+                <DatePicker value={value} onChange={event => dateChangeHandler(event)} calendar={persian} locale={persian_fa} className="bg-dark Animation" />
             </div>
         </div>
     )
