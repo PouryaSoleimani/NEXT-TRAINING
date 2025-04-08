c5146c1dcd63fb947ad76cd914d6391226eca5e7
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-08 12:32:00 +0330

    REFACTOR

--- pages/htmlcss-review/css-review.tsx
+++ pages/htmlcss-review/css-review.tsx
@@ -7,11 +7,13 @@ import "./cssreview.module.css"
 
 // COMPONENT ========================================================================================================================================================================
 const CCSReviewComponent = () => {
-  return (
-    <div className='w-screen h-screen text-white'>
+    return (
+
+        <div className='w-screen h-screen text-white'>
             <h1 className='bg-blue-900 text-center py-1 font-bold'>CCS REVIEW</h1>
-    </div>
-  )
+        </div>
+
+    )
 }
 
 export default CCSReviewComponent
