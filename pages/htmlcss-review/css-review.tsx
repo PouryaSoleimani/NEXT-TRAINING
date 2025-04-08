2105fc0e46507cf26738b1349e1b044f9d13937e
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-09 01:00:14 +0330

    REVIEW

--- pages/htmlcss-review/css-review.tsx
+++ pages/htmlcss-review/css-review.tsx
@@ -19,7 +19,7 @@ const CCSReviewComponent = () => {
     // RETURN ============================================================================================================================================================
     return (
         <div className='min-h-screen bg-gradient-to-br from-black via-gray-900 to-black text-white'>
-             
+
             <motion.header initial={{ opacity: 0, y: -50 }} animate={{ opacity: 1, y: 0 }} className='bg-black/80 backdrop-blur-lg p-6 sticky top-0 z-50 border-b border-gray-800' >
                 <h1 className='text-4xl font-black text-white text-center'> CSS REVIEW </h1>
             </motion.header>
@@ -32,10 +32,6 @@ const CCSReviewComponent = () => {
                         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium mollitia deserunt commodi eligendi in ut vero, nobis quaerat, libero illum magnam non blanditiis neque ipsa exercitationem! Officiis itaque sit iusto. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur eaque culpa obcaecati explicabo nisi repellat temporibus, suscipit quia impedit aperiam velit voluptatum officiis laboriosam. Enim dignissimos ut natus odio laborum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ex blanditiis, nesciunt voluptas rem assumenda unde adipisci minima ullam quasi tempore doloremque aspernatur sunt explicabo aliquam, molestias numquam fugit odio!
                     </p>
                 </div>
-
-
-
-
             </main>
         </div>
     )
