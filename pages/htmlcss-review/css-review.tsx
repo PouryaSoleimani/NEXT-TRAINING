18a23baf64da4cc7584d588b6a2d69cdb84ae683
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-08 12:23:19 +0330

    REFACTOR

---
+++ pages/htmlcss-review/css-review.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./cssreview.module.css"
+
+
+
+
+// COMPONENT ===================================================================================================================================================================
+const CCSReviewComponent = () => {
+  return (
+    <div>CCSReviewComponent</div>
+  )
+}
+
+export default CCSReviewComponent
