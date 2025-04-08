ab2a01c1e602cff7a6c7c173230dc336cf6cabb7
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-08 18:28:05 +0330

    REVIEW

--- pages/htmlcss-review/css-review.tsx
+++ pages/htmlcss-review/css-review.tsx
@@ -12,6 +12,18 @@ const CCSReviewComponent = () => {
         <div className='w-screen h-screen text-white'>
             <h1 className='bg-blue-900 text-center py-1 font-bold'>CCS REVIEW</h1>
             <p className={styles.text}>HELLO WOLRD</p>
+
+            <div className='bg-zinc-800'>
+                <h1 className='myTitle'>Hello World</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+            </div>
         </div>
 
     )
