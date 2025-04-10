61361c6d48002408c7742bcc2d051e5ed908d044
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-10 11:55:38 +0330

    REVIEW

--- pages/htmlcss-review/ProductBoxes.tsx
+++ pages/htmlcss-review/ProductBoxes.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ProductBoxes = () => {
     return (
-        <div>
+        <div className='w-screen h-screen flex justify-center item-center bg-gradient-to-br from-zinc-900 via-zinc-950 to-black'>
 
         </div>
     )
