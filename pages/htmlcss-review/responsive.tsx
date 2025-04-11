1812003b66a4c09ca6fcaf69d092d2b8ac56108f
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-11 17:39:22 +0330

    SHADCN

--- pages/htmlcss-review/responsive.tsx
+++ pages/htmlcss-review/responsive.tsx
@@ -11,8 +11,8 @@ import React from 'react'
 
 function responsive() {
     return (
-        <div className='w-screen flex items-center justify-center overflow-x-hidden'>
-            <nav className='w-full  sticky top-0 left-0 right-0 bg-zinc-700 p-2 flex items-baseline justify-between'>
+        <div className='w-screen flex items-center justify-center overflow-x-hidden z-0'>
+            <nav className='w-full   bg-zinc-700 p-2 flex items-baseline justify-between z-50'>
                 <Image src="/next.svg" width={60} height={50} alt="logo" />
                 <h1 id="HEADER___TITLE" className='text-lg text-white font-bold tracking-tighter leading-10 m-0 hidden sm:block'>RESPONSIVE DESIGN</h1>
                 <DropDownMenuComponent />
