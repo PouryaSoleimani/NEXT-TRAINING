e00af583c9c66a08984c8159949a1323fc4e91c8
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-11 21:03:09 +0330

    SHAD____CN

--- COMPONENTS/CAROUSEL/CarouselComponent.tsx
+++ COMPONENTS/CAROUSEL/CarouselComponent.tsx
@@ -3,6 +3,7 @@ import * as React from "react"
 "use client"
 import { Card, CardContent } from "@/COMPONENTS/ui/card"
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious, } from "@/COMPONENTS/ui/carousel"
+import Image from "next/image"
 
 
 
@@ -11,14 +12,14 @@ export function CarouselComponent() {
     return (
 
         <section className="w-full">
-            <Carousel opts={{ align: "start", }} className="w-full p-3 sm:p-5 max-w-3xl mx-auto">
+            <Carousel opts={{ align: "start", loop: true }} className="w-full p-3 sm:p-5 max-w-3xl mx-auto">
                 <CarouselContent>
-                    {Array.from({ length: 15 }).map((_, index) => (
+                    {Array.from({ length: 4 }).map((_, index) => (
                         <CarouselItem key={index} className="md:basis-1/3 lg:basis-1/4">
                             <div className="p-1">
                                 <Card>
-                                    <CardContent className="flex aspect-square items-center justify-center p-6">
-                                        <p className="text-3xl font-semibold">{index + 1}</p>
+                                    <CardContent className="flex aspect-square items-center justify-center p-0">
+                                        <Image width={200} height={200} src={`/NEW___PICS/PRODUCTS/product___${index + 1}.webp`} alt="product--image" />
                                     </CardContent>
                                 </Card>
                             </div>
--- COMPONENTS/HEADER/NavigationMenuComponent.tsx
+++ COMPONENTS/HEADER/NavigationMenuComponent.tsx
@@ -69,6 +69,7 @@ export function NavigationMenuComponent() {
                     </Link>
                 </NavigationMenuItem>
 
+
             </NavigationMenuList>
         </NavigationMenu>
     )
