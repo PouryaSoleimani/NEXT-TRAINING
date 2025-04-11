558b4459ab1e90c1ea179712ac3d94ca3f42d97a
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-11 20:48:29 +0330

    SHAD___CN

--- COMPONENTS/HEADER/NavBarComponent.tsx
+++ COMPONENTS/HEADER/NavBarComponent.tsx
@@ -1,3 +1,4 @@
+//^ NAVBAR COMPONENT ================================================================================================================
 import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
 import DropDownMenuComponent from './DropDownMenuComponent'
@@ -7,17 +8,8 @@ import { NavigationMenuComponent } from './NavigationMenuComponent'
 // COMPONENT ================================================================================================================
 function NavBarComponent() {
 
-    useEffect(() => {
-        const handleScroll = () => { const currentScroll = window.scrollY; };
-
-        window.addEventListener('scroll', handleScroll);
-        return () => { window.removeEventListener('scroll', handleScroll); };
-
-    }, []);
-
-
     return (
-        <nav className='sticky top-0 left-0 bg-zinc-700 px-2 py-2 sm:py-0 flex items-center justify-between z-50'>
+        <nav className='sticky top-0 left-0 bg-zinc-700 px-2 py-2 sm:py-0 flex items-center justify-between  z-50'>
             <Image src="/next.svg" width={150} height={80} alt="logo" className='h-full' />
             <h1 id="HEADER___TITLE" className='text-3xl text-white font-bold tracking-tighter m-0 hidden lg:block lg:w-full text-center'>RESPONSIVE DESIGN PAGE</h1>
             <DropDownMenuComponent />
