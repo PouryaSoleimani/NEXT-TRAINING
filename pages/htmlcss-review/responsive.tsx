084cb1afcd2dd69897cb0dee02fb4d352d02b847
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-11 16:31:45 +0330

    CSS REVIEW

---
+++ pages/htmlcss-review/responsive.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function responsive() {
+    return (
+        <div className='w-screen overflow-x-hidden'>
+            <nav className='w-full sticky top-0 left-0 right-0'>
+                <h1 id="HEADER___TITLE" className='text-md'>RESPONSIVE DESIGN</h1>
+            </nav>
+        </div>
+    )
+}
+
+export default responsive
