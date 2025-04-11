0744721e2cd86a57ecda8a469543db6ec1661793
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-11 16:34:05 +0330

    CSS REVIEW

--- pages/htmlcss-review/responsive.tsx
+++ pages/htmlcss-review/responsive.tsx
@@ -1,10 +1,12 @@
+import Image from 'next/image'
 import React from 'react'
 
 function responsive() {
     return (
         <div className='w-screen overflow-x-hidden'>
-            <nav className='w-full sticky top-0 left-0 right-0'>
-                <h1 id="HEADER___TITLE" className='text-md'>RESPONSIVE DESIGN</h1>
+            <nav className='w-full sticky top-0 left-0 right-0 bg-zinc-700 p-2 flex items-center justify-between'>
+                <Image src="/next.svg" width={50} height={50} alt="logo" />
+                <h1 id="HEADER___TITLE" className='text-xs text-white font-bold tracking-tighter'>RESPONSIVE DESIGN</h1>
             </nav>
         </div>
     )
