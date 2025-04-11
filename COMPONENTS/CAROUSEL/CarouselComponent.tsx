aa02f4bd0dca1c4a5cbd55ffb28a508c65931004
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-12 01:30:39 +0330

    SHAD___CN

--- COMPONENTS/CAROUSEL/CarouselComponent.tsx
+++ COMPONENTS/CAROUSEL/CarouselComponent.tsx
@@ -7,16 +7,16 @@ import Image from "next/image"
 
 
 
-
+//  COMPOENENT ==================================================================================================================================
 export function CarouselComponent() {
     return (
 
         <section className="w-full">
-            <Carousel opts={{ align: "start", loop: true }} className="w-full p-5 sm:p-3 max-w-6xl mx-auto">
+            <Carousel opts={{ align: "start", loop: true , }} className="w-full p-5 sm:p-3 max-w-max mx-auto">
                 <CarouselContent>
 
                     {Array.from({ length: 4 }).map((_, index) => (
-                        <CarouselItem key={index} className="sm:basis-1/2 md:basis-1/3 lg:basis-1/4">
+                        <CarouselItem key={index} className="sm:basis-1/2 lg:basis-1/3 xl:basis-1/4">
                             <div className="p-1">
                                 <Card className="w-full h-full">
                                     <CardContent className="flex aspect-square items-center justify-center p-0 w-full h-full">
