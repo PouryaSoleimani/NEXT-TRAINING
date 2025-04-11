6e9465c625d42c180ec1ce283ff382a3f1b08b18
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-11 17:54:34 +0330

    SHADCN

--- COMPONENTS/HEADER/NavigationMenuComponent.tsx
+++ COMPONENTS/HEADER/NavigationMenuComponent.tsx
@@ -51,18 +51,15 @@ export function NavigationMenuComponent() {
         <NavigationMenu className="hidden sm:block">
             <NavigationMenuList>
                 <NavigationMenuItem>
-                    <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+                    <NavigationMenuTrigger> Getting started</NavigationMenuTrigger>
                     <NavigationMenuContent>
                         <ul className="grid gap-3 p-1 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
                             <li className="row-span-3">
-                                <NavigationMenuLink asChild>
-                                    <a
-                                        className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                                        href="/"
-                                    >
-                                        <IoLogoApple />
-                                        <div className="mb-2 mt-4 text-lg font-medium">
-                                            shadcn/ui
+                                <NavigationMenuLink>
+                                    <a className="flex h-full w-full hover:bg-zinc-800 duration-300 select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-3 no-underline outline-none focus:shadow-md" href="/"  >
+
+                                        <div className="mb-2 mt-4 text-lg font-medium flex items-center justify-start gap-x-1">
+                                            <IoLogoApple className="w-6 h-6" />  shadcn/ui
                                         </div>
                                         <p className="text-sm leading-tight text-muted-foreground">
                                             Beautifully designed components built with Radix UI and
@@ -115,7 +112,7 @@ const ListItem = React.forwardRef<React.ElementRef<"a">, React.ComponentPropsWit
             <NavigationMenuLink asChild>
                 <a
                     ref={ref}
-                    className={cn("block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+                    className={cn("block hover:bg-zinc-800 duration-300 select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors focus:bg-accent focus:text-accent-foreground",
                         className
                     )}
                     {...props}
