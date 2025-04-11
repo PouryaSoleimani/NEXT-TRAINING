bd8f56105bdf244f994476588a084add29d177a4
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-11 18:41:33 +0330

    SHADCN

--- COMPONENTS/HEADER/NavigationMenuComponent.tsx
+++ COMPONENTS/HEADER/NavigationMenuComponent.tsx
@@ -27,23 +27,6 @@ const components: { title: string; href: string; description: string }[] = [
         description:
             "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
     },
-    {
-        title: "Scroll-area",
-        href: "/docs/primitives/scroll-area",
-        description: "Visually or semantically separates content.",
-    },
-    {
-        title: "Tabs",
-        href: "/docs/primitives/tabs",
-        description:
-            "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-    },
-    {
-        title: "Tooltip",
-        href: "/docs/primitives/tooltip",
-        description:
-            "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-    },
 ]
 
 export function NavigationMenuComponent() {
@@ -83,7 +66,7 @@ export function NavigationMenuComponent() {
                 <NavigationMenuItem>
                     <NavigationMenuTrigger>Components</NavigationMenuTrigger>
                     <NavigationMenuContent>
-                        <ul className="grid w-[400px] gap-3 p-3 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                        <ul className="flex flex-col w-[300px] gap-3 p-3 md:w-[300px] md:grid-cols-2 lg:w-[300px] ">
                             {components.map((component) => (
                                 <ListItem key={component.title} title={component.title} href={component.href}    >
                                     {component.description}
@@ -95,9 +78,7 @@ export function NavigationMenuComponent() {
 
                 <NavigationMenuItem>
                     <Link href="/docs" legacyBehavior passHref className="font-bold">
-                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                            Documentation
-                        </NavigationMenuLink>
+                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>  Documentation  </NavigationMenuLink>
                     </Link>
                 </NavigationMenuItem>
 
@@ -112,17 +93,9 @@ const ListItem = React.forwardRef<React.ElementRef<"a">, React.ComponentPropsWit
     return (
         <li>
             <NavigationMenuLink asChild>
-                <a
-                    ref={ref}
-                    className={cn("block hover:bg-zinc-800 duration-300 select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors focus:bg-accent focus:text-accent-foreground",
-                        className
-                    )}
-                    {...props}
-                >
+                <a ref={ref} className={cn("block hover:bg-zinc-800 duration-300 select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors focus:bg-accent focus:text-accent-foreground", className)}       {...props}   >
                     <div className="text-sm font-medium leading-none">{title}</div>
-                    <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-                        {children}
-                    </p>
+                    <p className="line-clamp-2 text-sm leading-snug text-muted-foreground"> {children} </p>
                 </a>
             </NavigationMenuLink>
         </li>
