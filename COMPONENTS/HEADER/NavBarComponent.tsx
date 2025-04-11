32dce6a3051446f044cbe5713656ba96f4d73e90
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-11 19:13:44 +0330

    SHAD___CN

--- COMPONENTS/HEADER/NavBarComponent.tsx
+++ COMPONENTS/HEADER/NavBarComponent.tsx
@@ -1,11 +1,16 @@
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import DropDownMenuComponent from './DropDownMenuComponent'
 import { NavigationMenuComponent } from './NavigationMenuComponent'
 
 
 // COMPONENT ================================================================================================================
 function NavBarComponent() {
+
+    const [isScroll, setIsScroll] = useState(false)
+
+
+
     return (
         <nav className='w-full sticky top-0 left-0 bg-zinc-700 px-2 py-2 sm:py-0 flex items-center justify-between z-50'>
             <Image src="/next.svg" width={150} height={80} alt="logo" className='h-full object-cover' />
