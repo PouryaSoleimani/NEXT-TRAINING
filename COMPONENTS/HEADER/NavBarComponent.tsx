8afd7930859550d86e5e75b70cbc9f33a6273d20
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-11 17:49:15 +0330

    SHADCN

--- COMPONENTS/HEADER/NavBarComponent.tsx
+++ COMPONENTS/HEADER/NavBarComponent.tsx
@@ -7,9 +7,9 @@ import { NavigationMenuComponent } from './NavigationMenuComponent'
 // COMPONENT ================================================================================================================
 function NavBarComponent() {
     return (
-        <nav className='w-full sticky top-0 left-0   bg-zinc-700 p-2 flex items-baseline justify-between z-50'>
+        <nav className='w-full sticky top-0 left-0 bg-zinc-700 p-2 flex items-baseline justify-between z-50'>
             <Image src="/next.svg" width={60} height={50} alt="logo" />
-            <h1 id="HEADER___TITLE" className='text-lg text-white font-bold tracking-tighter leading-10 m-0 hidden sm:block'>RESPONSIVE DESIGN</h1>
+            <h1 id="HEADER___TITLE" className='text-lg text-white font-bold tracking-tighter leading-10 m-0 hidden lg:block'>RESPONSIVE DESIGN</h1>
             <DropDownMenuComponent />
             <NavigationMenuComponent />
         </nav>
