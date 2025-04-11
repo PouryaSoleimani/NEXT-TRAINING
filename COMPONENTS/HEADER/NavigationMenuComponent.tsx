4426cd46b9733d0eed6a36bf015a4e28032aeb1c
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-11 17:32:09 +0330

    SHADCN

---
+++ COMPONENTS/HEADER/NavigationMenuComponent.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuIndicator,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    NavigationMenuViewport,
+} from "@/COMPONENTS/ui/navigation-menu"
+
+
+function NavigationMenuComponent() {
+    return (
+        <NavigationMenu>
+            <NavigationMenuList>
+                <NavigationMenuItem>
+                    <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                        <NavigationMenuLink>Link</NavigationMenuLink>
+                    </NavigationMenuContent>
+                </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
+    )
+}
+
+export default NavigationMenuComponent
