0fdd90a750d5c4030dc2b72691fa0f1f74d759de
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-12 05:26:05 +0330

    🟡 PRODUCTS

--- COMPONENTS/PRODUCTS/ProductsWrapperComponent.tsx
+++ COMPONENTS/PRODUCTS/ProductsWrapperComponent.tsx
@@ -3,7 +3,7 @@ import SkeletonComponent from '../SKELETON/SkeletonComponent'
 
 function ProductsWrapperComponent() {
     return (
-        <div className='w-full h-full flex items-center gap-5 border-8 border-red-900 bg-black z-50'>
+        <div className='w-full h-full flex items-center justify-center gap-5  bg-black z-50 p-4 lg:p-10 flex-wrap'>
             {Array.from({ length: 10 }).fill("").map((item: any) => (
                 <SkeletonComponent />
             ))}
