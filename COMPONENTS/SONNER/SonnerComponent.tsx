fa6916e229fbf8b0b4203b5950a5f55e94b5db86
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-12 06:16:56 +0330

    🟡 PRODUCTS

--- COMPONENTS/SONNER/SonnerComponent.tsx
+++ COMPONENTS/SONNER/SonnerComponent.tsx
@@ -8,6 +8,8 @@ function SonnerComponent() {
     function toastHandler() {
         toast("Event has been created", { description: "Sunday, December 03, 2023 at 9:00 AM", action: { label: "Undo", onClick: () => console.log("Undo"), }, })
     }
+
+
     return (
         <>
             <Toaster />
