74dda81b39846c742993a08ddd39a422238d1e0f
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-12 14:50:08 +0330

    ⬛ SHAD____CN ⬛

---
+++ COMPONENTS/PAGINATION/PaginationComponent.tsx
@@ -0,0 +1,33 @@
+import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious, } from "@/COMPONENTS/ui/pagination"
+
+
+
+export function PaginationComponent() {
+
+    return (
+        <Pagination>
+            <PaginationContent>
+                <PaginationItem>
+                    <PaginationPrevious href="#" />
+                </PaginationItem>
+                <PaginationItem>
+                    <PaginationLink href="#">1</PaginationLink>
+                </PaginationItem>
+                <PaginationItem>
+                    <PaginationLink href="#" isActive>
+                        2
+                    </PaginationLink>
+                </PaginationItem>
+                <PaginationItem>
+                    <PaginationLink href="#">3</PaginationLink>
+                </PaginationItem>
+                <PaginationItem>
+                    <PaginationEllipsis />
+                </PaginationItem>
+                <PaginationItem>
+                    <PaginationNext href="#" />
+                </PaginationItem>
+            </PaginationContent>
+        </Pagination>
+    )
+}
