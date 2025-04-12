17255a1d810b94c2edc54fe1297b5fa970cb0d8a
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-12 06:41:40 +0330

    🟡 PRODUCTS

--- COMPONENTS/PRODUCTS/ProductsWrapperComponent.tsx
+++ COMPONENTS/PRODUCTS/ProductsWrapperComponent.tsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import SkeletonComponent from '../SKELETON/SkeletonComponent'
-
+import React from "react";
+import SkeletonComponent from "../SKELETON/SkeletonComponent";
 
 // COMPONENT
 function ProductsWrapperComponent() {
-    return (
-        <>
-            <div className='w-full h-full grid grid-cols-1 sm:grid-cols-2 place-items-center justify-items-center place-content-center md:grid-cols-3 lg:grid-cols-4 gap-3 bg-black z-50 p-4 lg:p-10'>
-                {Array.from({ length: 12 }).fill("").map((item: any) => (
-                    <SkeletonComponent />
-                ))}
-            </div>
-
-        </>
-    )
+  return (
+    <>
+      <div className="w-full h-full grid grid-cols-1 sm:grid-cols-2 place-items-center justify-items-center place-content-center md:grid-cols-3 lg:grid-cols-4 gap-3 bg-black z-50 p-4 lg:p-10">
+        {Array.from({length: 12})
+          .fill("")
+          .map((item: any) => (
+            <SkeletonComponent />
+          ))}
+      </div>
+    </>
+  );
 }
 
-export default ProductsWrapperComponent
+export default ProductsWrapperComponent;
---
+++ COMPONENTS/TABS/TabsComponent.tsx
@@ -0,0 +1,15 @@
+// ^ TABS COMPONENT ====================================================================================================================================
+import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/COMPONENTS/ui/tabs";
+
+export function TabsComponent() {
+  return (
+    <Tabs defaultValue="account" className="w-[400px]">
+      <TabsList>
+        <TabsTrigger value="account">Account</TabsTrigger>
+        <TabsTrigger value="password">Password</TabsTrigger>
+      </TabsList>
+      <TabsContent value="account">Make changes to your account here.</TabsContent>
+      <TabsContent value="password">Change your password here.</TabsContent>
+    </Tabs>
+  );
+}
