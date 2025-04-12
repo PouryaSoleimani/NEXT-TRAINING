7597c60b2a71494bf0e6f9f07521c4f01125156c
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-12 15:34:02 +0330

    ⬛ SHAD____CN ⬛

--- COMPONENTS/RADIOGROUP/RadioGroupComponent.tsx
+++ COMPONENTS/RADIOGROUP/RadioGroupComponent.tsx
@@ -6,21 +6,22 @@ import { RadioGroup, RadioGroupItem } from "@/COMPONENTS/ui/radio-group"
 
 function RadioGroupComponent() {
     return (
-
-        <RadioGroup defaultValue="comfortable">
-            <div className="flex items-center space-x-2">
-                <RadioGroupItem value="default" id="r1" />
-                <Label htmlFor="r1">Default</Label>
-            </div>
-            <div className="flex items-center space-x-2">
-                <RadioGroupItem value="comfortable" id="r2" />
-                <Label htmlFor="r2">Comfortable</Label>
-            </div>
-            <div className="flex items-center space-x-2">
-                <RadioGroupItem value="compact" id="r3" />
-                <Label htmlFor="r3">Compact</Label>
-            </div>
-        </RadioGroup>
+        <section className='flex items-center justify-center mt-10 text-white'>
+            <RadioGroup defaultValue="compact">
+                <div className="flex items-center space-x-2 text-white">
+                    <RadioGroupItem value="default" id="r1" />
+                    <Label htmlFor="r1">Default</Label>
+                </div>
+                <div className="flex items-center space-x-2 text-white">
+                    <RadioGroupItem value="comfortable" id="r2" />
+                    <Label htmlFor="r2">Comfortable</Label>
+                </div>
+                <div className="flex items-center space-x-2 text-white">
+                    <RadioGroupItem value="compact" id="r3" />
+                    <Label htmlFor="r3">Compact</Label>
+                </div>
+            </RadioGroup>
+        </section>
 
     )
 }
