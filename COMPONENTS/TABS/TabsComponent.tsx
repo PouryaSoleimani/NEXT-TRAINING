7e07d6c5ef8cdf4b9b4b0b19ca7e86b0b5b0d8c6
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-12 06:48:36 +0330

    🟡 PRODUCTS

--- COMPONENTS/TABS/TabsComponent.tsx
+++ COMPONENTS/TABS/TabsComponent.tsx
@@ -3,13 +3,19 @@ import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/COMPONENTS/ui/tabs";
 
 export function TabsComponent() {
   return (
-    <Tabs defaultValue="account" className="w-[400px]">
-      <TabsList>
-        <TabsTrigger value="account">Account</TabsTrigger>
-        <TabsTrigger value="password">Password</TabsTrigger>
-      </TabsList>
-      <TabsContent value="account">Make changes to your account here.</TabsContent>
-      <TabsContent value="password">Change your password here.</TabsContent>
-    </Tabs>
+    <div className="w-full flex items-center justify-center py-4 mx-auto">
+      <Tabs defaultValue="account" className="w-full sm:w-2/3 bg-zinc-100 mx-auto">
+        <TabsList className="bg-zinc-300 text-white w-full py-4">
+          <TabsTrigger value="account" className=" text-black text-xl  p-2">
+            Account
+          </TabsTrigger>
+          <TabsTrigger value="password" className="text-black text-xl  p-2">
+            Password
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">Make changes to your account here.</TabsContent>
+        <TabsContent value="password">Change your password here.</TabsContent>
+      </Tabs>
+    </div>
   );
 }
--- pages/htmlcss-review/responsive.tsx
+++ pages/htmlcss-review/responsive.tsx
@@ -1,28 +1,28 @@
 //^ RESPONSIVE PAGE =====================================================================================================================================
-import { CarouselComponent } from "@/COMPONENTS/CAROUSEL/CarouselComponent"
-import DrawerComponent from "@/COMPONENTS/DRAWER/DrawerComponent"
-import NavBarComponent from "@/COMPONENTS/HEADER/NavBarComponent"
-import ProductsWrapperComponent from "@/COMPONENTS/PRODUCTS/ProductsWrapperComponent"
-import SonnerComponent from "@/COMPONENTS/SONNER/SonnerComponent"
-import { Separator } from "@radix-ui/react-dropdown-menu"
-import React from 'react'
-
+import {CarouselComponent} from "@/COMPONENTS/CAROUSEL/CarouselComponent";
+import DrawerComponent from "@/COMPONENTS/DRAWER/DrawerComponent";
+import NavBarComponent from "@/COMPONENTS/HEADER/NavBarComponent";
+import ProductsWrapperComponent from "@/COMPONENTS/PRODUCTS/ProductsWrapperComponent";
+import SonnerComponent from "@/COMPONENTS/SONNER/SonnerComponent";
+import {TabsComponent} from "@/COMPONENTS/TABS/TabsComponent";
+import {Separator} from "@radix-ui/react-dropdown-menu";
+import React from "react";
 
 // COMPONENT==== =====================================================================================================================================
 function responsivePage() {
-
-    return (
-        <div className='w-full min-h-screen relative z-0'>
-            <NavBarComponent />
-            <CarouselComponent />
-            <Separator className='text-white bg-zinc-800 h-0.5 w-2/3 mx-auto my-5' />
-            <DrawerComponent />
-            <Separator className='text-white bg-zinc-800 h-0.5 w-2/3 mx-auto my-5' />
-            <ProductsWrapperComponent />
-            <SonnerComponent />
-            <div id="OVERLAY" className="h-[1500px] w-full"></div>
-        </div>
-    )
+  return (
+    <div className="w-full min-h-screen relative z-0">
+      <NavBarComponent />
+      <CarouselComponent />
+      <Separator className="text-white bg-zinc-800 h-0.5 w-2/3 mx-auto my-5" />
+      <DrawerComponent />
+      <Separator className="text-white bg-zinc-800 h-0.5 w-2/3 mx-auto my-5" />
+      <ProductsWrapperComponent />
+      <SonnerComponent />
+      <TabsComponent />
+      <div id="OVERLAY" className="h-[1500px] w-full"></div>
+    </div>
+  );
 }
 
-export default responsivePage
+export default responsivePage;
