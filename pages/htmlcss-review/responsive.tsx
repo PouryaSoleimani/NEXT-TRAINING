c411f4d872d6c44294e5b84727f54c90cab9ff36
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-12 05:30:14 +0330

    🟡 PRODUCTS

--- pages/htmlcss-review/responsive.tsx
+++ pages/htmlcss-review/responsive.tsx
@@ -14,8 +14,9 @@ function responsivePage() {
         <div className='w-full min-h-screen relative z-0'>
             <NavBarComponent />
             <CarouselComponent />
+            <Separator className='text-white bg-zinc-800 h-0.5 w-2/3 mx-auto my-5' />
             <DrawerComponent />
-            <Separator className="my-4 bg-zinc-400" />
+            <Separator className='text-white bg-zinc-800 h-0.5 w-2/3 mx-auto my-5' />
             <ProductsWrapperComponent />
             <div id="OVERLAY" className="h-[2000px] w-full"></div>
         </div>
