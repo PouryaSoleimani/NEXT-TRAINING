869171715afb8dd93e10ed44ef85dc102305ddb6
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-12 05:29:20 +0330

    🟡 PRODUCTS

--- COMPONENTS/PRODUCTS/ProductsWrapperComponent.tsx
+++ COMPONENTS/PRODUCTS/ProductsWrapperComponent.tsx
@@ -5,7 +5,7 @@ import { Separator } from '@radix-ui/react-dropdown-menu'
 function ProductsWrapperComponent() {
     return (
         <>
-            <Separator />
+            <Separator className='text-white bg-zinc-800 h-0.5 w-2/3 mx-auto'/>
             <div className='w-full h-full flex items-center justify-center gap-5  bg-black z-50 p-4 lg:p-10 flex-wrap'>
                 {Array.from({ length: 10 }).fill("").map((item: any) => (
                     <SkeletonComponent />
