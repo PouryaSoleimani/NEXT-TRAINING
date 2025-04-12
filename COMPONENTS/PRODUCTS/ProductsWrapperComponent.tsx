eeaef0bec1b5ab0721eb6a9fb646259ae6595f41
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-12 06:32:04 +0330

    🟡 PRODUCTS

--- COMPONENTS/PRODUCTS/ProductsWrapperComponent.tsx
+++ COMPONENTS/PRODUCTS/ProductsWrapperComponent.tsx
@@ -8,9 +8,7 @@ function ProductsWrapperComponent() {
         <>
             <div className='w-full h-full grid grid-cols-1 sm:grid-cols-2 place-items-center justify-items-center place-content-center md:grid-cols-3 lg:grid-cols-4 gap-3 bg-black z-50 p-4 lg:p-10'>
                 {Array.from({ length: 12 }).fill("").map((item: any) => (
-
                     <SkeletonComponent />
-
                 ))}
             </div>
         </>
