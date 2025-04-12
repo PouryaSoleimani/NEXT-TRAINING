0fbc7bd1f25e4b92f9d9a433367bcce2355891bc
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-12 06:18:08 +0330

    🟡 PRODUCTS

--- COMPONENTS/SONNER/SonnerComponent.tsx
+++ COMPONENTS/SONNER/SonnerComponent.tsx
@@ -14,11 +14,7 @@ function SonnerComponent() {
         <>
             <Toaster />
             <div className="w-full flex items-center justify-center py-5">
-                <Button
-                    variant="outline"
-                    onClick={() => toastHandler}
-                    className="text-white  font-semibold px-4 py-8 leading-tight tracking-tighter m-0 rounded border-zinc-800 flex text-base"
-                >
+                <Button variant="outline" onClick={() => toastHandler()} className="text-white  font-semibold px-4 py-8 leading-tight tracking-tighter m-0 rounded border-zinc-800 flex text-base" >
                     Show Toast
                 </Button>
             </div>
