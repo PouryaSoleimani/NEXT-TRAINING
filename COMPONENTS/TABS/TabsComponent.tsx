3dc085e95f7a8c024aa866f9bfe57633fb4ad1c3
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-12 06:54:40 +0330

    🟡 PRODUCTS

--- COMPONENTS/TABS/TabsComponent.tsx
+++ COMPONENTS/TABS/TabsComponent.tsx
@@ -1,21 +1,20 @@
 // ^ TABS COMPONENT ====================================================================================================================================
-import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/COMPONENTS/ui/tabs";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/COMPONENTS/ui/tabs";
 
 export function TabsComponent() {
-  return (
-    <div className="w-full flex items-center justify-center py-4 mx-auto">
-      <Tabs defaultValue="account" className="w-full sm:w-2/3 bg-zinc-100 mx-auto">
-        <TabsList className="bg-zinc-300 text-white w-full py-4">
-          <TabsTrigger value="account" className=" text-black text-xl  p-2">
-            Account
-          </TabsTrigger>
-          <TabsTrigger value="password" className="text-black text-xl  p-2">
-            Password
-          </TabsTrigger>
-        </TabsList>
-        <TabsContent value="account">Make changes to your account here.</TabsContent>
-        <TabsContent value="password">Change your password here.</TabsContent>
-      </Tabs>
-    </div>
-  );
+    return (
+        <div className="w-full flex items-center justify-center py-4 mx-auto">
+            <Tabs defaultValue="account" className="w-full sm:w-2/3 bg-zinc-100 mx-auto rounded">
+
+                <TabsList className="bg-zinc-300 text-white w-full">
+                    <TabsTrigger value="account" className=" text-black text-xl  p-2"> Account </TabsTrigger>
+                    <TabsTrigger value="password" className="text-black text-xl  p-2"> Password </TabsTrigger>
+                </TabsList>
+
+                <TabsContent value="account" className="h-[20rem] p-3">Make changes to your account here.</TabsContent>
+                <TabsContent value="password" className="h-[20rem] p-3">Change your password here.</TabsContent>
+
+            </Tabs>
+        </div>
+    );
 }
--- COMPONENTS/ui/tabs.tsx
+++ COMPONENTS/ui/tabs.tsx
@@ -3,29 +3,22 @@
 import * as React from "react";
 import * as TabsPrimitive from "@radix-ui/react-tabs";
 
-import {cn} from "@/lib/utils";
+import { cn } from "@/lib/utils";
 
 const Tabs = TabsPrimitive.Root;
 
 const TabsList = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.List>,
   React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>
->(({className, ...props}, ref) => (
-  <TabsPrimitive.List
-    ref={ref}
-    className={cn(
-      "inline-flex h-9 items-center justify-center rounded-lg bg-muted p-1 text-muted-foreground",
-      className
-    )}
-    {...props}
-  />
+>(({ className, ...props }, ref) => (
+  <TabsPrimitive.List ref={ref} className={cn("inline-flex h-16 items-center justify-center rounded bg-zinc-200 p-1", className)}   {...props} />
 ));
 TabsList.displayName = TabsPrimitive.List.displayName;
 
 const TabsTrigger = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.Trigger>,
   React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
->(({className, ...props}, ref) => (
+>(({ className, ...props }, ref) => (
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
@@ -40,7 +33,7 @@ TabsTrigger.displayName = TabsPrimitive.Trigger.displayName;
 const TabsContent = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof TabsPrimitive.Content>
->(({className, ...props}, ref) => (
+>(({ className, ...props }, ref) => (
   <TabsPrimitive.Content
     ref={ref}
     className={cn(
@@ -52,4 +45,4 @@ const TabsContent = React.forwardRef<
 ));
 TabsContent.displayName = TabsPrimitive.Content.displayName;
 
-export {Tabs, TabsList, TabsTrigger, TabsContent};
+export { Tabs, TabsList, TabsTrigger, TabsContent };
