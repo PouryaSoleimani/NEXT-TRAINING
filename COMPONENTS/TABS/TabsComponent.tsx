7c7e67757b9525b4d28192b6a7245df57bd92241
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-12 14:44:03 +0330

    ⬛ SHAD____CN ⬛

--- COMPONENTS/TABS/TabsComponent.tsx
+++ COMPONENTS/TABS/TabsComponent.tsx
@@ -6,13 +6,17 @@ export function TabsComponent() {
         <div className="w-full flex items-center justify-center py-4 mx-auto">
             <Tabs defaultValue="account" className="w-full sm:w-2/3 bg-zinc-100 mx-auto rounded">
 
-                <TabsList className="bg-zinc-300 text-white w-full">
-                    <TabsTrigger value="account" className=" text-black text-xl  p-2"> Account </TabsTrigger>
-                    <TabsTrigger value="password" className="text-black text-xl  p-2"> Password </TabsTrigger>
+                <TabsList className="bg-zinc-700 text-white w-full">
+                    <TabsTrigger value="inbox" className=" text-black text-xl  p-2 font-black"> Inbox </TabsTrigger>
+                    <TabsTrigger value="account" className=" text-black text-xl  p-2 font-black"> Account </TabsTrigger>
+                    <TabsTrigger value="dashboard" className=" text-black text-xl  p-2 font-black"> Dashboard </TabsTrigger>
+                    <TabsTrigger value="password" className="text-black text-xl  p-2 font-black"> Settings </TabsTrigger>
                 </TabsList>
 
-                <TabsContent value="account" className="h-[20rem] p-3">Make changes to your account here.</TabsContent>
-                <TabsContent value="password" className="h-[20rem] p-3">Change your password here.</TabsContent>
+                <TabsContent value="inbox" className="h-[20rem] p-3 bg-zinc-900 text-white text-center font-black text-6xl">INBOX</TabsContent>
+                <TabsContent value="account" className="h-[20rem] p-3 bg-zinc-900 text-white text-center font-black text-6xl">ACCOUNT</TabsContent>
+                <TabsContent value="dashboard" className="h-[20rem] p-3 bg-zinc-900 text-white text-center font-black text-6xl">DASHBOARD</TabsContent>
+                <TabsContent value="password" className="h-[20rem] p-3 bg-zinc-900 text-white text-center font-black text-6xl">PASSWORD</TabsContent>
 
             </Tabs>
         </div>
