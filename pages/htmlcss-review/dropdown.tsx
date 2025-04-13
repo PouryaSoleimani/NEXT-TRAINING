05f261f78f7455a5a98eb8d6fb478fe9961c6f10
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-13 09:09:25 +0330

    ⬛ SHAD____CN ⬛

--- pages/htmlcss-review/dropdown.tsx
+++ pages/htmlcss-review/dropdown.tsx
@@ -7,11 +7,11 @@ function DropDownPage() {
         <div className='w-screen h-screen flex  items-center justify-center bg-gradient-to-br from-zinc-900 via-zinc-950 to-black'>
 
             <div className='w-fit h-fit relative'>
-                <button className='w-64 justify-center py-1.5 bg-blue-800 hover:bg-blue-900 duration-200 rounded text-2xl font-semibold tracking-tight text-white flex items-center'>
+                <button id='DROPDOWN____BTN' className='w-64 justify-center py-1.5 bg-blue-800 hover:bg-blue-900 duration-200 rounded text-2xl font-semibold tracking-tight text-white flex items-center'>
                     Toggle
-                    <ChevronDown className='translate-y-0.5' />
+                    <ChevronDown id='DROPDOWN____CHEVRON' className='translate-y-1' />
                 </button>
-                <div className='flex flex-col bg-zinc-200 px-[43px] py-1.5 rounded text-xl gap-y-1 items-center absolute top-[40px]'>
+                <div id='DROPDOWN___CONTENT' className='flex flex-col bg-zinc-200 w-64 items-center justify-center py-1.5 rounded text-xl gap-y-1 text-center absolute top-[40px]'>
                     <Link href="#" className='hover:bg-zinc-400 p-1 w-full'>About</Link>
                     <Link href="#" className='hover:bg-zinc-400 p-1 w-full'>Gallery</Link>
                     <Link href="#" className='hover:bg-zinc-400 p-1 w-full'>Products</Link>
