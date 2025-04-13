f44e7ad34db782298ef7dd058a7a8fd65fa4be3b
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-13 09:39:19 +0330

    ⬛ SHAD____CN ⬛

--- pages/htmlcss-review/dropdown.tsx
+++ pages/htmlcss-review/dropdown.tsx
@@ -6,12 +6,12 @@ function DropDownPage() {
     return (
         <div className='w-screen h-screen flex  items-center justify-center bg-gradient-to-br from-zinc-900 via-zinc-950 to-black'>
 
-            <div className='w-fit h-fit relative'>
+            <div id='WRAPPER' className='w-fit h-fit relative'>
                 <button id='DROPDOWN____BTN' className='w-64 justify-center py-2 bg-blue-800 hover:bg-blue-900 duration-200 rounded text-2xl font-semibold tracking-tight text-white flex items-center gap-4'>
                     Toggle
                     <ChevronDown id='DROPDOWN____CHEVRON' className='translate-y-0.5' />
                 </button>
-                <div id='DROPDOWN___CONTENT' className='flex flex-col bg-zinc-200 w-64 items-center justify-center py-1.5 rounded text-xl gap-y-1 text-center absolute top-[40px]'>
+                <div id='DROPDOWN___CONTENT' className='dropdown__container'>
                     <Link href="#" className='hover:bg-zinc-400 p-1 w-full'>About</Link>
                     <Link href="#" className='hover:bg-zinc-400 p-1 w-full'>Gallery</Link>
                     <Link href="#" className='hover:bg-zinc-400 p-1 w-full'>Products</Link>
