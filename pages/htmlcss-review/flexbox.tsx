e01680be0ddd3303ca6d23da0c4af7254c7dd9bb
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-14 00:12:23 +0330

    ⬛ SHAD_____CN ⬛

--- pages/htmlcss-review/flexbox.tsx
+++ pages/htmlcss-review/flexbox.tsx
@@ -2,12 +2,22 @@ import React from 'react'
 
 function FlexBox() {
     return (
-        <div id='FLEX____CONTAINER' className='w-[95%] h-auto py-16 bg-zinc-800 mx-auto mt-16'>
-            <div className="FLEX____ITEM">ABOUT</div>
-            <div className="FLEX____ITEM">GALLERY</div>
-            <div className="FLEX____ITEM">CART</div>
-            <div className="FLEX____ITEM">LOGOUT</div>
-        </div>
+        <>
+            <div id='FLEX____CONTAINER' className='w-[95%] h-[59ren] py-16 bg-zinc-800 mx-auto mt-16'>
+                <div className="FLEX____ITEM">ABOUT</div>
+                <div className="FLEX____ITEM">GALLERY</div>
+                <div className="FLEX____ITEM">CART</div>
+                <div className="FLEX____ITEM">LOGOUT</div>
+            </div>
+
+            <div id="FLEX____CONTAINER2" className='w-[95%] bg-zinc-800 mx-auto mt-16'>
+                <div className="FLEX____ITEM2">ABOUT</div>
+                <div className="FLEX____ITEM2">GALLERY</div>
+                <div className="FLEX____ITEM2">CART</div>
+                <div className="FLEX____ITEM2">LOGOUT</div>
+            </div>
+
+        </>
     )
 }
 
