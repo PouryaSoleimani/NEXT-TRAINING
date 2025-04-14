26704503453faff8c8d76ace8cad1ee6708f8b9e
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-14 11:25:34 +0330

    🟧 FLEXBOX 🟧

--- pages/htmlcss-review/flexbox.tsx
+++ pages/htmlcss-review/flexbox.tsx
@@ -39,8 +39,15 @@ function FlexBox() {
 
             {/* FLEX BASIS */}
             <div id="FLEX____CONTAINER4" className='w-[95%] py-4 bg-zinc-600/30 backdrop-blur-2xl mx-auto mt-16 flex px-6 gap-4 rounded'>
-                <div className="FLEX____ITEM4 basis-1/3"><Image src="/vercel.svg" width={100} height={200} alt='logo' /></div>
-                <div className="FLEX____ITEM4 basis-2/3"><LogIn /><Settings /></div>
+                <div id='NAVBAR___MOBILE' className='flex md:hidden items-center justify-between w-full'>
+                    <div className="FLEX____ITEM4 basis-1/4"><Image src="/vercel.svg" width={100} height={200} alt='logo' /></div>
+                    <div className="FLEX____ITEM4 p-0 justify-end basis-1/4"><LogIn className='bg-black p-2 rounded w-12 h-12' /><Settings className='bg-black p-2 rounded w-12 h-12' /></div>
+                </div>
+                <div id='NAVBAR___DESKTOP' className='hidden md:flex items-center justify-between w-full'>
+                    <div className="FLEX____ITEM4 basis-1/4"><Image src="/vercel.svg" width={120} height={200} alt='logo' /></div>
+                    <div className="FLEX____ITEM4 basis-2/4"><p>Getting Started</p> <p>Products</p> <p>Docs</p></div>
+                    <div className="FLEX____ITEM4 basis-1/4"><LogIn className='bg-black p-2 rounded w-12 h-12' /><Settings className='bg-black p-2 rounded w-12 h-12' /></div>
+                </div>
             </div>
         </>
     )
