b22cf10bd27b7da565589dab87cc62a805498400
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-14 13:15:03 +0330

    🟧 FLEXBOX 🟧

--- pages/htmlcss-review/flexbox.tsx
+++ pages/htmlcss-review/flexbox.tsx
@@ -56,12 +56,12 @@ function FlexBox() {
                     <div className="FLEX____ITEM4 basis-1/4">
                         <IoLogoGithub className="size-16 -translate-x-2 hover:scale-105 duration-300" />
                     </div>
-                    <div className="FLEX____ITEM4 flexitem basis-2/4 text-3xl font-bold flex gap-4 items-end justify-center">
+                    <div className="FLEX____ITEM4 flexitem basis-2/4 text-3xl font-bold flex gap-4 items-center justify-center py-3 -translate-y-1">
                         <p className='hover:underline duration-200 cursor-pointer decoration-blue-900 decoration-4'>Getting Started</p>
                         <p className='hover:underline duration-200 cursor-pointer decoration-blue-900 decoration-4'>Products</p>
                         <p className='hover:underline duration-200 cursor-pointer decoration-blue-900 decoration-4'>Docs</p>
                     </div>
-                    <div className="FLEX____ITEM4 basis-1/4 order-1 gap-2">
+                    <div className="FLEX____ITEM4 basis-1/4 order-1 gap-2 py-3">
                         <LogIn className='bg-black hover:text-blue-900 duration-300 p-2 rounded w-12 h-12' />
                         <Settings className='bg-black hover:text-blue-900 duration-300 p-2 rounded w-12 h-12' />
                     </div>
