86fc67f3d00b7cceabf4f1d049f78213cd8d3b16
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-13 03:16:01 +0330

    REFACOTR

--- pages/infinite-scroll/index.tsx
+++ pages/infinite-scroll/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const index = () => {
+const InfiniteScroll = () => {
     return (
         <div>index</div>
     )
 }
 
-export default index
+export default InfiniteScroll
