e898d7e96a3fd43dfef4028874b52047db32fc23
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-13 03:30:07 +0330

    REFACOTR

--- pages/infinite-scroll/index.tsx
+++ pages/infinite-scroll/index.tsx
@@ -5,19 +5,19 @@ import React, { useEffect, useState } from 'react'
 type SingleCoinType = { id: string, symbol: string, name: string }
 
 const InfiniteScroll = () => {
-    const [coinsData, setCoinsData] = useState([])
+    const [commentsData, setCommentsData] = useState([])
 
-    const fetchCryptoData = () => {
-        axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false").then(data => {
+    const fetchCommentsData = () => {
+        axios.get("https://jsonplaceholder.typicode.com/comments").then(data => {
             console.info("COINS DATA ===>", data.data)
-            setCoinsData(data.data)
+            setCommentsData(data.data)
         })
     }
-    useEffect(() => { fetchCryptoData() }, [])
+    useEffect(() => { fetchCommentsData() }, [])
 
     return (
         <div className='w-screen h-screen flex flex-wrap gap-10 p-20 items-center justify-around'>
-            {coinsData.map((item: SingleCoinType) => (
+            {commentsData.map((item: SingleCoinType) => (
                 <Card key={item.id} className='size-32 flex items-center justify-center text-2xl font-bold'>{item.name}</Card>
             ))}
         </div>
