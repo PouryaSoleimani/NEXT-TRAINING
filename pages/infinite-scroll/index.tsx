62addf907eca83ec1bfdf8bfc257127632dbd888
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-13 04:39:15 +0330

    INFINITE SCROLL

--- pages/infinite-scroll/index.tsx
+++ pages/infinite-scroll/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client"
 import CommentListComponent from '@/COMPONENTS/COMMENT__LIST/CommentListComponent'
 import axios from 'axios'
@@ -18,25 +19,23 @@ const InfiniteScroll = () => {
 
     useEffect(() => { fetchCommentsData() }, [page])
 
-    const handleScroll = () => {
-        if (typeof window !== 'undefined') {
-            console.log("HEIGHT ------>", document.documentElement.scrollHeight)
-            console.log(_setPage)
-            console.log("Current page ------>", page);
-        }
-    }
 
     useEffect(() => {
+        const handleScroll = () => {
+            console.log("SCROLL HEIGHT →", document.documentElement.scrollHeight);
+        };
+
         if (typeof window !== 'undefined') {
-            window?.addEventListener("scroll", handleScroll)
+            window.addEventListener("scroll", handleScroll);
         }
+
         return () => {
             window.removeEventListener("scroll", handleScroll);
         };
-    }, [page])
+    }, []);
 
     return (
-        <div className='w-screen h-screen flex items-center justify-center gap-10 p-20  overflow-x-hidden'>
+        <div className='w-screen flex items-center justify-center gap-10 p-20  overflow-x-hidden'>
             <CommentListComponent commentsData={commentsData} />
         </div>
     )
