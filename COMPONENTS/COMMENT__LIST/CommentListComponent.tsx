072ef15836b69b55562ecd2f8361903b91ce554b
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-13 04:11:24 +0330

    INFINITE SCROLL

--- COMPONENTS/COMMENT__LIST/CommentListComponent.tsx
+++ COMPONENTS/COMMENT__LIST/CommentListComponent.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
-interface CommentsProps { commentsData: { id: number; name: string }[]; }
-type SingleCoinType = { id: number, name: string }
+interface CommentsProps { commentsData: { id: number; title: string }[]; }
+type SingleCoinType = { id: number, title: string }
 
 const CommentListComponent: React.FC<CommentsProps> = ({ commentsData }) => {
     return (
         <div className='w-screen h-screen grid grid-cols-4 grid-rows-3 place-items-center'>
             {commentsData.map((item: SingleCoinType) => (
-                <div key={item.id} className='size-32 flex items-center justify-center text-xl tracking-tighter font-bold bg-gradient-to-br from-zinc-900 via-gray-900 to-zinc-950 text-white'>
-                    {item.name.slice(0, 10)}
+                <div key={item.id} className='size-32 rounded-lg flex items-center justify-center text-xl tracking-tighter font-bold bg-gradient-to-br from-zinc-700 via-gray-800 to-zinc-900 text-white'>
+                    {item.title.slice(0, 10)}
                 </div>
             ))}
         </div>
--- pages/infinite-scroll/index.tsx
+++ pages/infinite-scroll/index.tsx
@@ -6,16 +6,24 @@ import React, { useEffect, useState } from 'react'
 
 const InfiniteScroll = () => {
     const [commentsData, setCommentsData] = useState([])
-    const [_page, _setPage] = useState(12)
+    const [_page, _setPage] = useState(1)
 
 
     const fetchCommentsData = () => {
-        axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=12&page=${_page}&sparkline=false`).then(data => {
+        axios.get(`https://jsonplaceholder.typicode.com/posts?_page=${_page}&_limit=12`).then(data => {
             console.info("COINS DATA ===>", data.data)
             setCommentsData(data.data)
         })
     }
     useEffect(() => { fetchCommentsData() }, [_page])
+
+    const handleScroll = () => {
+        console.info("HEIGHT +++++>", document.documentElement.scrollHeight)
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll)
+    }, [])
 
     return (
         <div className='w-screen h-screen flex items-center justify-center gap-10 p-20  overflow-x-hidden'>
