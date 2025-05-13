81cc82c50ea2b18fc9d2909e3280a2a2148f8ba2
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-13 04:12:06 +0330

    INFINITE SCROLL

--- COMPONENTS/COMMENT__LIST/CommentListComponent.tsx
+++ COMPONENTS/COMMENT__LIST/CommentListComponent.tsx
@@ -6,7 +6,7 @@ const CommentListComponent: React.FC<CommentsProps> = ({ commentsData }) => {
     return (
         <div className='w-screen h-screen grid grid-cols-4 grid-rows-3 place-items-center'>
             {commentsData.map((item: SingleCoinType) => (
-                <div key={item.id} className='size-32 rounded-lg flex items-center justify-center text-xl tracking-tighter font-bold bg-gradient-to-br from-zinc-700 via-gray-800 to-zinc-900 text-white'>
+                <div key={item.id} className='size-32 flex items-center justify-center text-xl tracking-tighter font-bold bg-gradient-to-br from-zinc-900 via-gray-900 to-zinc-950 text-white'>
                     {item.title.slice(0, 10)}
                 </div>
             ))}
