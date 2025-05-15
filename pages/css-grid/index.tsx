284354c44227c4772806743943416149a63f214a
Pourya_Delivando <Pourya@delivando.com>
2025-05-15 06:20:01 +0330

    INFINITE SCROLL

--- pages/css-grid/index.tsx
+++ pages/css-grid/index.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const index = () => {
     return (
-        <div>index</div>
+        <div className='grid__container'>
+            <div className="grid__item item__1">1</div>
+            <div className="grid__item item__2">2</div>
+            <div className="grid__item item__3">3</div>
+            <div className="grid__item item__4">4</div>
+            <div className="grid__item item__5">5</div>
+            <div className="grid__item item__6">6</div>
+            <div className="grid__item item__7">7</div>
+            <div className="grid__item item__8">8</div>
+        </div>
     )
 }
 
